17ee201e36b9ddf0f7e7ec30db6933ddbb4d2384
S <s>
2023-10-02 00:19:10 +0400

    {"types":"C","runs":38}

--- problem01.java
+++ problem01.java
@@ -4,15 +4,64 @@ public class problem01 extends Karel {
 
     public void run() {
         while (true) {
-            putBeeper(); // Place a beeper on the current block
-            if (frontIsClear()) {
-                move(); // Move to the next block if the front is clear
-            } else {
-                break; // If the front is blocked, stop the loop
+            putBeeperInRow();
+            if (!moveToNextRow()) {
+                break; // Stop the loop if there are no more rows
             }
         }
     }
+
+    // Move Karel to the next row
+    private boolean moveToNextRow() {
+        if (facingEast()) {
+            turnLeft();
+            if (frontIsClear()) {
+                move();
+                turnLeft();
+                return true; // Moved to the next row
+            } else {
+                return false; // No more rows to move
+            }
+        } else {
+            turnRight();
+            if (frontIsClear()) {
+                move();
+                turnRight();
+                return true; // Moved to the next row
+            } else {
+                return false; // No more rows to move
+            }
+        }
+    }
+
+    // Put beepers in the current row
+    private void putBeeperInRow() {
+        while (frontIsClear()) {
+            putBeeper();
+            move();
+        }
+        putBeeper(); // Put a beeper in the last block of the row
+        turnAround(); // Turn around to prepare for the next row
+        while (frontIsClear()) {
+            move(); // Move back to the starting position of the next row
+        }
+        turnAround(); // Turn around to face the next row
+    }
+
+    // Turn Karel 180 degrees
+    private void turnAround() {
+        turnLeft();
+        turnLeft();
+    }
+
+    // Turn Karel right
+    private void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
 }
+
 
 			
 
