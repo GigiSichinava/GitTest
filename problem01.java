fab6a83f0420c23b0f12b7d75904e1c948d1292d
S <s>
2023-10-01 21:51:30 +0400

    {"types":"C","runs":8}

--- problem01.java
+++ problem01.java
@@ -2,17 +2,8 @@
 
 	public class problem01 extends Karel {
 		public void run(){
-			for (int i=0; i < 4; i++) {
-				move();				
+			while (frontIsClear()) {
+				move();
 			}
-			for (int i=0; i < 10; i++) {
-				pickBeeper();	
-			}
-			move();
-			for (int i=0; i < 10; i++) {
-				putBeeper();
-				
-			}
-					
-		}
+			
 }
