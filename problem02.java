0f0c0f34b477082f5384c2216991fe9c4aa67f75
S <s>
2023-10-02 20:39:04 +0400

    {"types":"C","runs":3}

--- problem02.java
+++ problem02.java
@@ -1,4 +1,15 @@
+import stanford.karel.*;
 
 public class problem02 {
-
-}
+	public void run(){
+	for (int i=0; i < 4; i++){
+		move();
+	}
+	for (int i=0; i < 10; i++){
+		pickBeeper();
+	}
+		move();
+	for (int i=0; i < 10; i++){
+		putBeeper();
+	}		
+	}
