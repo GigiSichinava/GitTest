44fbbf21956340d9dbe3cc9c5dfb31b3a7a977d5
S <s>
2023-10-02 22:13:04 +0400

    {"types":"C","runs":48}

--- problem07.java
+++ problem07.java
@@ -1,4 +1,36 @@
+import stanford.karel.SuperKarel; ;
 
-public class problem07 {
+public class problem07 extends SuperKarel {
+	
+	public void run() {
+		
+		if (!beepersPresent()){
+			putBeeper ();
+		}
+		while(frontIsClear()){
+			fill();
+		if(frontIsClear()){
+				move();
+		}
+			putBeeper();
+			turnRight();
+		}
+	}
+		
+		private void fill() {
+			while(frontIsClear()){
+			move();
+			if (!beepersPresent()){
+			putBeeper();
+			}
+		}
+			turnLeft();
+			turnLeft();
+		while(frontIsClear()){
+			move();
+		}
+		turnRight();				
+		}
+		
+	}
 
-}
