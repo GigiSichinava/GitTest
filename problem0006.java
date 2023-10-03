b1c592f0fbde60abe37e66acc244193363bf805a
S <s>
2023-10-03 16:47:35 +0400

    {"types":"C","runs":30}

--- problem0006.java
+++ problem0006.java
@@ -17,6 +17,18 @@ public class problem0006 extends SuperKarel{
 			turnAround();
 		}
 		move();
-				
+		while(beepersPresent()){
+			pickBeeper();
+			move();
+			putBeeper();
+			move();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			move();
+			move();
+		}
+		
 	}
 }
