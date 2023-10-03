67171242c17b83041d27452a1a93c8c4133c9698
S <s>
2023-10-03 17:31:18 +0400

    {"types":"C","runs":47}

--- problem009.java
+++ problem009.java
@@ -1,4 +1,42 @@
+	import stanford.karel.*;
 
-public class problem009 {
+public class problem009 extends SuperKarel {
+	public void run(){
+		move();
+		while(beepersPresent());
+		pickBeeper();
+		move();
+		move();
+		putBeeper();
+		move();
+		putBeeper();
+		turnAround();
+		move();
+		move();
+		move();
+		turnAround();
+		
+		
+		
+		
+		
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*კარელმა უნდა გაამრავლოს ორი რიცხვი, 2x1 უჯრაზე დევს n ცალი ბრილიანტი, 3x1 უჯრაზე
+m ცალი ბრილიანტი, კარელმა 4x1 უჯრაზე უნდა დადოს n * m ბრილიანტი. ჩათვალეთ, რომ
+კარელს ჩანთაში უსასრულო რაოდენობის ბრილიანტი აქვს. */
