60b5dd30bc50a78e94d2f6200839f2d126d79ba6
S <s>
2023-10-03 11:35:07 +0400

    {"types":"C","runs":1}

--- problem08.java
+++ problem08.java
@@ -2,7 +2,24 @@ import stanford.karel.*;
 
 public class problem08 extends Karel{
 	public void run(){
+		int n=0;
+		int m=0;
+			
+		move();
 		
+		while(beepersPresent()){
+			pickBeeper();
+			n++;
+		}
+		
+		while(beepersPresent()){
+			pickBeeper();
+			m++;
+		}
+		
+		move();
+		for (int i=0; i < n-m; i++ );
+			
 		
 	}
 
