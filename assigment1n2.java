ee53307f6533f95b6218c934edaefb3b01406c7b
S <s>
2023-10-10 13:54:00 +0400

    {"types":"C","runs":64}

--- assigment1n2.java
+++ assigment1n2.java
@@ -3,8 +3,10 @@ import stanford.karel.SuperKarel;
 public class assigment1n2 extends SuperKarel {
 	public void run() {									
 	//	startPosition();
+	while(beepersInBag()){	
 		fillColumn();
-		moveToTheNextColumn();		
+		moveToTheNextColumn();	
+	}
 	}
 
 	/*
