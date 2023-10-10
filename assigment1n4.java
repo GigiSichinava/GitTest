e24df399d205e23245f1de4e5d67f5c4d91ec3cf
S <s>
2023-10-10 12:18:50 +0400

    {"types":"C","runs":26}

--- assigment1n4.java
+++ assigment1n4.java
@@ -3,13 +3,15 @@ import stanford.karel.*;
 public class assigment1n4 extends SuperKarel {
 	public void run(){
 		fillRow();
-		
-		
-		
+		findCentre();
+				
 	}
 
+		
+	
+
 	private void fillRow() {
-		// fill the first row with beepers
+		// fill the first row with beepers except first and last
 		while(frontIsClear()){
 			move();
 			putBeeper();
@@ -23,7 +25,23 @@ public class assigment1n4 extends SuperKarel {
 		turnAround();
 		move();
 	}		
+
+	private void findCentre() {
+		// find centre of the first row
+		while(beepersPresent()){
+			move();
+		}
+		turnAround();
+		move();
+		pickBeeper();
+		move();
+
+	}
+
+
 }
+
+
 	
 	
 	
