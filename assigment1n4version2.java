422bd9135dc007549be242be07d57a0b71e8fbf8
S <s>
2023-10-10 11:40:20 +0400

    {"types":"C","runs":7}

--- assigment1n4version2.java
+++ assigment1n4version2.java
@@ -3,13 +3,13 @@ import stanford.karel.SuperKarel;
 public class assigment1n4version2 extends SuperKarel{
 	public void run(){
 		while(frontIsClear()){
+			move();
 			turnLeft();
 			move();
 			move();
 			turnRight();
-			move();
 		}
-		turnRight();
+		
 		
 		
 	}
