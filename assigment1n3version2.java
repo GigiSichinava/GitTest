11464c7e88cb9e42a56c7fbc0c9990966eefb328
S <s>
2023-10-11 19:40:07 +0400

    {"types":"C","runs":41}

--- assigment1n3version2.java
+++ assigment1n3version2.java
@@ -1,4 +1,23 @@
+import stanford.karel.SuperKarel;
 
-public class assigment1n3version2 {
+public class assigment1n3version2 extends SuperKarel {
+public void run(){
+	
+	fillFirstLine();
+	//changeLine();
+	//fillSecondLine();	
+	
+	}
 
+private void fillFirstLine() {
+	while(frontIsClear()){
+		putBeeper();
+		move();
+		if(frontIsClear()){
+			move();
+			putBeeper();
+		}
+	}
+	
+}
 }
