89679bc0eca71d3bf146c62de623d7830b41584e
S <s>
2023-10-11 19:04:28 +0400

    {"types":"C","runs":11}

--- assigment1n4.java
+++ assigment1n4.java
@@ -2,14 +2,16 @@ import stanford.karel.*;
 
 public class assigment1n4 extends SuperKarel {
 	public void run(){
+	//make Karel to put beeper in the center of the first street.
 		fillRow();
 		findCentre();
-		putBeeper();		
+		finishJob();		
 	}
 
 
 	private void fillRow() {
-		// fill the first row with beepers except first and last corners
+		/* fill the first row with beepers except first and last corners
+		 * code considers 1x1 map also */
 		while(frontIsClear()){
 			move();
 			putBeeper();
@@ -30,8 +32,10 @@ public class assigment1n4 extends SuperKarel {
 		}
 	}		
 
+	
 	private void findCentre() {
 		// find center of the first row
+		
 	while(beepersPresent()){
 		
 		while(beepersPresent()){
@@ -47,12 +51,17 @@ public class assigment1n4 extends SuperKarel {
 		if(frontIsClear()){
 			move();
 		}
+	}
+			//make Karel ready to put beeper in  the center	
+		turnAround();
+		if(frontIsClear()){
+		move();
 		}
-		//make karel ready to put beeper in  the center	
-			turnAround();
-			if(frontIsClear()){
-				move();
-			}
+	}
+	
+	
+	private void finishJob(){
+		putBeeper();
 	}
 
 }
