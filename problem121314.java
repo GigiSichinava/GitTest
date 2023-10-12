32f0fa0059918b98eeaa5471c4438610b1271d4c
S <s>
2023-10-12 17:40:18 +0400

    {"types":"C","runs":7}

--- problem121314.java
+++ problem121314.java
@@ -2,12 +2,33 @@ import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class problem121314 extends GraphicsProgram {
-	private static final double PI = 3.14;
-	
+//	private static final double PI = 3.14;
+	private static final double HOUSE_WIDTH = 200;
+	private static final double HOUSE_HEIGTH = 300;
+	private static final double DOOR_WIDTH = 40;
+	private static final double DOOR_HEIGHT = 80;
+
+
 	public void run() {
 			 
-			drawMidLine();
+		drawWall();
+//		drawDoof();
+//		drawDoor();
+//		drawWindow();
+	}
+
+
+	private void drawWall() {
+		GRect wall = new GRect (HOUSE_WIDTH, HOUSE_HEIGTH);
+		int x = (int) (getWidth() /2 - HOUSE_WIDTH /2);
+		int y = getHeight() - HOUSE_HEIGHT;
+		add (wall, x, y);
 		}
+		
+	}	
+		
+//			drawMidLine();
+//		}
 	
 		/*		int a = 5;
 			a = a+1;
@@ -28,13 +49,13 @@ public class problem121314 extends GraphicsProgram {
 	*/
 	
 
-	private void drawMidLine() {
-		double x1 = 0;
-		double y1 = getHeight() / 2;
-		double x2 = getWidth();
-		double y2 = y1;
-		GLine Line = new GLine(x1, y1, x2, y2);
-		add (Line);
-	}
+//	private void drawMidLine() {
+//		double x1 = 0;
+//		double y1 = getHeight() / 2;
+//		double x2 = getWidth();
+//		double y2 = y1;
+//		GLine Line = new GLine(x1, y1, x2, y2);
+//		add (Line);
+//	}
 }	
 	
