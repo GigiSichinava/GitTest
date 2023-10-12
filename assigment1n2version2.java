e648fd0f60833532322a2cba7f978499fb69a031
S <s>
2023-10-12 22:04:24 +0400

    {"types":"C","runs":51}

--- assigment1n2version2.java
+++ assigment1n2version2.java
@@ -6,6 +6,7 @@ public class assigment1n2version2 extends SuperKarel {
 		while(frontIsClear()){
 		getToTheStartingPosition();
 		fillColumn();
+		getToTheStartingPosition();
 		changeColumn();
 		
 		}
