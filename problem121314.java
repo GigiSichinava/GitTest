ce4d4da56e68cf87f92b3210a6f20948be6c0dc6
S <s>
2023-10-12 17:08:54 +0400

    {"types":"C","runs":2}

--- problem121314.java
+++ problem121314.java
@@ -1,4 +1,15 @@
+import acm.program.GraphicsProgram;
 
-public class problem121314 {
+public class problem121314 extends GraphicsProgram {
+	public void run() {
+			int a = 5;
+			a = a+1;
+	}
 
+	private void testVoid() {
+		int a = 7;
+		
+	}
+	
+	
 }
