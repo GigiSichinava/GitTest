fbacf906da6b6c00b5e21db57ef570a603e8a516
S <s>
2023-10-13 02:03:18 +0400

    {"types":"C","runs":17}

--- assigment1n2version2.java
+++ assigment1n2version2.java
@@ -76,8 +76,9 @@ public class assigment1n2version2 extends SuperKarel {
 				
 				turnLeft();				
 				if(frontIsClear()){
-					turnRight();
 					move();
+				}else{
+					turnRight();
 				}
 				
 		 }															
