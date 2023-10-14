2461319b286d207a287b3dd11d1ab361e684a8cc
S <s>
2023-10-14 22:08:33 +0400

    {"types":"C","runs":29}

--- assigment1n4.java
+++ assigment1n4.java
@@ -2,7 +2,6 @@ import stanford.karel.*;
 
 public class assigment1n4 extends SuperKarel {
 	public void run(){
-	//make Karel to put beeper in the center of the first street.
 		fillRow();
 		findCentre();
 		finishJob();		
