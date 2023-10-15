ded6f852545fdb1dc0962b7cd98db185fdecc6a4
S <s>
2023-10-16 02:02:46 +0400

    {"types":"C","runs":4}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -10,8 +10,11 @@ import acm.program.*;
 public class ConsoleProgrammSample extends ConsoleProgram {
 
 	public void run() {
-		println("hellow world");
-		println("Yeat da best in da buildin");
+		println("This programe adds two numbers");
+		int n1 = readInt ("Enter n1:");
+		int n2 = readInt ("Enter n2:");
+		int total = n1 + n2;
+		println("The total is "+ total +".");
 	}
 
 }
