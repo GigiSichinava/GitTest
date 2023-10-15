00f16d5f123615155cca5b4cfa4c48fb7dd90427
S <s>
2023-10-16 02:09:26 +0400

    {"types":"C","runs":6}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -11,11 +11,8 @@ public class ConsoleProgrammSample extends ConsoleProgram {
 
 	public void run() {
 		
-		println ("adds two numbers");
-		int n1 = readInt ("Enter n1");
-		int n2 = readInt ("Enter n2");
-		int total = n1 + n2;
-		println("total is" + total + ".");
+	
+	
 	}
 
 }
