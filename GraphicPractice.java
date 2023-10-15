505491d54fd494c4d8fa54f254ab91f901bdfa76
S <s>
2023-10-16 02:17:17 +0400

    {"types":"C","runs":7}

--- GraphicPractice.java
+++ GraphicPractice.java
@@ -6,14 +6,10 @@ import acm.program.GraphicsProgram;
 public class GraphicPractice extends GraphicsProgram {
 	public void run(){
 		
-		GOval myOval = new GOval (200, 200);
-		add(myOval, 100, 100);
-		myOval.setFilled(true);
-		myOval.setColor(Color.RED);
-		
-		myOval = new GOval (200, 200);
-		add(myOval, 250, 100);
-		
+		GRect rect = new GRect (200, 200);
+		rect.setFilled(true);
+		rect.setColor(Color.BLUE);
+		add(rect, 50, 50);
 		
 	}
 	
