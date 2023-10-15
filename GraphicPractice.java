b42f496417864078d6576932894d7bc422b23e0c
S <s>
2023-10-16 02:17:43 +0400

    {"types":"C","runs":8}

--- GraphicPractice.java
+++ GraphicPractice.java
@@ -6,10 +6,10 @@ import acm.program.GraphicsProgram;
 public class GraphicPractice extends GraphicsProgram {
 	public void run(){
 		
-		GRect rect = new GRect (200, 200);
+		GRect rect = new GRect (20, 20);
 		rect.setFilled(true);
 		rect.setColor(Color.BLUE);
-		add(rect, 50, 50);
+		add(rect, 90, 90);
 		
 	}
 	
