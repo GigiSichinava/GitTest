a3df8d2f66826d8a5198993d8e0a2e322d04d278
S <s>
2023-10-16 02:21:23 +0400

    {"types":"C","runs":10}

--- GraphicPractice.java
+++ GraphicPractice.java
@@ -6,10 +6,8 @@ import acm.program.GraphicsProgram;
 public class GraphicPractice extends GraphicsProgram {
 	public void run(){
 		
-		GRect rect = new GRect (50, 50);
-		rect.setFilled(true);
-		rect.setColor(Color.RED);
-		add(rect, 60, 60);
+		int age = 29;
+		println ("age");
 	}
 	
 }
