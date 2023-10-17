efef4541bba64387b301460e0f865c413f672178
S <s>
2023-10-17 16:46:43 +0400

    {"types":"C","runs":17}

--- problem12.java
+++ problem12.java
@@ -19,6 +19,7 @@ public class problem12 extends GraphicsProgram{
 		
 			GRect rect3 = new GRect (190, 130, 40, 60);
 			rect3.setColor(Color.BLACK);
+			rect3.setColor(Color.RED);
 			rect3.setFilled(true);
 			add(rect3);
 			
