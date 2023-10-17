3e258fc39d93c9acb83f5cad9a88478f6d53638e
S <s>
2023-10-17 17:39:12 +0400

    {"types":"C","runs":39}

--- problem17.java
+++ problem17.java
@@ -2,8 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class problem17 extends ConsoleProgram {
 	public void run(){
-		
-		
+			
 		double a = readDouble ("Enter real number: ");
 		int integerPart = (int)a;
 		double fractionPart = a - integerPart;
