4f02488961fdd39b078c57633a03a0d546350d52
S <s>
2023-10-17 17:38:11 +0400

    {"types":"C","runs":38}

--- problem17.java
+++ problem17.java
@@ -4,6 +4,12 @@ public class problem17 extends ConsoleProgram {
 	public void run(){
 		
 		
+		double a = readDouble ("Enter real number: ");
+		int integerPart = (int)a;
+		double fractionPart = a - integerPart;
+		
+		println("Integer part: " + integerPart);
+		println("Fraction part: " + fractionPart);
 		
 	}
 
