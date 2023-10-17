d044a7911cb39b5601f8af4e6a06a0cbaf6ddfc6
S <s>
2023-10-17 17:25:24 +0400

    {"types":"C","runs":32}

--- problem15.java
+++ problem15.java
@@ -3,9 +3,15 @@ import acm.program.ConsoleProgram;
 public class problem15 extends ConsoleProgram{
 	public void run(){
 		
+		int a = readInt("Enter first number: ");
+		int b = readInt ("Enter secont number: ");
+		int c = readInt ("Enter third number: ");
 		
+		double d = (1/(double) a) + (1/(double) b) + (1/(double) c);
 		
-		
+		println (1/d);
+		println((a+b+c)/3.0);		
 		
 	}
+
 }
