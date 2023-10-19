39dc77c21acb847895487dc946c3b524d6f234a6
S <s>
2023-10-19 17:54:24 +0400

    {"types":"C","runs":11}

--- problem20.java
+++ problem20.java
@@ -3,16 +3,15 @@ import acm.program.ConsoleProgram;
 public class problem20 extends ConsoleProgram{
 	public void run(){
 		
-		int n = readInt("Enter your number: ");		
-		
-			int sum = 0;
+		int n = readInt("Enter your number: ");				
+		int sum = 0;
 			
 		for (int i = 0; i < n; i++){
 			int k = i + 1;
 			int num = readInt ("Enter number" + k + ":");
-			
 			sum += num;	
 		}
+		
 		println("sum: " + sum);
 		
 	}
--- problem21.java
+++ problem21.java
@@ -1,4 +1,23 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class problem21 {
+public class problem21 extends GraphicsProgram{
+	private static final int NUM_LINES = 11;
+	public void run(){
+		
+		for(int i = 0; i < NUM_LINES; i++);
+		 drawHorisontalLines(i);
+	}
+	private void drawHorisontalLines(int i) {
+		double y = i * (getHeight() / (NUM_LINES - 1) );
+		double x1 = 0;
+		double x2 = getWidth();
+		GLine line = new GLine (x1, y, x2, y);
+		add(line);
+		
+		
+		
+	}
+	
 
 }
