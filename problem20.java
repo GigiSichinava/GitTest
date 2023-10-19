0c21b48faca3dd0bd147e7e2eee5a8f04b4a3059
S <s>
2023-10-19 17:35:37 +0400

    {"types":"C","runs":5}

--- problem20.java
+++ problem20.java
@@ -4,12 +4,14 @@ public class problem20 extends ConsoleProgram{
 	public void run(){
 		
 		int n = readInt("Enter your number: ");		
-		int c = 0;
 		
-		for(int i = 1; i <= n; i++){
-			int m = readInt("Add your number: ");
-			c += m;						
+			int sum = 0;		
+		for (int i = 0; i < n; i++){
+			int num = readInt ("Enter number" + 1 + ":");
+			
+			sum += num;	
 		}
-		println(c);
+		println("sum: " + sum);
+		
 	}
 }
