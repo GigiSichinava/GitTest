89663d437693ac09118c143bbab4f4f598c1b533
S <s>
2023-10-24 17:43:14 +0400

    {"types":"C","runs":4}

--- problem24.java
+++ problem24.java
@@ -8,7 +8,7 @@ public class problem24 extends ConsoleProgram{
 		println(numberOfDivisions);
 	}
 
-	private void numberOfDivisors() {
+	private void numberOfDivisors(int n) {
 		int num = 0;
 		for(int i = 1; i <= n; i++){
 			if (n % i == 0) {  //% - reminder
