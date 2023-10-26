347810fd98d7a60f654491698701d55d117791e6
S <s>
2023-10-26 17:19:46 +0400

    {"types":"C","runs":2}

--- problem27.java
+++ problem27.java
@@ -3,9 +3,12 @@ import acm.program.ConsoleProgram;
 public class problem27 extends ConsoleProgram {
 	public void run(){
 		int n = readInt ("Enter n: ");
-		int m = readInt ("Enter m");
-		println(m^n);
-		
+		int m = readInt ("Enter m: ");
+		 	
+		for(int i=0; i <= m; i++){
+			n = n * n;
+		}
+		println(n);
 		
 	}
 }
