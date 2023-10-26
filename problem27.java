c590022008324ab2a216fc4db7aeef67032b02a7
S <s>
2023-10-26 17:16:34 +0400

    {"types":"C","runs":1}

--- problem27.java
+++ problem27.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class problem27 {
-
+public class problem27 extends ConsoleProgram {
+	public void run(){
+		int n = readInt ("Enter n: ");
+		int m = readInt ("Enter m");
+		println(m^n);
+		
+		
+	}
 }
