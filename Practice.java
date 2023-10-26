e18a97884c6f85f1dd684d7cad4f71660806bb9a
S <s>
2023-10-26 23:27:01 +0400

    {"types":"C","runs":41}

--- Practice.java
+++ Practice.java
@@ -4,9 +4,12 @@ public class Practice extends ConsoleProgram{
 	public void run(){
 		 
 		int a = readInt ("Enter first number: ");
-		 int b = readInt ("Enter second number: ");
-		 int sum = (a + b) / 2 ;
-		 print(sum);
+		int b = readInt ("Enter second number: ");
+		int c = readInt ("Entwr third number: ");
+		
+		int sum = 1/a + 1/b + 1/c;
+		
+		 print(1/sum);
 		
 		
 		
