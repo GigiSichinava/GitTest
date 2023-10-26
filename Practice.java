39ed0c531662d5b0ea1ad3d0edee17390d14fa38
S <s>
2023-10-26 23:28:20 +0400

    {"types":"C","runs":44}

--- Practice.java
+++ Practice.java
@@ -3,13 +3,13 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		 
-		int a = readInt ("Enter first number: ");
-		int b = readInt ("Enter second number: ");
-		int c = readInt ("Entwr third number: ");
+		double a = readInt ("Enter first number: ");
+		double b = readInt ("Enter second number: ");
+		double c = readInt ("Entwr third number: ");
 		
 		double sum = 1/a + 1/b + 1/c;
 		
-		 print(1/sum);
+		 println(1/sum);
 		
 		
 		
