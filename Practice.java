51aa9ce137e26760d4128f9e94e4618ad5b9d9b5
S <s>
2023-10-26 20:20:31 +0400

    {"types":"C","runs":5}

--- Practice.java
+++ Practice.java
@@ -3,12 +3,13 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
-	println("This program adds two integer nubers! ");	
+	println("This program adds two integer numbers! ");	
+	println("");
 	int a = readInt ("Enter your number: ");
 	int b = readInt ("Enter your number: ");
 	int sum = a + b;
 	
-	println("Your result is" + sum);
+	println("Your result is " + sum);
 	
 	}
 	
