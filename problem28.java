aeb497ddde305097bceb995c4dfc7ce5134d8c78
S <s>
2023-10-26 17:44:48 +0400

    {"types":"C","runs":26}

--- problem28.java
+++ problem28.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class problem28 {
-
+public class problem28 extends ConsoleProgram{
+	
+	private static final int SENTINEL = -1;
+	
+	
+	public void run(){
+		int n = readInt ("Enter positive number: ");
+		int numberOfOdds = 0;
+		
+		while(true){
+			
+			int odd = readInt ("Enter your number: ");
+			if(odd == SENTINEL);
+			break;
+		}
+		
+		while (n % 2 == 0){
+			numberOfOdds++;
+			
+		}
+	}
 }
