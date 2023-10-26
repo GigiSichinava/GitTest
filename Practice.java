522a80d9f9d732407ffc1673965d2ccd4bbe8c3c
S <s>
2023-10-26 23:04:20 +0400

    {"types":"C","runs":34}

--- Practice.java
+++ Practice.java
@@ -12,8 +12,8 @@ public class Practice extends GraphicsProgram{
 	private static final int HEIGHT = 200; // height of face
 	private static final int LEFT_EYE_DIAMETER = 45; //diameter of left eye
 	private static final int RIGHT_EYE_DIAMETER = 45; // diameter of right eye
-	private static final int MOUTH_WIDTH = 140; //width of mouth
-	private static final int MOUTH_HEIGHT = 40; //height of mouth
+	private static final int MOUTH_WIDTH = 120; //width of mouth
+	private static final int MOUTH_HEIGHT = 35; //height of mouth
 	
 	public void run(){
 		
@@ -52,8 +52,8 @@ public class Practice extends GraphicsProgram{
 		add(rightEye);
 	
 	// MOUTH OF THE ROBOT
-		int mainXMouth = mainX + 30;
-		int mainYMouth = mainY + 150;		
+		int mainXMouth = mainX + 40;
+		int mainYMouth = mainY + 140;		
 		GRect mouth = new GRect (mainXMouth, mainYMouth, MOUTH_WIDTH, MOUTH_HEIGHT);
 		mouth.setFilled(true);
 		mouth.setColor(Color.BLACK);
