a7ba21a092503e5db4edd042fc619d1852217f58
S <s>
2023-10-26 20:37:30 +0400

    {"types":"C","runs":17}

--- Practice.java
+++ Practice.java
@@ -1,19 +1,17 @@
-import acm.graphics.GLabel;
-import acm.program.ConsoleProgram;
+
+import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class Practice extends GraphicsProgram{
+	
+	
 	public void run(){
+		int x = getWidth() ;
+		int y = getHeight() / 2;
 		
-//	println("This program adds two integer numbers! ");	
-//	println("  ");
-//	int a = readInt ("Enter your number: ");
-//	int b = readInt ("Enter your number: ");
-//	int sum = a + b;
+		GLine a = new GLine (y, 0, x, y );
+		add (a);
 	
-	GLabel a = new GLabel ("Hello world", 10 ,40);
-	a.setFont("Lucida Blackletter-28");
-	add (a);
+	
 	}
-	
 }
