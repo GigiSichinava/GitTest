1e99772c4d6a12ac4d83023eefda6f42b37f0a5d
S <s>
2023-10-27 12:14:23 +0400

    {"types":"C","runs":26}

--- Practice.java
+++ Practice.java
@@ -1,7 +1,30 @@
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
+	
+	private static final int SENTINEL = -1;
+	
 	public void run(){
 		 
+	while(true){	
+		int n  = readInt ("Enter your number: ");
+		if ( n == SENTINEL ){
+			break;
+		}
+		if (isPrimeFast(n)){
+			println("prime");
+		}else{
+			println("not prime");
+		}	
+			
+	}
 		
+	}
+
+	private boolean isPrimeFast(int n) {
+		int result = 0;
+		for (int i = 1 ; i <= n; i++)
+			
+		return false;
+	}
 }
