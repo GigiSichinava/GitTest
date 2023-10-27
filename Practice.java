960d758abf77ffa394d848eb89c9ab591e35e13d
S <s>
2023-10-27 10:05:56 +0400

    {"types":"C","runs":1}

--- Practice.java
+++ Practice.java
@@ -3,19 +3,15 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		 
-		int n = readInt ("Enter your number: ");
+		int a = readInt ("Enter your number: ");
 		int sum = 0;
 		
-		for ( int i = 0; i < n; i++ ){
-			int k = i + 1;
-			int num = readInt ("Your number " + k + ": ");
+		for (int i=0; i < a; i++);
+			int k = a + 1;
+			int num = readInt ("Enter number" + k + ":");
 			sum += num;
 			
-		}
-		
-		println ("Result: " + sum );
-		
-		
+		println(sum);			
 		
 	}
 }
