23defa41dac628d0893150d0e157faa2d04d4a25
S <s>
2023-10-27 12:10:12 +0400

    {"types":"C","runs":24}

--- problem31.java
+++ problem31.java
@@ -1,4 +1,35 @@
+import acm.program.ConsoleProgram;
 
-public class problem31 {
+public class problem31 extends ConsoleProgram{
+	
+	private static final int SENTINEL = -1;
+	
+	public void run(){
+		 
+	while(true){	
+		int n  = readInt ("Enter your number: ");
+		if ( n == SENTINEL ){
+			break;
+		}
+		if (isPrimeFast(n)){
+			println("prime");
+		}else{
+			println("not prime");
+		}	
+			
+	}
+		
+	}
 
+	private boolean isPrimeFast(int n) {
+		int result = 0;
+		for (int i = 1 ; i * i <= n; i++){
+			if (n % i == 0){
+				return(false);
+			}
+		}
+		
+			
+		return false;
+	}
 }
