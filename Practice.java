d2c710649ef6ca49a9c5a1a0baae92659ebdd0a1
S <s>
2023-10-27 10:07:42 +0400

    {"types":"C","runs":4}

--- Practice.java
+++ Practice.java
@@ -8,7 +8,7 @@ public class Practice extends ConsoleProgram{
 		
 		for (int i = 0; i < a; i++){
 			int k = i + 1;
-			int num = readInt ("Enter number " + k + ":");
+			int num = readInt ("Enter number " + k + ": ");
 			sum += num;
 		}
 		
