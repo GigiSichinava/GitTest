3c68c444085c6fe4f808acedce98d489d97c02cd
S <s>
2023-10-27 10:38:22 +0400

    {"types":"C","runs":23}

--- problem29.java
+++ problem29.java
@@ -8,8 +8,10 @@ public class problem29 extends ConsoleProgram{
 	}
 		
 		private int fibonacciNumber(int n){
-			
-		int a =0, b = 1;
+			 if ( n == 1){
+				 return 0;
+			 }
+		int a = 0, b = 1;
 		for (int i = 3; i <= n; i++){
 			int c = a + b;
 			a = b;
