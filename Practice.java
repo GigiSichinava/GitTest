1bd03cf5334587c66a0be32e2e87239fe182d706
S <s>
2023-10-30 01:01:16 +0400

    {"types":"A","runs":10}

---
+++ Practice.java
@@ -0,0 +1,18 @@
+import acm.program.ConsoleProgram;
+import stanford.karel.SuperKarel;
+
+public class Practice extends SuperKarel {
+	
+
+	public void run() {
+		
+		
+		while (frontIsClear()){ 
+			move();
+		}
+		
+		
+	
+		
+	}
+}		
