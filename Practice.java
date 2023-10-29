225ec5ccd371c7964e465ef0aa30cb93d7da88f3
S <s>
2023-10-29 19:55:34 +0400

    {"types":"C","runs":7}

--- Practice.java
+++ Practice.java
@@ -1,8 +1,26 @@
-import acm.program.ConsoleProgram;
+/*
+ * File: Hailstone.java
+ * Name: 
+ * Section Leader: 
+ * --------------------
+ * This file is the starter file for the Hailstone problem.
+ */
+
+import acm.program.*;
 
 public class Practice extends ConsoleProgram {
-	public void run(){
+	
+	private static final int SENTINEL = 1;
+	
+	public void run() {
+	
+		int n = readInt ("Enter a number: ");
 		
-		println ("you got this cause you are you" );
+		while (n != SENTINEL){			
+			if( n % 2 == 0){
+			int	nextNumber = n / 2;
+				
+			}
+		}
 	}
 }
