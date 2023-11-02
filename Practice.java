713777e1bafcba455d134ffdad992d2bd256e77a
S <s>
2023-11-02 16:06:39 +0400

    {"types":"C","runs":30}

--- Practice.java
+++ Practice.java
@@ -19,37 +19,25 @@ public class Practice extends ConsoleProgram {
 		int steps = 0;
 
 		// use while statement when n doesn't equal to 1
-		while (n != SENTINEL) { 	
-			if (n % 2 == 0) { 		// if first number is even
-				int nextNumber = n / 2; 	// divide even number
+		while (n != SENTINEL) {
+
+			if (n % 2 == 0) { // if first number is even
+				int nextNumber = n / 2; // divide even number
 				println(n + " is even so I take half: " + nextNumber);
 				n = nextNumber;
 
-//				if (nextNumber % 2 != 0) { 			// when n is odd
-//					if (nextNumber != SENTINEL) { 	// check if next number equals to 1
-//													
-//						nextNumber = n * 3 + 1; 	// while number is odd use n * 3 + 1 formula
-//												
-//						println(n + " is odd, so I make 3n + 1: " + nextNumber);
-//						n = nextNumber;
-//					}
-//				}
 			}
 
 			else {
 
-				if (n % 2 != 0) { 				// if first number is odd
-					int nextNumber = n * 3 + 1; // while number is odd use n * 3 + 1 formula
-												
+				if (n % 2 != 0) { // if first number is odd
+					int nextNumber = n * 3 + 1; // while number is odd use n * 3
+												// + 1 formula
+
 					println(n + " is odd, so I make 3n + 1: " + nextNumber);
 					n = nextNumber;
 				}
 
-//				if (n % 2 == 0) { 			// if first number is even
-//					int nextNumber = n / 2; // divide even number
-//					println(n + " is even so I take half: " + nextNumber);
-//					n = nextNumber;
-//				}
 			}
 			steps++;
 		}
