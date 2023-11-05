573c5a7d08798ef3c3661d6282fec8125308e8dd
S <s>
2023-11-05 22:33:14 +0400

    {"types":"C","runs":10}

--- Practice.java
+++ Practice.java
@@ -10,7 +10,7 @@ public class Practice extends ConsoleProgram {
 	
  	public void run(){
  		int a = 0;
- 		for (int i = 0; i < 5; i++){
+ 		for (int i = 0; i < 1; i++){
  			for (int j = 0; j < 3; j ++){
  				a += 1;
  				println(a);
