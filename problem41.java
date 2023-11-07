5fc010743b61e75ed1dd39971917280a66f0a649
S <s>
2023-11-08 01:16:35 +0400

    {"types":"C","runs":168}

--- problem41.java
+++ problem41.java
@@ -10,12 +10,15 @@ public class problem41 extends GraphicsProgram{
 		GOval oval = new GOval (D, D);
 		add(oval, 0, getHeight() / 2 - D / 2);
 	
-	while(oval.getX() <= getWidth() - D  ){
+	while(oval.getX() <= getWidth() - D ){
 		pause(PAUSE);
 		oval.move(10, 0);
 	}
 	
-	
+	while(oval.getX() >= 0){
+		pause(PAUSE);
+		oval.move(10, 0);
+	}
 	
 	}
 }
