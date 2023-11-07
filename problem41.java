1984eb303f7d321f40d35ad654b0906761123494
S <s>
2023-11-08 01:37:28 +0400

    {"types":"C","runs":204}

--- problem41.java
+++ problem41.java
@@ -14,18 +14,17 @@ public class problem41 extends GraphicsProgram {
 
 		GOval oval = new GOval(D, D);
 		add(oval, 0, getHeight() / 2 - D / 2);
-		
 
-		
 		while (true) {
-			Color color1 = rgen.nextColor();
 
+			Color color1 = rgen.nextColor();
 			while (oval.getX() <= getWidth() - D) {
 				pause(PAUSE);
 				oval.move(10, 0);
 				oval.setFilled(true);
 				oval.setColor(color1);
 			}
+
 			Color color2 = rgen.nextColor();
 			while (oval.getX() >= 0) {
 				pause(PAUSE);
@@ -33,7 +32,7 @@ public class problem41 extends GraphicsProgram {
 				oval.setFilled(true);
 				oval.setColor(color2);
 			}
-			
+
 		}
 
 	}
