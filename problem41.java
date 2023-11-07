34a379a0c3ee091f67a368267429189c9c55ab0a
S <s>
2023-11-08 01:11:51 +0400

    {"types":"C","runs":159}

--- problem41.java
+++ problem41.java
@@ -1,4 +1,21 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class problem41 {
-
+public class problem41 extends GraphicsProgram{
+	
+	private final int PAUSE = 1000;
+	private final int R = 10;
+	public void run(){
+		
+		GOval oval = new GOval (R, R);
+		add(oval);
+	
+	while(oval.getX() <= getWidth() - R *2 ){
+		pause(PAUSE);
+		oval.move(10, 0);
+	}
+	
+	
+	
+	}
 }
