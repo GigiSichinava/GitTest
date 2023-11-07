47d4e74c3203a978c2b37a13e2e97b202b95cf78
S <s>
2023-11-07 19:03:52 +0400

    {"types":"C","runs":107}

--- Practice.java
+++ Practice.java
@@ -25,10 +25,10 @@ public class Practice extends GraphicsProgram {
 			oval.move(5, -5);
 		}
 
-//		while (oval.getX() <= getWidth() - D / 2) {
-//			pause(PAUSE);
-//			oval.move(-5, -5);
-//		}
+		while (oval.getY() >= 0) {
+			pause(PAUSE);
+			oval.move(-5, -5);
+		}
 
 //		while (oval.getY() >=  0 ) {
 //			pause(PAUSE);
