c32c2b8d019cabac44ac68ed36aaa15d6bd9a531
S <s>
2023-11-07 17:51:06 +0400

    {"types":"C","runs":55}

--- Practice.java
+++ Practice.java
@@ -7,20 +7,20 @@ import acm.util.RandomGenerator;
 public class Practice extends GraphicsProgram {	
 	
 	private static final int PAUSE = 100;
-	private static final int R = 100;
+	private static final int D = 100;
 	
 	public void run(){
 		
-		GOval oval = new GOval(R, R);
+		GOval oval = new GOval(D, D);
 		add(oval);
 		oval.setLocation(0,0);
 		
 		
-		while(oval.getY() != getHeight() - R){
+		while(oval.getY() != getHeight() - D){
 			pause(PAUSE);
 			oval.move(10, 10);			
 		}
-	
+		
 			
 	}
 }
