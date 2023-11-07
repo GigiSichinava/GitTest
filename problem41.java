701d2e0da91afb4781235826b8ff4ec7025d0c0f
S <s>
2023-11-08 01:35:53 +0400

    {"types":"C","runs":201}

--- problem41.java
+++ problem41.java
@@ -19,14 +19,14 @@ public class problem41 extends GraphicsProgram {
 		
 		while (true) {
 			Color color1 = rgen.nextColor();
-//			Color color2 = rgen.nextColor();
+
 			while (oval.getX() <= getWidth() - D) {
 				pause(PAUSE);
 				oval.move(10, 0);
 				oval.setFilled(true);
 				oval.setColor(color1);
 			}
-			
+			Color color2 = rgen.nextColor();
 			while (oval.getX() >= 0) {
 				pause(PAUSE);
 				oval.move(-10, 0);
