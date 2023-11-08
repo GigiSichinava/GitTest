121c84fa1c596ff795c45a8cfeb6792c109a0c22
S <s>
2023-11-08 17:00:12 +0400

    {"types":"C","runs":243}

--- problem38.java
+++ problem38.java
@@ -1,4 +1,36 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class problem38 {
-
+public class problem38 extends ConsoleProgram {
+	
+	private RandomGenerator random;
+	
+	private static final int SENTINEL = -1;
+	
+	public void run() {
+		random = RandomGenerator.getInstance();
+		while (true) {
+			int chosenNumber = readInt("enter number 0-36: ");
+			if (chosenNumber == SENTINEL) {
+				break;
+			}
+			int randomNumber = getRandomNumber();
+			processResult(chosenNumber, randomNumber);
+		}
+	}
+	
+	private void processResult(int chosenNumber, int randomNumber) {
+		println(".....");
+		println(randomNumber);
+		if (randomNumber == chosenNumber) {
+			println("you win!");
+		} else {
+			println("you lose!");
+		}
+	}
+	
+	private int getRandomNumber() {
+		return random.nextInt(0, 36);
+	}
+	
 }
