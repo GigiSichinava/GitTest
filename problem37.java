35dc133eb1a1f4662c4578541f244345f6616d71
S <s>
2023-11-08 17:02:01 +0400

    {"types":"C","runs":246}

--- problem37.java
+++ problem37.java
@@ -2,22 +2,24 @@ import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
 public class problem37 extends ConsoleProgram {
-	
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	public void run(){
-		
-		while(true){
-		int a = readInt ("Enter your number here: ");
 
-		int randomNumber =  rgen.nextInt(0, 36);
-		println("Game has chosen: " + randomNumber);
-	
-		if (randomNumber == a){
-			println("You have won!");
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+
+		while (true) {
+
+			int a = readInt("Enter your number here: ");
+
+			int randomNumber = rgen.nextInt(0, 36);
+			println("Game has chosen: " + randomNumber);
+
+			if (randomNumber == a) {
+				println("You have won!");
+			} else
+				println("You have lost");
+				println();
+
 		}
-		else
-			println("You have lost");
-	
-	}
 	}
 }
