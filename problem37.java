a2d5043474c5e691b4b257321e6e7d5425189501
S <s>
2023-11-08 17:29:46 +0400

    {"types":"C","runs":284}

--- problem37.java
+++ problem37.java
@@ -11,21 +11,24 @@ public class problem37 extends ConsoleProgram {
 
 			int a = readInt("Enter your number here: ");
 
+			// check if number is between 0 and 36
 			if (a > 36 || a < 0) {
 				println("Error number!");
 				println();
-			}else{
-
-			int randomNumber = rgen.nextInt(0, 36);
-			println("Game has chosen: " + randomNumber);
-
-			if (randomNumber == a) {
-				println("You have won!");
-				println();
 			} else {
-				println("You have lost");
-				println();
-			}
+				// if number is between 0 and 36
+				int randomNumber = rgen.nextInt(0, 36);
+				println("Game has chosen: " + randomNumber);
+
+				// if our number equals to game's number we win
+				if (randomNumber == a) {
+					println("You have won!");
+					println();
+				} else {
+					// if our number doesn't equal to game's number we win
+					println("You have lost");
+					println();
+				}
 			}
 		}
 	}
