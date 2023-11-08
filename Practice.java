308fca2ff3b5f23dd30eaaa97150c219f0a68b3a
S <s>
2023-11-08 23:47:07 +0400

    {"types":"AC","runs":306}

--- Practice.java
+++ Practice.java
@@ -1,39 +1,75 @@
-import java.awt.Color;
+/*
+ * File: Breakout.java
+ * -------------------
+ * Name:
+ * Section Leader:
+ * 
+ * This file will eventually implement the game of Breakout.
+ */
 
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JFrame;
 
 public class Practice extends GraphicsProgram {
 
-	private final int PAUSE = 10;
-	private final int D = 100;
-	private RandomGenerator rgen = RandomGenerator.getInstance();
+/** Width and height of application window in pixels */
+	public static final int APPLICATION_WIDTH = 400;
+	public static final int APPLICATION_HEIGHT = 600;
 
+/** Dimensions of game board (usually the same) */
+	private static final int WIDTH = APPLICATION_WIDTH;
+	private static final int HEIGHT = APPLICATION_HEIGHT;
+
+/** Dimensions of the paddle */
+	private static final int PADDLE_WIDTH = 60;
+	private static final int PADDLE_HEIGHT = 10;
+
+/** Offset of the paddle up from the bottom */
+	private static final int PADDLE_Y_OFFSET = 30;
+
+/** Number of bricks per row */
+	private static final int NBRICKS_PER_ROW = 10;
+
+/** Number of rows of bricks */
+	private static final int NBRICK_ROWS = 10;
+
+/** Separation between bricks */
+	private static final int BRICK_SEP = 4;
+
+/** Width of a brick */
+	private static final int BRICK_WIDTH =
+	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+
+/** Height of a brick */
+	private static final int BRICK_HEIGHT = 8;
+
+/** Radius of the ball in pixels */
+	private static final int BALL_RADIUS = 10;
+
+/** Offset of the top brick row from the top */
+	private static final int BRICK_Y_OFFSET = 70;
+
+/** Number of turns */
+	private static final int NTURNS = 3;
+
+/* Method: run() */
+/** Runs the Breakout program. */
 	public void run() {
-
-		GOval oval = new GOval(D, D);
-		add(oval, 0, getHeight() / 2 - D / 2);
-
-		while (true) {
-
-			Color color1 = rgen.nextColor();
-			while (oval.getX() <= getWidth() - D) {
-				pause(PAUSE);
-				oval.move(10, 0);
-				oval.setFilled(true);
-				oval.setColor(color1);
-			}
-
-			Color color2 = rgen.nextColor();
-			while (oval.getX() >= 0) {
-				pause(PAUSE);
-				oval.move(-10, 0);
-				oval.setFilled(true);
-				oval.setColor(color2);
-			}
-
-		}
-
+		/* You fill this in, along with any subsidiary methods */
 	}
+
+	public class frame{
+		
+		JFrame frame	 = new JFrame ();
+		frame.setBounds 10, 10, 10, 10,
+	}
+	
+	
 }
