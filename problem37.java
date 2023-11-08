ac243939b79f1b32572d82181c94087adf2c43c3
S <s>
2023-11-08 16:52:32 +0400

    {"types":"C","runs":232}

--- problem37.java
+++ problem37.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class problem37 {
-
+public class problem37 extends ConsoleProgram {
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run(){
+		
+		int a = readInt ("Enter your number here: ");
+		
+		int randomNumber =  rgen.nextInt(0, 36);
+		println(randomNumber);
+	}
 }
