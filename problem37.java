79e48dd5692681ac90f95834f29def6ae86625c8
S <s>
2023-11-08 17:27:16 +0400

    {"types":"C","runs":281}

--- problem37.java
+++ problem37.java
@@ -13,7 +13,7 @@ public class problem37 extends ConsoleProgram {
 
 			if (a > 36 || a < 0) {
 				println("Error number!");
-				println();
+				return;
 			}
 
 			int randomNumber = rgen.nextInt(0, 36);
