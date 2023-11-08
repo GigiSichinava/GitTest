cb8bc032cf3b14acc880774b43a07618b55b2a67
S <s>
2023-11-09 00:34:54 +0400

    {"types":"C","runs":333}

--- Practice.java
+++ Practice.java
@@ -65,11 +65,11 @@ public class Practice extends GraphicsProgram {
 	}
 
 	double startingX = BRICK_SEP;
-	double startingY = 0;
+	double startingY = PADDLE_Y_OFFSET;
 	private void addBricks() {
 		for (int rowNumber = 0; rowNumber < NBRICK_ROWS; rowNumber++) {
 			for (int bricksNumber = 0; bricksNumber <= NBRICKS_PER_ROW; bricksNumber++) {
-				GRect rect = new GRect(startingX + BRICK_WIDTH * bricksNumber + BRICK_SEP, BRICK_Y_OFFSET + BRICK_HEIGHT * rowNumber, BRICK_WIDTH, BRICK_HEIGHT);
+				GRect rect = new GRect(startingX + BRICK_WIDTH * bricksNumber + BRICK_SEP, startingY + BRICK_HEIGHT * rowNumber, BRICK_WIDTH, BRICK_HEIGHT);
 				rect.setFilled(false);
 				add(rect);
 			}
