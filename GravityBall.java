4916016ffa9a7cd0d1d34f58eb0241ea3866161c
S <s>
2023-11-10 20:54:45 +0400

    {"types":"C","runs":631}

--- GravityBall.java
+++ GravityBall.java
@@ -1,4 +1,28 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class GravityBall {
+public class GravityBall extends GraphicsProgram{
+	
+	final static double D = 50;
+	final static double PAUSE = 10;
+	final static double x = 10;
+	final static double y = 10;
+	
+			
+	
+	public void run(){
+		
+		GOval oval = new GOval (D, D);
+		oval.setFilled(true);
+		add(oval);
+		
+		double dx = x;
+		double dy = y;
+		
+		while (oval.getY() < getHeight() - D){
+			
+		}
+	}
+	
 
 }
