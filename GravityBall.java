6ada1345a5e737c4be2d312ffdc0f3a722d7e479
S <s>
2023-11-10 21:12:21 +0400

    {"types":"C","runs":663}

--- GravityBall.java
+++ GravityBall.java
@@ -23,7 +23,7 @@ public class GravityBall extends GraphicsProgram{
 			dy = dy + GRAVITY;
 			pause(PAUSE);
 			if(oval.getY() > getHeight() - D){
-//				dy = dy / 2;
+				dy = dy * 3/2;
 				dy = -dy;	
 				
 			}
