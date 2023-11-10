f3eb8ec11170aebf5ca4efd1a8952e434d723446
S <s>
2023-11-10 20:38:32 +0400

    {"types":"C","runs":623}

--- random.java
+++ random.java
@@ -14,26 +14,37 @@ public class random extends GraphicsProgram {
 	private final int y = 5;
 	private final double GRAVITY = 0.7;
 
-	public void run() {
+	private double dy;
+	private double dx;
+	private GOval ball;
 
-		GOval ball = new GOval (D, D);
-		ball.setFilled(true);
-		add(ball, D / 2, 100);		
-		int dx = x;
-		int dy = y;
-		
-		while(true){
-		dy += GRAVITY;
-		ball.move(dx, dy);
-		pause(PAUSE);
-		
-		if(ball.getY() >= getHeight() - D){
-			dy = -dy;
-			
-		}
-		}
+	public void run() {
+			setup();
+			while(ball.getX() <= getWidth()){			
+			moveBall();
+			checkBall();
+			}
 	}
 	
-	
+	private void setup() {
+		GOval ball = new GOval(D, D);
+		ball.setFilled(true);
+		add(ball, D / 2, 100);
+	}
+
+	private void moveBall() {
+		dy += GRAVITY;
+		ball.move(dx, dy);
+	}
+
+	private void checkBall(){
+		if(ball.getY() >= getHeight() - D){
+		dy = -dy;
+		double diff = ball.getY() - (getHeight() - D);
+		 ball.move(0, -2 * diff);
+	}
+			
+	}
 }
+
 
