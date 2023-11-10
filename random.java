be357dcb823d3a596dc7fc9b6f87e6ecb980dd30
S <s>
2023-11-10 20:45:02 +0400

    {"types":"C","runs":629}

--- random.java
+++ random.java
@@ -1,47 +1,75 @@
-import java.awt.Color;
-import java.awt.event.MouseEvent;
-
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
-
+import acm.program.*;
+import acm.graphics.*;
 public class random extends GraphicsProgram {
 
-	private final int PAUSE = 50;
-	private final int D = 50;
-	private final double x = 5.0;
-	private final double y = 0.0;
-	private final double GRAVITY = 0.7;
+ /** Size (diameter) of the ball */
+ private static final int DIAM_BALL = 30;
 
-	private double dy = x;
-	private double dx = y;
-	private GOval ball;
+ /** Amount Y velocity is increased each cycle as a
+ * result of gravity */
+ private static final double GRAVITY = 3;
 
-	public void run() {
-		setup();
-		while (ball.getX() < getWidth()) {
-			moveBall();
-			checkBall();
-			pause(PAUSE);
-		}
-	}
+ /** Animation delay or pause time between ball moves */
+ private static final int DELAY = 50;
 
-	private void setup() {
-		GOval ball = new GOval(D, D);
-		ball.setFilled(true);
-		add(ball, D / 2, 100);
-	}
+ /** Initial X and Y location of ball */
+ private static final double X_START = DIAM_BALL / 2;
+ private static final double Y_START = 100;
 
-	private void moveBall() {
-		dy += GRAVITY;
-		ball.move(dx, dy);
-	}
+ /** X Velocity */
+ private static final double X_VEL = 5;
 
-	private void checkBall() {
-		if (ball.getY() > getHeight() - D) {
-			dy = -dy * GRAVITY;
-			double diff = ball.getY() - (getHeight() - D);
-			ball.move(0, -2 * diff);
-		}
-	}
+ /** Amount Y Velocity is reduced when it bounces */
+ private static final double BOUNCE_REDUCE = 0.9;
+
+ /** Starting X and Y Velocties */
+ private double xVel = X_VEL;
+ private double yVel = 0.0;
+
+ /* private instance variable */
+ private GOval ball;
+
+ public void run() {
+ setup();
+
+ // Simulation ends when ball goes off right hand
+ // end of screen
+ while (ball.getX() < getWidth()) {
+ moveBall();
+ checkForCollision();
+ pause(DELAY);
+ }
+ }
+
+
+ /** Create and place ball. */
+ private void setup() {
+ ball = new GOval(X_START, Y_START, DIAM_BALL, DIAM_BALL);
+ ball.setFilled(true);
+ add(ball);
+ }
+
+ /** Update and move ball */
+ private void moveBall() {
+ // increase yVelocity due to gravity on each cycle
+ yVel += GRAVITY;
+ ball.move(xVel,yVel);
+ }
+
+ /** Determine if collision with floor, update velocities
+ * and location as appropriate. */
+ private void checkForCollision() {
+ // determine if ball has dropped below the floor
+ if (ball.getY() > getHeight() - DIAM_BALL) {
+
+ // change ball's Y velocity to now bounce upwards
+ yVel = -yVel * BOUNCE_REDUCE;
+
+ // assume bounce will move ball an amount above the
+ // floor equal to the amount it would have dropped
+ // below the floor.
+ double diff = ball.getY() - (getHeight() - DIAM_BALL);
+ ball.move(0, -2 * diff);
+ }
+ }
 }
