c699826290a704c57b9f13e4968342f734405d1f
S <s>
2023-11-13 19:53:35 +0400

    {"types":"C","runs":35}

--- Practice.java
+++ Practice.java
@@ -121,9 +121,9 @@ public class Practice extends GraphicsProgram {
 	//  Add Paddle
 	GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
 	private void addPaddle() {
-		int paddleX = getWidth() / 2 - PADDLE_WIDTH / 2;
-		int paddleY = getHeight() - PADDLE_Y_OFFSET;
-		paddle.setLocation(paddleX, paddleY);
+//		int paddleX = getWidth() / 2 - PADDLE_WIDTH / 2;
+//		int paddleY = getHeight() - PADDLE_Y_OFFSET;
+//		paddle.setLocation(paddleX, paddleY);
 		paddle.setFilled(true);
 		add(paddle);
 	}
