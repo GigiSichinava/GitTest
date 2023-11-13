a07c8d6f1fcff503f29a12963a2aed413891f7a2
S <s>
2023-11-13 20:49:34 +0400

    {"types":"C","runs":60}

--- problem42.java
+++ problem42.java
@@ -2,11 +2,13 @@ import java.awt.event.MouseEvent;
 
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class problem42 extends GraphicsProgram {
 
 	private static final int D = 30;
-
+	private RandomGenerator rgen =  RandomGenerator.getInstance();
+	
 	GOval oval = new GOval (D, D);
 	public void run() {
 		addMouseListeners();
@@ -14,8 +16,11 @@ public class problem42 extends GraphicsProgram {
 	}
 
 	private void createBall() {
+		int x = rgen.nextInt(0, getWidth());
+		int y = rgen.nextInt(0, getHeight());
+		
 		oval.setFilled(true);
-		add(oval);
+		add(oval, x, y);
 	}
 
 	public void mouseClicked (MouseEvent e){
