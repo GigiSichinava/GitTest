4d4b72238b580d5923af4c6d328f8396aac40e84
S <s>
2023-11-13 20:32:58 +0400

    {"types":"C","runs":55}

--- problem42.java
+++ problem42.java
@@ -1,4 +1,28 @@
+import java.awt.event.MouseEvent;
 
-public class problem42 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+
+public class problem42 extends GraphicsProgram{
+
+	private static final int D = 30;
+	private static GOval oval;
+	
+	public void run(){
+		addMouseListeners();
+		createBall();
+	}
+
+	private void createBall() {
+		GOval oval = new GOval (D, D);
+		
+	}
+
+	
+	public void mouseClicked (MouseEvent e){
+		add(oval);
+		
+	}
+	
 }
