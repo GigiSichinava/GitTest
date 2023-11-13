c392cee7479b64145c4106d717a63e551601df11
S <s>
2023-11-13 21:10:25 +0400

    {"types":"C","runs":85}

--- Practice.java
+++ Practice.java
@@ -135,11 +135,7 @@ public class Practice extends GraphicsProgram {
 	
 	// mouseMoved 
 	public void mouseMoved(MouseEvent e) {
-		if(e.getX() < WIDTH / 2){
-		paddle.setLocation(e.getX() + PADDLE_WIDTH , getHeight() - PADDLE_Y_OFFSET);	
-	} else if (e.getX() > WIDTH / 2){
-		paddle.setLocation(e.getX() - PADDLE_WIDTH , getHeight() - PADDLE_Y_OFFSET);	
-	}
+		paddle.setLocation(e.getX() - PADDLE_WIDTH / 2, getHeight() - PADDLE_Y_OFFSET);	
 	}
 	
 
