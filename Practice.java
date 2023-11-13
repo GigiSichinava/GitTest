bfc44f2204641233c4028d33f29fe89d9331e6e3
S <s>
2023-11-13 19:05:29 +0400

    {"types":"C","runs":5}

--- Practice.java
+++ Practice.java
@@ -131,7 +131,7 @@ public class Practice extends GraphicsProgram {
 
 	// Application Size
 	private void applicationSize() {
-		setSize(WIDTH, HEIGHT);
+		setSize(APPLICATION_WIDTH,APPLICATION_HEIGHT);
 	}
 
 	// Add Bricks
