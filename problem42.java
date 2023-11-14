e5fdd3d5a7665e94c5f2cfd6ba122f977ab5d6aa
S <s>
2023-11-14 17:05:11 +0400

    {"types":"C","runs":106}

--- problem42.java
+++ problem42.java
@@ -20,21 +20,22 @@ public class problem42 extends GraphicsProgram {
 	}
 
 	private void createBall() {
+		pause(10);
 		int x = rgen.nextInt(0, getWidth() - D / 2);
 		int y = rgen.nextInt(0, getHeight() - D / 2);
 		oval.setFilled(true);
 		Color color = rgen.nextColor();
 		oval.setColor(color);
 		add(oval, x, y);
-		oval.move(vx, vy);
-		pause(10);
+		
+		
 	}
 
 	public void mouseClicked (MouseEvent e){
 		oval.setLocation(e.getX() - D / 2, e.getY() - D / 2);
 		Color color1 = rgen.nextColor();
 		oval.setColor(color1);
-
+		oval.move(vx, vy);
 	}
 
 }
