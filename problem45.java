a83a32a80937d71168c0b927d4c73f8680bdbc2f
S <s>
2023-11-14 17:52:10 +0400

    {"types":"C","runs":141}

--- problem45.java
+++ problem45.java
@@ -6,13 +6,14 @@ import acm.program.GraphicsProgram;
 
 public class problem45 extends GraphicsProgram {
 
-	GOval oval;
+	private GObject  lastPressed;
 	private static final int D = 50;
 	private int prevX;
 	private int prevY;
 
+	
+	GOval oval = new GOval (D, D);
 	public void run() {
-		oval = new GOval(D, D);
 		oval.setFilled(true);
 		add(oval, getWidth() / 2 - D / 2, getHeight() / 2 - D / 2);
 		addMouseListeners();
@@ -27,11 +28,11 @@ public class problem45 extends GraphicsProgram {
 	}
 
 	public void mouseDragged(MouseEvent e) {
-		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj != null) {
+//		GObject obj = getElementAt(e.getX(), e.getY());
+		if (lastPressed != null) {
 			
 			//move object
-			obj.move(e.getX() - prevX, e.getY() - prevY);
+			lastPressed.move(e.getX() - prevX, e.getY() - prevY);
 			
 			//updated info
 			prevX = e.getX();
