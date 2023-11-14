2cbcf4385c479f21328dbb78a80386ce0532b8dc
S <s>
2023-11-14 17:14:00 +0400

    {"types":"C","runs":118}

--- Test.java
+++ Test.java
@@ -1,8 +1,23 @@
-import acm.program.GraphicsProgram;
- 
-public class Test extends GraphicsProgram{
-	public void run(){
+import java.awt.event.MouseEvent;
 
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class Test extends GraphicsProgram {
+
+	private static final int R = 20;
+
+	public void run() {
+
+		addMouseListeners();
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		GOval brush = new GOval (R, R);
+		int x = e.getX() - R;
+		int y = e.getY()- R;
+		add(brush, x , y);
 		
 	}
 
