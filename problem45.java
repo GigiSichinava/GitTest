fceccf1fa208b66c16575e528f168a444710291e
S <s>
2023-11-14 17:40:33 +0400

    {"types":"C","runs":138}

--- problem45.java
+++ problem45.java
@@ -16,7 +16,7 @@ public class problem45 extends GraphicsProgram{
 	
 	public void mousePressed(MouseEvent e){
 		oval.setFilled(true);
-		add(oval, getWidth() - D/2, getHeight() - D/2);
+		add(oval, getWidth()/2 - D/2, getHeight()/2 - D/2);
 	}
 
 	public void mouseDragged(MouseEvent e){
