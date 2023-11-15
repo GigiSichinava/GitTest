5a8826492992c23875463cfa42552a587ce485c0
S <s>
2023-11-16 01:59:08 +0400

    {"types":"C","runs":209}

--- Review.java
+++ Review.java
@@ -11,16 +11,14 @@ public class Review extends GraphicsProgram {
 
 	public void run() {
 		addMouseListeners();
-		addBall();
-		moveBall();
-	}
+		
 
-	private void addBall() {
+	
 		GOval oval = new GOval(R * 2, R * 2);
 		oval.setFilled(true);
-	}
+	
 
-	private void moveBall() {
+	
 		
 		int vx = 0;
 		int vy = 5;
