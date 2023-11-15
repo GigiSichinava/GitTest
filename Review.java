9a4505412c0eff97df392cb65ea6f7ba7197471d
S <s>
2023-11-16 01:26:36 +0400

    {"types":"C","runs":163}

--- Review.java
+++ Review.java
@@ -1,4 +1,18 @@
+import java.awt.event.MouseEvent;
 
-public class Review {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class Review extends GraphicsProgram {
+
+	public void run() {
+
+		addMouseListeners();
+
+	}
+	
+	public void mouseClicked(MouseEvent e){
+		GOval oval = new GOval (10,10);
+		add(oval, getX(), getY());
+	}
 }
