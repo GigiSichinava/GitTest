8fb3f6581e3a30f751c4e5af3e6058969e1b1483
S <s>
2023-11-16 01:50:46 +0400

    {"types":"C","runs":194}

--- Review.java
+++ Review.java
@@ -5,7 +5,6 @@ import acm.program.GraphicsProgram;
 
 public class Review extends GraphicsProgram {
 
-	
 	private static final int R = 5;
 	private GOval oval;
 
@@ -14,7 +13,6 @@ public class Review extends GraphicsProgram {
 		moveBall();
 	}
 
-
 	private void addBall() {
 		addMouseListeners();
 		GOval oval = new GOval(R * 2, R * 2);
@@ -22,16 +20,20 @@ public class Review extends GraphicsProgram {
 	}
 
 	private void moveBall() {
+		pause(10);
 		int vx = 0;
 		int vy = 5;
-		oval.move(vx,vy);
-		if(oval.getY() >= getHeight()){
-		vy = -vy;
+
+		while (oval.getY() + 2 * R <= getHeight()) {
+			oval.move(vx,vy);
+			if (oval.getY() >= getHeight()) {
+				vy = -vy;
+			}
 		}
 	}
-	
-	public void MouseClicked(MouseEvent e){
+
+	public void MouseClicked(MouseEvent e) {
 		add(oval, e.getX() - R, e.getY() - R);
 	}
-	
+
 }
