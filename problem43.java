ea837bf47ff9898927738c1e7329ef5f0d24e105
S <s>
2023-11-16 01:37:33 +0400

    {"types":"C","runs":180}

--- problem43.java
+++ problem43.java
@@ -26,8 +26,6 @@ public class problem43 extends GraphicsProgram {
 	private void addBrush(int centerX, int centerY) {
 		GOval brush = new GOval(R, R);
 		brush.setFilled(true);
-		int x = centerX - R;
-		int y = centerY - R;
-		add(brush, x, y);
+		add(brush, centerX - R, centerY - R);
 	}
 }
