9222c481af03d25e3e8c5fb3be817249b171b109
S <s>
2023-11-16 00:20:02 +0400

    {"types":"C","runs":159}

--- problem45.java
+++ problem45.java
@@ -25,17 +25,17 @@ public class problem45 extends GraphicsProgram {
 			prevY = e.getY();
 		}
 	}
-
-	public void mouseDragged(MouseEvent e) {
-		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj != null) {
-			
-			//move object
-			obj.move(e.getX() - prevX, e.getY() - prevY);
-			
-			//updated values 
-			prevX = e.getX();
-			prevY = e.getY();
-		}
+//
+//	public void mouseDragged(MouseEvent e) {
+//		GObject obj = getElementAt(e.getX(), e.getY());
+//		if (obj != null) {
+//			
+//			//move object
+//			obj.move(e.getX() - prevX, e.getY() - prevY);
+//			
+//			//updated values 
+//			prevX = e.getX();
+//			prevY = e.getY();
+//		}
 	}
-}
+//}
