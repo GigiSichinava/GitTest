52205619694eac28dd5637fa7ad9e859d4d150e9
S <s>
2023-11-16 17:15:36 +0400

    {"types":"C","runs":254}

--- StingsPractice.java
+++ StingsPractice.java
@@ -4,10 +4,11 @@ public class StingsPractice extends ConsoleProgram{
 	public void run(){
 		
 		String str1 = "mari";
-		String str2 = "maki";
-		
+		String str2 = "mari";
+		String str3 =new String ("mari");
 		
 		println("1, 2 " + (str1 == str2) + " " +  str1.equals(str2));
+		println("1, 3" + (str1 == str3) + " " + str1.equals(str3));
 		
 		
 	}
