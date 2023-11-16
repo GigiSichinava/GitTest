05d3ad9affc86f20e8a01d9ba7d31d825876640d
S <s>
2023-11-17 01:17:05 +0400

    {"types":"C","runs":275}

--- StingsPractice.java
+++ StingsPractice.java
@@ -3,20 +3,20 @@ import acm.program.ConsoleProgram;
 public class StingsPractice extends ConsoleProgram {
 	public void run() {
 
-		 String text = readLine("Enter your text here: ");
-		 String symbolText = readLine("Enter symbol you are searching for: ");
-		
-		 char symbol = symbolText.charAt(0);
-		
-		 int numOccurances = 0;
-		 for (int i = 0; i < text.length(); i++) {
-		 if (text.charAt(i) == symbol) {
-		 numOccurances++;
-		
-		 }
-		 }
-		
-		 println(numOccurances);
-		
-		 }
+		String text = readLine("Enter your text here: ");
+		String symbolText = readLine("Enter symbol you are searching for: ");
+
+		char symbol = symbolText.charAt(0);
+
+		int numOccurances = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == symbol) {
+				numOccurances++;
+
+			}
+		}
+
+		println(numOccurances);
+
+	}
 }
