ae79a88f154e543964f31620759e3ddb9156231b
S <s>
2023-11-17 03:58:32 +0400

    {"types":"AR","runs":402}

--- Problem27.java
+++ Problem27.java
@@ -1,6 +1,6 @@
 import acm.program.ConsoleProgram;
 
-public class problem27 extends ConsoleProgram {
+public class Problem27 extends ConsoleProgram {
 	public void run(){
 		int n = readInt ("Enter n: ");
 		int m = readInt ("Enter m: ");
