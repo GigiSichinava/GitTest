1ed8d34ddb94c825f69596243501c2961e81e385
S <s>
2023-11-16 17:10:43 +0400

    {"types":"C","runs":250}

--- StingsPractice.java
+++ StingsPractice.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class StingsPractice {
+public class StingsPractice extends ConsoleProgram{
+	public void run(){
+		
+		String str1 = "mari";
+		String str2 = "mari";
+		
+		
+		println("1, 2 " + str1);
+		
+	}
 
 }
