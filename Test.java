8a01374b6912050f8db408b9a0c582855e7f85dd
S <s>
2023-11-17 02:14:33 +0400

    {"types":"C","runs":307}

--- Test.java
+++ Test.java
@@ -121,7 +121,7 @@ public class Test extends GraphicsProgram {
 	public void mouseMoved(MouseEvent e) {
 		double X = e.getX();
 		if (X >= PADDLE_WIDTH / 2) {
-			if (X <= APPLICATION_WIDTH - PADDLE_WIDTH / 2) {
+			if (X <= APPLICATION_WIDTH + PADDLE_WIDTH / 2) {
 				paddle.setLocation(X - PADDLE_WIDTH / 2,
 						APPLICATION_HEIGHT - PADDLE_HEIGHT - (double) (2.4 * PADDLE_Y_OFFSET));
 			}
