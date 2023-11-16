63853e44eb9e48c7681ccd36e4a5eb995e8ec9f4
S <s>
2023-11-17 01:36:49 +0400

    {"types":"C","runs":279}

--- Test.java
+++ Test.java
@@ -1,34 +1,71 @@
-import java.awt.event.MouseEvent;
 
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
+/*
+ * File: Breakout.java
+ * -------------------
+ * Name:
+ * Section Leader:
+ * 
+ * This file will eventually implement the game of Breakout.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Test extends GraphicsProgram {
 
-	private static final int R = 10;
+	/** Width and height of application window in pixels */
+	public static final int APPLICATION_WIDTH = 400;
+	public static final int APPLICATION_HEIGHT = 600;
 
+	/** Dimensions of game board (usually the same) */
+	private static final int WIDTH = APPLICATION_WIDTH;
+	private static final int HEIGHT = APPLICATION_HEIGHT;
+
+	/** Dimensions of the paddle */
+	private static final int PADDLE_WIDTH = 60;
+	private static final int PADDLE_HEIGHT = 10;
+
+	/** Offset of the paddle up from the bottom */
+	private static final int PADDLE_Y_OFFSET = 30;
+
+	/** Number of bricks per row */
+	private static final int NBRICKS_PER_ROW = 10;
+
+	/** Number of rows of bricks */
+	private static final int NBRICK_ROWS = 10;
+
+	/** Separation between bricks */
+	private static final int BRICK_SEP = 4;
+
+	/** Width of a brick */
+	private static final int BRICK_WIDTH = (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+
+	/** Height of a brick */
+	private static final int BRICK_HEIGHT = 8;
+
+	/** Radius of the ball in pixels */
+	private static final int BALL_RADIUS = 10;
+
+	/** Offset of the top brick row from the top */
+	private static final int BRICK_Y_OFFSET = 70;
+
+	/** Number of turns */
+	private static final int NTURNS = 3;
+
+	/* Method: run() */
+	/** Runs the Breakout program. */
 	public void run() {
+		applicationSize();
 
-		addMouseListeners();
-	}
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		GOval brush = new GOval (R, R);
-		int x = e.getX() - R/2;
-		int y = e.getY() - R/2;
-		add(brush, x , y);
 	}
 
-	public void mousePressed(MouseEvent e){
-			addBrush(e.getX() + R/2, e.getY() + R/2);
+	private void applicationSize() {
+		setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 	}
-	
-	
-	private void addBrush(int centerX, int centerY){
-		GOval brush = new GOval (R, R);
-		int x = centerX- R;
-		int y = centerY - R;
-		add(brush, x,y);
-	}
+
 }
