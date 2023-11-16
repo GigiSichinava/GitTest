2b4efae0d5b09c51ce9bf9eafe85c509e4a73745
S <s>
2023-11-17 02:07:41 +0400

    {"types":"C","runs":301}

--- random.java
+++ random.java
@@ -44,7 +44,7 @@ public class random extends GraphicsProgram {
 	private static final int BRICK_SEP = 4;
 
 	/** Width of a brick */
-	private static final int BRICK_WIDTH = (WIDTH - (NBRICKS_PER_ROW + 3) * BRICK_SEP) / NBRICKS_PER_ROW;
+	private static final int BRICK_WIDTH = (WIDTH - ((NBRICKS_PER_ROW - 1) * BRICK_SEP)) / NBRICKS_PER_ROW;
 
 	/** Height of a brick */
 	private static final int BRICK_HEIGHT = 8;
