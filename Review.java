2ae45c2a366efbba967b73f810b343b361c61231
S <s>
2023-11-17 14:52:05 +0400

    {"types":"C","runs":3}

--- Review.java
+++ Review.java
@@ -4,10 +4,28 @@ import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 import javafx.scene.input.MouseEvent;
 
-public class Review extends GraphicsProgram {
-
-	
-		
+public class Review extends ConsoleProgram {
+	private int countCharacters(String string, char c) {
+		int count = 0;
+		for (int i = 0; i < string.charAt(i); i++) {
+			if (string.charAt(i) == c) {
+				count++;
+			}
+		}
+		return count;
 	}
 
+	public void run() {
+		String text = readLine("Enter your text here ");
+		int maxCount = 0;
+		char maxCharcater = 0;
+		for (char c = 'a'; c <= 'z'; c++) {
+			int count = countCharacters(text, c);
+			if (count > maxCount) {
+				maxCount = count;
+				maxCharcater = c;
+			}
+		}
+		println(maxCharcater);
+	}
 }
