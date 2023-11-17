1f2424a718712b449754a893fba88eac9e4ac5b9
S <s>
2023-11-17 12:49:44 +0400

    {"types":"C","runs":406}

--- Review.java
+++ Review.java
@@ -7,24 +7,20 @@ import javafx.scene.input.MouseEvent;
 public class Review extends GraphicsProgram {
 
 	private static final int R = 20;
-	GOval oval = new GOval (R, R);
-	
-	public void run(){
+	GOval oval = new GOval(R, R);
+
+	public void run() {
 		addMouseListeners();
-		GOval oval = new GOval (R, R);
+		GOval oval = new GOval(R, R);
 		oval.setFilled(true);
-		add(oval);		
+		add(oval);
 	}
 
 	public void mouseDragged(MouseEvent e){
-		if(getElementAt(e.getX(), e.getY() == oval){
-		oval.setLocation(e.getX() - x, e.getY() - y)	
+		if(getElementAt(e.getX(), e.getY()) == oval){
+		oval.setLocation(e.getX() - R, e.getY() - R);	
 		}
 		
 	}
-	
-	public void mousePressed(MouseEvent e){
-		
-	}
-	
+
 }
