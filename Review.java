7dca0b34d445122a0bb3becfc2ba45c4a702bad0
S <s>
2023-11-17 14:55:08 +0400

    {"types":"C","runs":7}

--- Review.java
+++ Review.java
@@ -1,8 +1,4 @@
-
-import acm.graphics.GOval;
 import acm.program.ConsoleProgram;
-import acm.program.GraphicsProgram;
-import javafx.scene.input.MouseEvent;
 
 public class Review extends ConsoleProgram {
 	private int countCharacters(String string, char c) {
