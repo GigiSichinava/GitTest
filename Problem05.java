ed29491661c7833f07a70ea24d8184c8815d4ef6
S <s>
2023-11-17 04:01:36 +0400

    {"types":"AR","runs":402}

--- Problem05.java
+++ Problem05.java
@@ -1,6 +1,6 @@
 	import stanford.karel.Karel;
 
-	public class problem05 extends Karel {
+	public class Problem05 extends Karel {
 		public void run(){
 			while (frontIsClear()){
 			if (beepersPresent()) {
