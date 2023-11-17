e8f7c99ec6e3e64a3f2a4980c0e7b86edb82f4b7
S <s>
2023-11-17 15:26:17 +0400

    {"types":"AR","runs":23}

--- Assigment1n1.java
+++ Assigment1n1.java
@@ -1,6 +1,6 @@
 import stanford.karel.SuperKarel;
 
-public class assigment1n1 extends SuperKarel {
+public class Assigment1n1 extends SuperKarel {
 	public void run(){
 		goToNewspaper();
 		takeNewspaper();
