646f00f515f1a3734ea3d8e03521e9dd22fe13e9
S <s>
2023-11-19 02:35:07 +0400

    {"types":"C","runs":159}

--- Review.java
+++ Review.java
@@ -7,8 +7,8 @@ public class Review extends GraphicsProgram {
 	
 	private static final int R = 50;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private int randomX = rgen.nextInt(0, getWidth());
-	private int randomY = rgen.nextInt(0, getHeight());
+	private int randomX = rgen.nextInt(R, getWidth());
+	private int randomY = rgen.nextInt(R, getHeight());
 	
 	public void run() {
 		
