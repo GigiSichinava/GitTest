4ca644f69270f802e4046083c3c76ce9874d336e
S <s>
2023-11-19 01:20:45 +0400

    {"types":"C","runs":108}

--- Test.java
+++ Test.java
@@ -168,7 +168,7 @@ public class Test extends GraphicsProgram {
 					lost.setColor(Color.red);
 					add(lost);
 				} else {
-					ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
+//					ball = new GOval(2 * BALL_RADIUS, 2 * BALL_RADIUS);
 					ball.setFilled(true);
 					ball.setColor(rgen.nextColor());
 					add(ball, getWidth() / 2 - BALL_RADIUS, getHeight() / 2 - BALL_RADIUS);
