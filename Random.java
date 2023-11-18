43f8ebe01e05fcaf1aea706f5cfff6f5f55916c0
S <s>
2023-11-18 18:30:22 +0400

    {"types":"C","runs":67}

--- Random.java
+++ Random.java
@@ -71,7 +71,6 @@ public class Random extends GraphicsProgram {
 	private static int yball;
 
 	public void run() {
-		setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 		setupGame();
 		paddle = new GRect(0, APPLICATION_HEIGHT - 3 * PADDLE_Y_OFFSET, PADDLE_WIDTH, PADDLE_HEIGHT);
 		paddle.setFilled(true);
