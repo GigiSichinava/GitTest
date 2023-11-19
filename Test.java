8de094cf0713281764a0ae90783dc0ed2978f0be
S <s>
2023-11-19 12:37:02 +0400

    {"types":"C","runs":220}

--- Test.java
+++ Test.java
@@ -196,8 +196,7 @@ public class Test extends GraphicsProgram {
 				// when touching object is paddle
 				if (collider == paddle) {
 					vy = -vy;
-					if (xball <= paddle.getX() + paddle.getHeight())
-						vx = -vx;
+					
 					if (xball >= paddle.getX() + paddle.getHeight() + (paddle.getWidth() / 2))
 						vx = -vx;
 				} else {
