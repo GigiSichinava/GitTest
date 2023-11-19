517b4cce3c20a462287ad4523921af856d89db64
S <s>
2023-11-19 18:22:05 +0400

    {"types":"C","runs":269}

--- Problem45Strings.java
+++ Problem45Strings.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Problem45Strings {
+public class Problem45Strings extends ConsoleProgram{
 
+	public void run(){
+		countSymbol(); 
+	}
+
+	private void countSymbol() {
+		String text = readLine ("Enter your text here: ");
+		String symbolInText = readLine ("Enter text that you are looking for: ");
+		
+		char symbol = symbolInText.charAt(0);
+		
+		int numberOfOccurances = 0;
+		for (int i = 0; i < text.length(); i++){
+			if(text.charAt(i) == symbol) {
+				numberOfOccurances++;
+				
+			}
+		}
+		println(numberOfOccurances);
+	}
 }
