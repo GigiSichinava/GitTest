a116a9b32f292c877f25bd89475c5ab9541ee195
S <s>
2023-11-21 23:36:49 +0400

    {"types":"C","runs":468}

--- Practice.java
+++ Practice.java
@@ -10,8 +10,8 @@ public class Practice extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int X = 100;
 	private GRect rect = new GRect(X, X);
-	int rectX = rgen.nextInt(0, 130);
-	int rectY = rgen.nextInt(0, 100);
+	int rectX = rgen.nextInt(0, 750);
+	int rectY = rgen.nextInt(0, 750);
 
 	public void run() {
 
