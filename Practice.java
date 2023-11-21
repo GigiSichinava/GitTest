30de55f05666b8e0b3d20a1427eff5fb09e5781e
S <s>
2023-11-22 02:12:07 +0400

    {"types":"C","runs":7}

--- Practice.java
+++ Practice.java
@@ -3,21 +3,16 @@ import acm.program.GraphicsProgram;
 import javafx.scene.input.MouseEvent;
 
 public class Practice extends GraphicsProgram {
-	
+
 	private static final int R = 50;
-	private GOval brush = new GOval (R, R);
-	
+	private GOval brush = new GOval(R, R);
+
 	public void run() {
 		addMouseListeners();
 	}
 
-	public void mousePressed(MouseEvent e){
+	public void mousePressed(MouseEvent e) {
 		add(brush, e.getX(), e.getY());
-		
 	}
 
-	private void addBrush(){
-		GOval brush = new GOval (R, R);				
-	}
-	
 }
