5afd247a432663f82c02fbddd4b8f11d714f956a
S <s>
2023-11-22 01:38:21 +0400

    {"types":"C","runs":491}

--- Practice.java
+++ Practice.java
@@ -4,7 +4,7 @@ import acm.program.GraphicsProgram;
 public class Practice extends GraphicsProgram{
 	public void run(){
 		
-		GRect rect = new GRect (10, 10, 10, 10,);
+		GRect rect = new GRect (10, 10, 10, 10);
 		add(rect);
 	}
 }
