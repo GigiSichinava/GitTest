9a9f9555e1d1c776d549b350401bb37584e53b21
S <s>
2023-11-22 01:54:00 +0400

    {"types":"C","runs":1}

--- Practice.java
+++ Practice.java
@@ -1,10 +1,21 @@
+import java.awt.event.MouseEvent;
+
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
-public class Practice extends GraphicsProgram{
-	public void run(){
-		
-		GRect rect = new GRect (10, 10, 10, 10);
+public class Practice extends GraphicsProgram {
+
+	private GRect rect = new GRect(10, 10, 10, 10);
+
+	public void run() {
+
+		GRect rect = new GRect(10, 10, 10, 10);
 		add(rect);
+		addMouseListeners();
 	}
+
+	public void mouseClicked(MouseEvent e){
+		add(rect, e.getX(), e.getY());
+	}
+
 }
