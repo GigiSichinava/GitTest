8f4d70decbaa8ca490c3c3d0129d2113134fc218
S <s>
2023-11-21 23:35:08 +0400

    {"types":"C","runs":459}

--- Practice.java
+++ Practice.java
@@ -10,15 +10,21 @@ public class Practice extends GraphicsProgram {
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	private static final int X = 100;
 	private GRect rect = new GRect(X, X);
+	int rectX = rgen.nextInt(0, getWidth() - X / 2);
+	int rectY = rgen.nextInt(0, getHeight() - X / 2);
 
 	public void run() {
 
-		int rectX = rgen.nextInt(0, getWidth() - X/2);
-		int rectY = rgen.nextInt(0, getHeight() - X/2);
 		add(rect, rectX, rectY);
 		rect.setFilled(true);
 		rect.setColor(rgen.nextColor());
 		addMouseListeners();
+	}
+
+	private void addRect() {
+		add(rect, rectX, rectY);
+		rect.setFilled(true);
+		rect.setColor(rgen.nextColor());
 	}
 
 	public void mouseClicked(MouseEvent e) {
