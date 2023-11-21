4d722e62241ae4f9244e2d1c397263e97e09cf11
S <s>
2023-11-21 22:26:36 +0400

    {"types":"C","runs":424}

--- Practice.java
+++ Practice.java
@@ -1,181 +1,24 @@
+import java.awt.event.MouseEvent;
 
-/*
- * File: Breakout.java
- * -------------------
- * Name:
- * Section Leader:
- * 
- * This file will eventually implement the game of Breakout.
- */
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-import acm.graphics.*;
-import acm.program.*;
-import acm.util.*;
+class Practice extends GraphicsProgram {
 
-import java.applet.*;
-import java.awt.*;
-import java.awt.event.*;
-
-public class Practice extends GraphicsProgram {
-
-	/** Width and height of application window in pixels */
-	public static final int APPLICATION_WIDTH = 400;
-	public static final int APPLICATION_HEIGHT = 600;
-
-	/** Dimensions of game board (usually the same) */
-	private static final int WIDTH = APPLICATION_WIDTH;
-	private static final int HEIGHT = APPLICATION_HEIGHT;
-
-	/** Dimensions of the paddle */
-	private static final int PADDLE_WIDTH = 60;
-	private static final int PADDLE_HEIGHT = 10;
-
-	/** Offset of the paddle up from the bottom */
-	private static final int PADDLE_Y_OFFSET = 30;
-
-	/** Number of bricks per row */
-	private static final int NBRICKS_PER_ROW = 10;
-
-	/** Number of rows of bricks */
-	private static final int NBRICK_ROWS = 10;
-
-	/** Separation between bricks */
-	private static final int BRICK_SEP = 4;
-
-	/** Width of a brick */
-	private static final int BRICK_WIDTH = (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
-
-	/** Height of a brick */
-	private static final int BRICK_HEIGHT = 8;
-
-	/** Radius of the ball in pixels */
-	private static final int BALL_RADIUS = 10;
-
-	/** Offset of the top brick row from the top */
-	private static final int BRICK_Y_OFFSET = 70;
-
-	/** Number of turns */
-	private static final int NTURNS = 3;
-
-	
-	private static int counter = 0;
-	GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
-	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private GRect rect = new GRect(100, 100, 100, 100);
 
 	public void run() {
+
+		GRect rect = new GRect(100, 100, 100, 100);
+		rect.setFilled(true);
+		add(rect);
 		addMouseListeners();
-		addBricks();
-		addPaddle();
-		addBall();
-		// getCollindingObject();
 	}
 
-	
-	
-	
-//	private GObject getCollidingObject() {
-//		double x = oval.getX();
-//		double y = oval.getY();
-//		if (getElementAt(x, y) != null) {
-//			return getElementAt(x, y);
-//		} else if (getElementAt(x, y + BALL_RADIUS * 2) != null) {
-//			return getElementAt(x, y + BALL_RADIUS * 2);
-//		} else if (getElementAt(x + BALL_RADIUS * 2, y + BALL_RADIUS * 2) != null) {
-//			return getElementAt(x + BALL_RADIUS * 2, y + BALL_RADIUS * 2);
-//		} else if (getElementAt(x + BALL_RADIUS * 2, y) != null) {
-//			return getElementAt(x + BALL_RADIUS * 2, y);
-//		} else {
-//			return null;
-//		}
-//	}
-
-	// Add Ball 
-	private double vx = rgen.nextDouble(1.0, 3.0);
-	private double vy = 3.0;
-
-	private void addBall() {
-		if (rgen.nextBoolean(0.5))
-			vx = -vx;
-		GOval oval = new GOval(BALL_RADIUS * 2, BALL_RADIUS * 2);
-		add(oval, getWidth() / 2 - BALL_RADIUS, getHeight() / 2 - BALL_RADIUS);
-		double dx = vx;
-		double dy = vy;
-
-		while (true) {
-			oval.move(dx, dy);
-			pause(30);
-
-			if (oval.getY() >= getHeight() - BALL_RADIUS * 2) {
-				dy = -dy;
-			}
-
-			if (oval.getX() > getWidth() - BALL_RADIUS * 2) {
-				dx = -dx;
-			}
-
-			if (oval.getY() <= 0) {
-				dy = -dy;
-			}
-
-			if (oval.getX() <= 0) {
-				dx = -dx;
-			}
+	public void mouseClicked(MouseEvent e){
+		if (getElementAt(e.getX(), e.getY()) == rect){
+			remove(rect);
 		}
+		
 	}
-
-	//  Add Paddle
-	private void addPaddle() {
-		int paddleX = getWidth() / 2 - PADDLE_WIDTH / 2;
-		int paddleY = getHeight() - PADDLE_Y_OFFSET;
-		paddle.setLocation(paddleX, paddleY);
-		paddle.setFilled(true);
-		add(paddle);
-	}
-	
-	// mouseMoved 
-	public void mouseMoved(MouseEvent e) {
-		double x = e.getX();
-		paddle.setLocation(e.getX() - PADDLE_WIDTH / 2, getHeight() - PADDLE_Y_OFFSET);	
-	}
-	
-
-	// Add Bricks
-	double startingX = BRICK_SEP / 2;
-	double startingY = BRICK_Y_OFFSET;
-
-	private void addBricks() {
-		for (int rowNumber = 0; rowNumber < NBRICK_ROWS; rowNumber++) {
-			for (int bricksNumber = 0; bricksNumber < NBRICKS_PER_ROW; bricksNumber++) {
-				GRect rect = new GRect(startingX + (BRICK_WIDTH + BRICK_SEP) * bricksNumber,
-						startingY + BRICK_HEIGHT * rowNumber, BRICK_WIDTH, BRICK_HEIGHT);
-				counter++;
-				rect.setFilled(false);
-				add(rect);
-				if (rowNumber < 2) {
-					rect.setFilled(true);
-					rect.setColor(Color.RED);
-				}
-				if (rowNumber < 4 && rowNumber > 1) {
-					rect.setFilled(true);
-					rect.setColor(Color.ORANGE);
-				}
-				if (rowNumber < 6 && rowNumber > 3) {
-					rect.setFilled(true);
-					rect.setColor(Color.YELLOW);
-				}
-				if (rowNumber < 8 && rowNumber > 5) {
-					rect.setFilled(true);
-					rect.setColor(Color.GREEN);
-				}
-				if (rowNumber < 10 && rowNumber > 7) {
-					rect.setFilled(true);
-					rect.setColor(Color.CYAN);
-				}
-			}
-			startingY = startingY + BRICK_SEP;
-		}
-	}
-
-	
-
 }
