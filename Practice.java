3ffff80f54a5b08c0b1c9d08231e303e45079c61
S <s>
2023-11-21 23:19:38 +0400

    {"types":"C","runs":446}

--- Practice.java
+++ Practice.java
@@ -6,19 +6,19 @@ import acm.program.GraphicsProgram;
 
 public class Practice extends GraphicsProgram {
 
+	private static final int X = 100; 
 	private GRect rect = new GRect(100, 100, 100, 100);
 
 	public void run() {
 
-		GRect rect = new GRect(100, 100, 100, 100);
+		GRect rect = new GRect(X, X, 100, 100);
 		add(rect);
 		rect.setFilled(true);
 		addMouseListeners();
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj == rect) {
+		if(getElementAt(100, 100) == rect){
 			remove(rect);
 		}
 
