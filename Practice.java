21c808634bb7e083341582a69507ace2d0c6c9a8
S <s>
2023-11-21 23:44:41 +0400

    {"types":"C","runs":478}

--- Practice.java
+++ Practice.java
@@ -21,11 +21,11 @@ public class Practice extends GraphicsProgram {
 		addMouseListeners();
 	}
 
-	private void addRect() {
-		add(rect, rectX, rectY);
-		rect.setFilled(true);
-		rect.setColor(rgen.nextColor());
-	}
+//	private void addRect() {
+//		add(rect, rectX, rectY);
+//		rect.setFilled(true);
+//		rect.setColor(rgen.nextColor());
+//	}
 
 	public void mouseClicked(MouseEvent e) {
 		GObject obj = getElementAt(e.getX(), e.getY());
