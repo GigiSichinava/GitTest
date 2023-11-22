b78d5084ead4dabccb649902077c21de12ac7f45
S <s>
2023-11-22 11:30:59 +0400

    {"types":"R","runs":1}

--- HPtest.java
+++
@@ -1,9 +0,0 @@
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
-
-public class HPtest extends GraphicsProgram{
-	public void run(){
-		GOval oval = new GOval (10,10,10,10);
-		add(oval);
-	}
-}
