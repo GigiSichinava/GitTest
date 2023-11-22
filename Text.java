fdd4b1ec820b074ff4afe0eedfe993c733c5d893
S <s>
2023-11-22 23:17:15 +0400

    {"types":"C","runs":1}

--- Text.java
+++ Text.java
@@ -1,4 +1,13 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-public class Text {
+public class Text extends GraphicsProgram{
+	public void run(){
+		
+		GOval oval = new GOval(20,20, 20, 30);
+		add(oval);
+		
+	}
+	
 
 }
