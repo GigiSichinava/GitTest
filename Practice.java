54d0b3514589df9b20f74e21c28b51a891bebb21
S <s>
2023-11-23 18:38:26 +0400

    {"types":"C","runs":8}

--- Practice.java
+++ Practice.java
@@ -6,13 +6,15 @@ import acm.program.GraphicsProgram;
 public class Practice extends GraphicsProgram {
 
 	private static final int R = 100;
-	private GOval oval = new GOval(2 * R, 2 * R);
+	//private GOval oval = new GOval(2 * R, 2 * R);
 
 	public void run() {
 		addMouseListeners();
 	}
 
 	public void mouseClicked(MouseEvent e) {
+		GOval oval = new GOval(2 * R, 2 * R);
+
 		oval.setLocation(e.getX() - R, e.getY() - R);
 	}
 }
