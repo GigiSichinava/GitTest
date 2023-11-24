6358a9ab3cce22d45c9ee3965894adf8c7209418
S <s>
2023-11-24 12:36:25 +0400

    {"types":"C","runs":1}

--- Problem51.java
+++ Problem51.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Problem51 {
+public class Problem51 extends ConsoleProgram {
+	public void run(){
+			String s = readLine();
+			println(deleteCharacters(s));
+
+	}
+		
+		
+		private String deleteCharacters(String string) {
+		String result = "";
+		for (int i = 0; i < string.length(); i++) {
+			if (i == 0 || string.charAt(i) != string.charAt(i - 1)) {
+				result += string.charAt(i);
+			}
+		}
+		return result;
+	}
 
 }
