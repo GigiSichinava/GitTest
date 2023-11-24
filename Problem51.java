341c19990654a70ab0669295bf95cc5e0e44e5ec
S <s>
2023-11-24 12:47:48 +0400

    {"types":"C","runs":2}

--- Problem51.java
+++ Problem51.java
@@ -1,14 +1,15 @@
 import acm.program.ConsoleProgram;
 
 public class Problem51 extends ConsoleProgram {
-	public void run(){
-			String s = readLine();
-			println(deleteCharacters(s));
+	public void run() {
+		String s = readLine();
+		println(deleteCharacters(s));
 
+		String a = readLine();
+		String b = readLine();
 	}
-		
-		
-		private String deleteCharacters(String string) {
+
+	private String deleteCharacters(String string) {
 		String result = "";
 		for (int i = 0; i < string.length(); i++) {
 			if (i == 0 || string.charAt(i) != string.charAt(i - 1)) {
@@ -16,6 +17,15 @@ public class Problem51 extends ConsoleProgram {
 			}
 		}
 		return result;
+	}
+
+	private boolean contains(String a, String b) {
+		for (int i = 0; i < a.length() - b.length(); i++) {
+			if (b.equals(a.substring(i, i + b.length()))) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
