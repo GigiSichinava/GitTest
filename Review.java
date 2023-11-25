3eb0ab4cf50093cd328c541e84e703bd37f8bcb1
S <s>
2023-11-25 14:21:48 +0400

    {"types":"A","runs":19}

---
+++ Review.java
@@ -0,0 +1,16 @@
+import acm.program.ConsoleProgram;
+
+public class Review extends ConsoleProgram {
+    public void run() {
+        String s = readLine("Enter your text here: ");
+        for (int i = 0; i < s.length(); i++) {
+            char left = s.charAt(i);
+            char right = s.charAt(s.length() - i - 1);
+            if (left != right) {
+                println("Negative");
+                return;
+            }
+        }
+        println("Positive");
+    }
+}
