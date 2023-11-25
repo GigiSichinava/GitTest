08fc33cbcf16407aed4a4972a2b13f8048f53f54
S <s>
2023-11-25 15:02:25 +0400

    {"types":"C","runs":28}

--- Review.java
+++ Review.java
@@ -7,13 +7,16 @@ public class Review extends ConsoleProgram {
         
         for (int i = 0; i < text.length(); i++) {
             char ch = text.charAt(i);
+            
             if (!used.contains("" + ch)) {
                 int amount = 0;
+                
                 for (int j = 0; j < text.length(); j++) {
                     if (ch == text.charAt(j)) {
                         amount++;
                     }
                 }
+                
                 println(ch + " : " + amount);
                 used += ch;
             }
