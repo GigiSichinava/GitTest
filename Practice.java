5cb23ccaeeaef31b43bd42e4bd6a940e56dc07ba
S <s>
2023-11-25 13:35:45 +0400

    {"types":"C","runs":8}

--- Practice.java
+++ Practice.java
@@ -1,20 +1,19 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
-
-		String text = readLine();
-		String symbolintext = readLine();
-
-		char symbol = symbolintext.charAt(0);
-
-		int numberofoccurances = 0;
-
-		for (int i = 0; i < text.length(); i++) {
-			if (text.charAt(i) == symbol) {
-				numberofoccurances++;
-			}
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
+		String text = readLine("Enter your text here: ");
+		String symbolInText = readLine("Symbol you are looking for: ");
+		
+		char ch = symbolInText.charAt(0);
+		int numberOfSymbols = 0;
+		
+		for(int i = 0; i < text.length(); i++){
+			if(text.charAt(i) == ch);
+			numberOfSymbols++;
 		}
-		println(numberofoccurances);
+		println(numberOfSymbols);
+		
 	}
 }
