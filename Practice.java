f4fca4f46c788844684698844eae687fec501490
S <s>
2023-11-25 21:40:54 +0400

    {"types":"C","runs":51}

--- Practice.java
+++ Practice.java
@@ -5,8 +5,8 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram {
 	public void run(){
 		
-		int n = readInt();
-		for(int i = 0; i < 0; i++){
+		double n = readDouble();
+		for(int i = 0; i < n; i++){
 			println("hello" + i);
 		}
 	}
