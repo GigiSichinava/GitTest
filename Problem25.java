ecdaf98afe068f5d69775fd43e2127475eed76cd
S <s>
2023-11-25 22:33:50 +0400

    {"types":"C","runs":92}

--- Problem25.java
+++ Problem25.java
@@ -18,3 +18,24 @@ public class Problem25 extends ConsoleProgram{
 		return -1;
 	}
 }
+
+
+
+//	public class Problem25 extends ConsoleProgram {
+//	    public void run() {
+//	        int n = readInt("Enter n: ");
+//	        int m = readInt("Enter m: ");
+//	
+//	        int gcd = gcd(n, m);
+//	        println("GCD is: " + gcd);
+//	    }
+//	
+//	    private int gcd(int n, int m) {
+//	        while (m != 0) {
+//	            int temp = m;
+//	            m = n % m;
+//	            n = temp;
+//	        }
+//	        return n;
+//	    }
+//	}
