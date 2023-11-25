976c9f74649b26ed1a9d7968d03551085bfbfb11
S <s>
2023-11-25 21:57:07 +0400

    {"types":"C","runs":61}

--- Practice.java
+++ Practice.java
@@ -1,10 +1,17 @@
-import acm.program.ConsoleProgram;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run(){
-	
+public class Practice extends GraphicsProgram {
+	public void run() {
+
+		int x = getWidth() / 8;
+		int y = getHeight() / 8;
+		GRect rect = new GRect(x, y);
 		
-		
-		
+		for (int rows = 0; rows < 8; rows++){
+			for(int colls = 0; colls < 8; colls++){
+				add(rect, x * rows, y);
+			}
+		}
 	}
 }
