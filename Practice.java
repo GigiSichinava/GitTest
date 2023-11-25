d8ea172272be4939325f5a063bc880a0c93cf34f
S <s>
2023-11-25 22:10:28 +0400

    {"types":"C","runs":68}

--- Practice.java
+++ Practice.java
@@ -1,30 +1,14 @@
-import acm.graphics.GRect;
-import acm.program.GraphicsProgram;
+import acm.program.ConsoleProgram;
 
-public class Practice extends GraphicsProgram {
-	
-	private static final int NUMBER_OF_ROWS = 8;
-	private static final int NUMBER_OF_COLUMNS = 8;
-
-	
-	public void run() {
-		int rectSize = getHeight() / NUMBER_OF_ROWS;
-		for(int i = 0; i < NUMBER_OF_ROWS; i++){
-			for(int j = 0; j < NUMBER_OF_ROWS; j++){
-				double x = i * rectSize;
-				double y = j * rectSize;
-				GRect rect = new GRect (x, y, rectSize, rectSize);
-				int c = i + j;
-				if (c % 2 == 0){
-					rect.setFilled(false);
-				} else {
-					rect.setFilled(true);
-					
-				}
-				add(rect);
-			}
-		}
-
+public class Practice extends ConsoleProgram{
+	public void run(){
 		
+		double a = readDouble();
+		int result = 0;
+		for(int i = 0; i < a; i++){
+			result += i;
+			
+		}
+		println(result);
 	}
 }
