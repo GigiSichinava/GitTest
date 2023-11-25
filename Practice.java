748d6f7406a7723be54b5e040af232966965473c
S <s>
2023-11-25 11:32:11 +0400

    {"types":"C","runs":1}

--- Practice.java
+++ Practice.java
@@ -1,23 +1,10 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
+// strings 101
 
-		String str = readLine();
-		String a = readLine();
-		String b = readLine();
+public class Practice extends ConsoleProgram{
+	public void run(){
+		String string = "beng biwo!";
+		println(string);
 	}
-
-	private void contains(String a, String b) {
-		String result = " ";
-
-		for (int i = 0; i < a.length(); i++) {
-			char ch = a.charAt(i);
-			if(ch == b.charAt(0)){
-				result = result + 1;
-			}
-		}
-		
-	}
-
 }
