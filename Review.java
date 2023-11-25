734c0709a726c3187b87918a75b45e3965814e2b
S <s>
2023-11-25 15:13:59 +0400

    {"types":"C","runs":30}

--- Review.java
+++ Review.java
@@ -1,25 +1,18 @@
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram {
-    public void run() {
-        String text = readLine("Enter Text: ");
-        String used = "";
-        
-        for (int i = 0; i < text.length(); i++) {
-            char ch = text.charAt(i);
-            
-            if (!used.contains("" + ch)) {
-                int amount = 0;
-                
-                for (int j = 0; j < text.length(); j++) {
-                    if (ch == text.charAt(j)) {
-                        amount++;
-                    }
-                }
-                
-                println(ch + " : " + amount);
-                used += ch;
-            }
-        }
-    }
+public class Review extends ConsoleProgram{
+	public void run(){
+		String num = readLine("Enter Number: ");
+		int ans = stringToInteger(num);
+		print(ans);
+	}
+
+	private int stringToInteger(String num) {
+		int res = 0;
+		for(int i = 0; i < num.length(); i++){
+			int n = num.charAt(i) - '0';
+			res = res * 10 + n;
+		}
+		return res;
+	}
 }
