96ff8bf76cf4f39b7a9d051e6f64bafe18d7ef82
S <s>
2023-11-25 12:05:26 +0400

    {"types":"C","runs":2}

--- Practice.java
+++ Practice.java
@@ -2,9 +2,17 @@ import acm.program.ConsoleProgram;
 
 // strings 101
 
-public class Practice extends ConsoleProgram{
-	public void run(){
-		String string = "beng biwo!";
-		println(string);
+public class Practice extends ConsoleProgram {
+	public void run() {
+		String str = "beng biwo";
+	}
+		
+
+	private String reveresdString(String str) {
+		String result = " ";
+		for (int i = 0; i < str.length(); i++) {
+			result = str.charAt(i) + result;
+		}
+		return result;
 	}
 }
