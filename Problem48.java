6386b102745e7e431670dd00ba677dc09b53afcc
S <s>
2023-11-25 15:12:22 +0400

    {"types":"C","runs":29}

--- Problem48.java
+++ Problem48.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Problem48 {
+public class Problem48 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		String used = "";
 
+		for (int i = 0; i < text.length(); i++) {
+			char ch = text.charAt(i);
+
+			if (!used.contains("" + ch)) {
+				int amount = 0;
+				
+				for(int j = 0; j < text.length(); j++){
+					if(ch == text.charAt(j)){
+						amount++;
+					}
+				}
+				println(ch + " : " + amount);
+				used = used + ch;
+			}
+		}
+	}
 }
