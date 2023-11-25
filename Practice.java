00f43b94b79cd75f4c685b1470180f11d2c51bae
S <s>
2023-11-25 12:50:48 +0400

    {"types":"C","runs":6}

--- Practice.java
+++ Practice.java
@@ -1,20 +1,20 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	public void run(){
-		
-		String str = "beng biwo";
-		println(str);
-		
-		String reversedStr = reverseString(str);
-		println(reversedStr);
-	}
-	
-	private String reverseString(String str){
-		String result = " ";
-		for(int i = 0; i < str.length(); i++){
-			result = str.charAt(i) + result;
+public class Practice extends ConsoleProgram {
+	public void run() {
+
+		String text = readLine();
+		String symbolintext = readLine();
+
+		char symbol = text.charAt(0);
+
+		int numberofoccurances = 0;
+
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == symbol) {
+				numberofoccurances++;
+			}
 		}
-		return result;
+		println(numberofoccurances);
 	}
 }
