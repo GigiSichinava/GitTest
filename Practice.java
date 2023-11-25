5c3c6269a4c98723ba7dccc671f41ffc5b90c0e0
S <s>
2023-11-25 14:14:43 +0400

    {"types":"C","runs":15}

--- Practice.java
+++ Practice.java
@@ -2,24 +2,15 @@ import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram {
     public void run() {
-        String text = readLine("Enter your text here: ");
-        int maxCount = 0;
-        char maxCharacter = 0;
-
-        for (char c = 'a'; c <= 'z'; c++) {
-            int count = 0;
-            for (int i = 0; i < text.length(); i++) {
-                if (text.charAt(i) == c) {
-                    count++;
-                }
-            }
-
-            if (count > maxCount) {
-                maxCount = count;
-                maxCharacter = c;
+        String s = readLine("Enter your text here: ");
+        for (int i = 0; i < s.length(); i++) {
+            char left = s.charAt(i);
+            char right = s.charAt(s.length() - i - 1);
+            if (left != right) {
+                println("Negative");
+                return;
             }
         }
-
-        println(maxCharacter);
+        println("Positive");
     }
 }
