0e28ba56010d50ccbf2898d015c750432dd2ed08
S <s>
2023-11-26 16:10:34 +0400

    {"types":"C","runs":48}

--- LinesMidterm.java
+++ LinesMidterm.java
@@ -1,4 +1,33 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
+import java.awt.event.MouseEvent;
 
-public class LinesMidterm {
+public class LinesMidterm extends GraphicsProgram {
+	
+	private GLine line;
+	private int x0;
+	private int y0;
+	
+	public void run(){
+		x0 = 0;
+		y0 = 0;
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e){
+		line = new GLine (x0, y0, e.getX(), e.getY());
+		add(line);	
+		}
+	
+//	public void mouseDragged(MouseEvent e){
+//		line.setEndPoint(e.getX(), e.getY());
+//	}
+//	
+	
+	public void mouseReleased(MouseEvent e){
+		x0 = e.getX();
+		y0 = e.getY();
+		
+	}
 
 }
