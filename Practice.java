a0eabc76e7ab97bc7a0f1ca96b723e4c8c96c601
S <s>
2023-11-26 13:45:40 +0400

    {"types":"C","runs":1}

--- Practice.java
+++ Practice.java
@@ -6,7 +6,7 @@ public class Practice extends ConsoleProgram{
 		int a = readInt("enter your number: ");
 		double sqrtA = Math.sqrt(a);
 		int result = 0;
-		for(int i = 0; i < sqrtA; i++){
+		for(int i = 1; i < sqrtA; i++){
 			if( a % i == 0){
 				result++;
 			}
