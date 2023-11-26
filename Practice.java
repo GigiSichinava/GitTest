08849ac75cf85218311dcd453507b72d43679612
S <s>
2023-11-26 14:03:25 +0400

    {"types":"C","runs":7}

--- Practice.java
+++ Practice.java
@@ -1,23 +1,18 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
-
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
+		int number = readInt("Ur number");
 		boolean isPrime = true;
-		int number = readInt();
-
-		for (int i = 2; i <= Math.sqrt(number); i++) {
-			if (number % i == 0) {
+		
+		for(int i = 1; i < Math.sqrt(number); i++){
+			if(number % i == 0){
 				isPrime = false;
 			} else {
 				isPrime = true;
 			}
 		}
-
-		if (isPrime) {
-			println("prime");
-		} else {
-			println("not prime");
-		}
+		println(isPrime);
 	}
 }
