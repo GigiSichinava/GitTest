17686fcb66bcdc41ca7a268d9cea040c83eef947
S <s>
2023-11-26 15:58:23 +0400

    {"types":"C","runs":38}

--- Practice.java
+++ Practice.java
@@ -18,9 +18,9 @@ public class Practice extends GraphicsProgram {
 		add(line);
 	}
 	
-//	public void mouseDragged(MouseEvent e){
-//		line.setEndPoint(e.getX(), e.getY());
-//	}
+	public void mouseDragged(MouseEvent e){
+		line.setEndPoint(e.getX(), e.getY());
+	}
 	
 	public void mouseReleased(MouseEvent e){
 		x0 = e.getX();
