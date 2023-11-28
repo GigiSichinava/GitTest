bcc73d12a8a7a936c50db40161a531df3508aebc
S <s>
2023-11-28 17:42:01 +0400

    {"types":"C","runs":68}

--- Practice.java
+++ Practice.java
@@ -1,15 +1,20 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	public void run(){
-		String text = readLine("Enter text");
-		String replaced = removeDuplications(text);
-		println(replaced);
+public class Practice extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text: ");
+		printNewString(text);
+
 	}
-	
-	private String removeDuplications(String text){
-		text.replace("a", "");
-		return text;
-		
+
+	private void printNewString(String text) {
+		String result = "";
+		for (int i = 0; i < text.length(); i++) {
+			char ch = text.charAt(i);
+			if (!result.contains("" + ch)) {
+				result += ch;
+			}
+		}
+		println(result);
 	}
 }
