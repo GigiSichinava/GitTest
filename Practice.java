ed8beadb5221222bc48818faf32edc0c844b34b1
S <s>
2023-11-28 12:29:58 +0400

    {"types":"C","runs":61}

--- Practice.java
+++ Practice.java
@@ -3,9 +3,12 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
+		String a = readLine("1");
+		String b = readLine("2");
+		
 	}
 	
-	private boolean find(String a, String b){
+	private boolean find1(String a, String b){
 		int index = 0;
 		for(int i = 0; i < a.length(); i++){
 			if(a.charAt(i) == b.charAt(index)){
@@ -14,4 +17,5 @@ public class Practice extends ConsoleProgram{
 		}
 		return index == b.length();
 	}
+
 }
