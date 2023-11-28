ca7ddfdbee65ed72fdc4a4aedc10824303fe39c5
S <s>
2023-11-28 17:12:27 +0400

    {"types":"C","runs":63}

--- Practice.java
+++ Practice.java
@@ -1,21 +1,39 @@
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
+	
+	private static final String SENTINEL = "END";	
+	
 	public void run(){
-		
-		String a = readLine("1");
-		String b = readLine("2");
+		while(true){
+			String s = readLine();
+			if(s.equals(SENTINEL)){
+				break;
+			}
+		}
+		int numChars = distinctChars(s);
+		if(numChars > maxChars){
+			ans = s;
+			
+		}
 		
 	}
 	
-	private boolean find1(String a, String b){
-		int index = 0;
-		for(int i = 0; i < a.length(); i++){
-			if(a.charAt(i) == b.charAt(index)){
-				index++;
-			}
-		}
-		return index == b.length();
-	}
+//	private boolean find1(String a, String b){
+//		int index = 0;
+//		for(int i = 0; i < a.length(); i++){
+//			if(a.charAt(i) == b.charAt(index)){
+//				index++;
+//			}
+//		}
+//		return index == b.length();
+//	}
 
+	
+	private boolean isAlternating(int num){
+		
+	}
+	
+	
+	
 }
