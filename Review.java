a9f75860657de33b30086e1a174e9e1e4d5feace
S <s>
2023-11-28 17:30:50 +0400

    {"types":"C","runs":66}

--- Review.java
+++ Review.java
@@ -1,28 +1,13 @@
 import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 import java.awt.event.MouseEvent;
+import java.util.StringTokenizer;
 
 public class Review extends GraphicsProgram {
-
-    private int xs, ys;
-    private GLine line;
-
-    public void init() {
-        xs = ys = 0;
-        addMouseListeners();
-    }
-
-    public void mousePressed(MouseEvent e) {
-        line = new GLine(xs, ys, e.getX(), e.getY());
-        add(line);
-    }
-
-    public void mouseDragged(MouseEvent e) {
-        line.setEndPoint(e.getX(), e.getY());
-    }
-
-    public void mouseReleased(MouseEvent e) {
-        xs = e.getX();
-        ys = e.getY();
-    }
+	public void run(){
+	
+		
+		
+		
+	}
 }
