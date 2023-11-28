5ed3acba6f7e42dc8b6513f5b9dda7101ccd402d
S <s>
2023-11-28 17:30:49 +0400

    {"types":"C","runs":66}

--- Practice.java
+++ Practice.java
@@ -3,6 +3,12 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
+	}
+	
+	private String removeDuplications(String text){
+		text.replace("i", "");
+		
+		return text;
 		
 	}
 }
