247a712598c040fce56ca61577610d03a7ad6922
S <s>
2023-11-30 00:01:40 +0400

    {"types":"C","runs":140}

--- Practice.java
+++ Practice.java
@@ -1,18 +1,17 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	public void run(){
-		
+public class Practice extends ConsoleProgram {
+	public void run() {
+
 		String text = readLine("Text: ");
 		String result = "";
-		
-		for(int i = 0; i < text.length(); i++){
+
+		for (int i = 0; i < text.length(); i++) {
 			char ch = text.charAt(i);
-			if(!result.contains("" + ch)){
-			result += ch;	
+			if (i == 0 || text.charAt(i) != text.charAt(i - 1)) {
+				result += ch;
 			}
+			println(result);
 		}
-		println(result);
-		
 	}
 }
