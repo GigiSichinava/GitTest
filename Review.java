9708eb964dced56f894e215f130447aca0ecaab6
S <s>
2023-11-30 03:53:02 +0400

    {"types":"C","runs":164}

--- Review.java
+++ Review.java
@@ -2,24 +2,9 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-		while(true){
-			String s = readLine("Enter text: ");
-			println(isProperString(s));
-		}
-		
-	}
-
-	private boolean isProperString(String s) {
-		char prevChar = 'a' - 1; // auto cast between char and int
-		for(int i = 0; i < s.length(); i++){
-			char currChar = s.charAt(i);
-			if(Character.isLowerCase(currChar)){
-				if(currChar <= prevChar){ // მკაცრად ზრდადია ტექსტი 
-					return false;
-				}
-				prevChar = currChar;
-			}
-		}
-		return true;
+	String str = readLine();
+	
+	String newstr = str.substring(3, 4);
+	println(newstr);
 	}
 }
