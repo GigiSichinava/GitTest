29cc42c74c1d6a870682039b12c762d7d7594c27
S <s>
2023-11-29 20:23:45 +0400

    {"types":"C","runs":74}

--- Practice.java
+++ Practice.java
@@ -3,17 +3,11 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
-		String text = readLine("text: ");
-		String symbol = readLine("symbol: ");
+		String text = readLine("text");
 		
-		int count = 0;
 		
 		for(int i = 0; i < text.length(); i++){
-			if(text.charAt(i) == symbol.charAt(0)){
-				count++;
-			}
+			
 		}
-		println(count);
-		
 	}
 }
