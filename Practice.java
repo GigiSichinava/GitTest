89bc3e9d0fd03d66aecccd4c5b1262566361872d
S <s>
2023-11-29 22:26:41 +0400

    {"types":"C","runs":99}

--- Practice.java
+++ Practice.java
@@ -1,13 +1,16 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	public void run(){
+public class Practice extends ConsoleProgram {
+	public void run() {
 		
-		String text = readLine("Text :");
-		stringToInteger();
-	}
-
-	private void stringToInteger() {
+		String num = readLine("Enter number: ");
+		int result = 0;
+		
+		for(int i = 0; i < num.length(); i++){
+			int n = num.charAt(i);
+			println(n);
+		}
+		
 		
 	}
 }
