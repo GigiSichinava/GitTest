ceae465fbeb3822d70cdda116fafd40287a8d389
S <s>
2023-11-29 20:44:34 +0400

    {"types":"C","runs":87}

--- Problem46Strings.java
+++ Problem46Strings.java
@@ -1,48 +1,26 @@
 import acm.program.ConsoleProgram;
 
-public class Problem46Strings extends ConsoleProgram {
-	private int countCharacters(String string, char c) {
-		int count = 0;
-		for (int i = 0; i < string.length(); i++) {
-			if (string.charAt(i) == c) {
-				count++;
-			}
-		}
-		return count;
-	}
-
-	public void run() {
-		String text = readLine("Enter your text here: ");
+public class Problem46Strings extends ConsoleProgram{
+	public void run(){
+		
+		String text = readLine("text: ");
+		
+		char maxCharacter = 0;
 		int maxCount = 0;
-		char maxCharcater = 0;
-		for (char c = 'a'; c <= 'z'; c++) {
-			int count = countCharacters(text, c);
-			if (count > maxCount) {
-				maxCount = count;
-				maxCharcater = c;
+		
+		for(char x = 'a'; x <= 'z'; x++){
+			int count = 0;
+			for(int i = 0; i < text.length(); i++){
+				if(text.charAt(i) == x){
+					count++;
+				}
 			}
+			if(count >= maxCount){
+				maxCount = count;
+				maxCharacter = x;
+			}
+			
 		}
-		println(maxCharcater);
+		println(maxCharacter);
 	}
 }
-
-
-	//	public void run(){
-	//		String text = readLine("Enter your text here: ");
-	//		int maxCharacter = 0;
-	//		int maxCount = 0;
-	//	
-	//		for(char x = 'a'; x <= 'z'; x++){
-	//			int count = 0;
-	//			for(int i = 0; i < text.length(); i++){
-	//				if(text.charAt(i) == x){
-	//					count++;
-	//				}
-	//			}
-	//			if(count > maxCount){
-	//			maxCount = count;
-	//			maxCharacter = x;
-	//			}
-	//		}
-	//		println(maxCharacter);
-	//	}
