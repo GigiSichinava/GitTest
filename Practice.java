f2c9dab2b2a21d5235df93f045f71a5f35ffd68d
S <s>
2023-11-30 01:33:41 +0400

    {"types":"C","runs":155}

--- Practice.java
+++ Practice.java
@@ -8,7 +8,7 @@ public class Practice extends ConsoleProgram{
 		
 		for(int i = 0; i < text.length(); i++){
 			if(i == 0 || text.charAt(i) != text.charAt(i - 1)){
-				result = result + text.charAt(i - 1);
+				result = result + text.charAt(i);
 			}
 		}
 		println(result);
