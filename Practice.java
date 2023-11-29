035ae76193c1ab11222341bc9f98cf7fbe63d187
S <s>
2023-11-29 20:47:05 +0400

    {"types":"C","runs":88}

--- Practice.java
+++ Practice.java
@@ -2,25 +2,8 @@ import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
 	public void run(){
-			
+		
 		String text = readLine("Text: ");
 		
-		int maxCount = 0;
-		char maxChar = 0;
-		
-		for(char x = 'a'; x < 'z'; x++){
-			int count = 0;
-			for(int i = 0; i < text.length(); i++){
-				if(text.charAt(i) == x){
-					count++;
-				}
-			}
-			if(count > maxCount){
-				maxCount = count;
-				maxChar = x;
-			}
-		}
-		println(maxChar);
-		
-	}		
+	}
 }
