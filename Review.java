7dee8f782c218b020b3677e529ce686879a22aa2
S <s>
2023-11-30 01:19:21 +0400

    {"types":"C","runs":146}

--- Review.java
+++ Review.java
@@ -1,13 +1,19 @@
-import acm.graphics.GLine;
-import acm.program.GraphicsProgram;
-import java.awt.event.MouseEvent;
-import java.util.StringTokenizer;
+import acm.program.ConsoleProgram;
 
-public class Review extends GraphicsProgram {
-	public void run(){
-	
-		
-		
-		
-	}
+public class Review extends ConsoleProgram {
+    public void run() {
+        String text = readLine("Text: ");
+
+        for (char x = 'a'; x <= 'z'; x++) {
+            int count = 0;
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) == x) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                println(x + ": " + count);  // Concatenate the character and count as strings
+            }
+        }
+    }
 }
