5b8b293f7ea7cea6cac2dd17a6bd055e0a4feab0
S <s>
2023-12-01 02:19:05 +0400

    {"types":"C","runs":86}

--- Practice.java
+++ Practice.java
@@ -1,22 +1,13 @@
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
-
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
 		String text = readLine("Text: ");
-
-		for (char x = 'a'; x < 'z'; x++) {
-			int count = 0;
-
-			for (int i = 0; i < text.length(); i++) {
-				if (x == text.charAt(i)) {
-					count++;
-				}
-			}
-			if(count > 0){
-				println(x + ": " + count);
-			}
-		}
-
+		StringTokenizer tokenizer = new StringTokenizer(text);
+		println(tokenizer.countTokens());
+		
 	}
 }
