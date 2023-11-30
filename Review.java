6c0b55b22b95348e9651f9654977cbcff672c5fd
S <s>
2023-11-30 12:47:11 +0400

    {"types":"C","runs":166}

--- Review.java
+++ Review.java
@@ -1,10 +1,19 @@
+import acm.graphics.GObject;
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class Review extends ConsoleProgram{
+public class Review extends GraphicsProgram{
 	public void run(){
-	String str = readLine();
+		
+		
+		GRect rect = new GRect (10, 10);
+		GObject obj = getElementAt(10, 10);
+		if(rect == obj){
+			remove(obj);
+		}
+		
 	
-	String newstr = str.toUpperCase();
-	println(newstr);
+	
 	}
 }
