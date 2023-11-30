1fba7e2178e653f3b5033b491d8412efde8edf60
S <s>
2023-12-01 01:45:26 +0400

    {"types":"C","runs":82}

--- Practice.java
+++ Practice.java
@@ -4,15 +4,21 @@ public class Practice extends ConsoleProgram {
 	public void run() {
 
 		String text = readLine("Text: ");
-
 		
+		if(isPalindrom(text)){
+			println("+");
+		}else{
+			println("-");
+		}
+	}
 		
+		private boolean isPalindrom(String text){
 		for (int i = 0; i < text.length(); i++) {
 
-			if (text.charAt(i) == text.charAt(text.length() - i - 1)) {
-			println("+");
+			if (text.charAt(i) != text.charAt(text.length() - i - 1)) {
+			return(false);
 			}
 		}
-		println("-");
+		return(true);
+		}
 	}
-}
