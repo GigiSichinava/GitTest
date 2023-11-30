5127a06fdd4a58200d2ad24228ffc8d5a21fd8e0
S <s>
2023-11-30 23:43:27 +0400

    {"types":"C","runs":10}

--- Practice.java
+++ Practice.java
@@ -1,20 +1,31 @@
-import java.awt.Color;
-
-import acm.graphics.GLine;
-import acm.graphics.GOval;
-import acm.graphics.GRect;
-import acm.program.GraphicsProgram;
-
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class Practice extends GraphicsProgram{
+	
+	private GOval oval;
+	
+	private static final int MAX_RADIUS = 100;
+	private static final int MIN_RADIUS = 10;
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
 	public void run(){
 		
-		GOval oval = new GOval (100,100);
-		oval.setFilled(true);
-		oval.setColor(Color.RED);
-		add(oval);
+		addMouseListeners();
+		addRandomCircle();
 		
 	}
+	
+	private void addRandomCircle(){
+		int R = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
+		
+		oval = new GOval(MIN_RADIUS, MAX_RADIUS);
+		oval.setFilled(true);
+		oval.setColor(rgen.nextColor());
+		add(oval, getWidth() - R, getHeight() - R);
+		
+	}
+	
 }
