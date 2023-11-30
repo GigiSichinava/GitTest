0675a19712c8d9ebd130991efb5c681ad44c976c
S <s>
2023-12-01 02:40:07 +0400

    {"types":"C","runs":87}

--- Practice.java
+++ Practice.java
@@ -1,13 +1,16 @@
-import java.util.StringTokenizer;
-
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	public void run(){
-		
-		String text = readLine("Text: ");
-		StringTokenizer tokenizer = new StringTokenizer(text);
-		println(tokenizer.countTokens());
-		
+public class Practice extends ConsoleProgram {
+	public void run() {
+	
+	String text = readLine("Text :");
+	int result = 0;
+	
+	for(int i = 0 ; i < text.length(); i++){
+		int n = text.charAt(i) - '0';
+		result = result + n;
+	}
+	println(result);
+	
 	}
 }
