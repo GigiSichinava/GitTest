0da69a4ac701db5fd139949da7a2d7fb2f1f0ea6
S <s>
2023-11-30 23:25:15 +0400

    {"types":"C","runs":5}

--- Practice.java
+++ Practice.java
@@ -1,12 +1,13 @@
+import com.sun.prism.paint.Color;
+
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class Practice extends GraphicsProgram{
 	public void run(){
 		
-		GOval oval = new GOval (100,100,100,100);
+		GOval oval = new GOval (100,100);
 		oval.setFilled(true);
-		oval.setColor(color.RED);
 		add(oval);
 		
 	}
