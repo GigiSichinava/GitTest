2a0a9b2261aac78617ab9434014a76850889ddec
S <s>
2023-11-30 17:39:06 +0400

    {"types":"C","runs":177}

--- Review.java
+++ Review.java
@@ -1,3 +1,6 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
@@ -6,27 +9,50 @@ public class Review extends GraphicsProgram{
 	
 	private RandomGenerator rgen =  RandomGenerator.getInstance();
 	
+	private GOval circle = null;
+	
 	private static final double c = 10;
 	private static final double RADISU_MIN = 50;
 	private static final double V_MIN = 1;
 	private static final double V_MAX = 5;
 	
+	private static final int DELAY = 100;
+	
+	private double vx = 0;
+	private double vy = 0;
 	
 	public void run(){
 		addMouseListeners();
 		addRandomCircle();
 		
+		while(true){
+			circle.move(vx,  vy);
+		
+			pause(DELAY);
+		}
+		
 	}
 	
 	private void addRandomCircle(){
 		double radius = rgen.nextDouble(RADISU_MIN, RADISU_MIN);
-		GOval circle = new GOval(radius, radius);
+		circle = new GOval(2  * radius, 2 * radius);
 		circle.setFilled(true);
 		circle.setColor(rgen.nextColor());
 		
 		double x = rgen.nextDouble(0, getWidth() - 2 * radius);
-		double y = rgen.nextDouble(0, getHeight() - radius * 2);
+		double y = rgen.nextDouble(0, getHeight() - 2 * radius);
 		add(circle, x, y);
 		
 	}
+	
+	public void mouseclicked(MouseEvent e){
+		
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if(obj == circle){
+			vx = rgen.nextDouble(V_MIN, V_MAX);
+			vy = rgen.nextDouble(V_MIN, V_MAX);
+		}	
+	}
+	
+
 }
