bcd68df8d1d79b6c4a1d1f9cfd01b804f3830c3b
S <s>
2023-11-30 12:54:25 +0400

    {"types":"C","runs":169}

--- Review.java
+++ Review.java
@@ -1,20 +1,38 @@
+import java.awt.event.MouseEvent;
+
 import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 
-public class Review extends GraphicsProgram{
+
+public class Review extends GraphicsProgram {
+
+	private GObject rect;
+	private GObject obj;
+	
 	public void run(){
 		
 		
 		GRect rect = new GRect (10, 10);
 		add(rect);
-		GObject obj = getElementAt(0, 0);
+		GObject obj = getElementAt(10, 10);
 		if(rect == obj){
 			remove(obj);
 		}
-		
+		addMouseListeners();
+	}
 	
+	
+	public void mouseClicked (MouseEvent e){		
+		
+		if (e.getX() == rect.getX()){
+			remove(obj);
+		}
+		
+	}
 	
 	}
-}
+	
+	
+	
