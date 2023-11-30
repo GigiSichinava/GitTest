3fc2d5c549cf34ae2fe39c499b64d75b0f34e0a3
S <s>
2023-12-01 02:03:34 +0400

    {"types":"C","runs":83}

--- Practice.java
+++ Practice.java
@@ -1,24 +1,21 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
-
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
 		String text = readLine("Text: ");
-		
 		if(isPalindrom(text)){
-			println("+");
+			println("Positive");
 		}else{
-			println("-");
+			println("Negative");
 		}
-	}
-		
+	}	
 		private boolean isPalindrom(String text){
-		for (int i = 0; i < text.length(); i++) {
-
-			if (text.charAt(i) != text.charAt(text.length() - i - 1)) {
-			return(false);
+			for(int i = 0; i < text.length(); i++){
+				if(text.charAt(i) != text.charAt(text.length() - 1 - i)){
+					return(false);
+				}
 			}
-		}
-		return(true);
+			return(true);
 		}
 	}
