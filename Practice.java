bfd4e5087844ea95cf49677a08fd2b739281585f
S <s>
2023-12-01 00:17:07 +0400

    {"types":"C","runs":61}

--- Practice.java
+++ Practice.java
@@ -1,15 +1,32 @@
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class Practice extends GraphicsProgram{
+	
+	private GOval oval;
+	
 	public void run(){
+
 		
-		GOval oval = new GOval (100, 100, 100, 100);
+		addMouseListeners();
+		oval = new GOval (100, 100, 100, 100);
 		oval.setFilled(true);
 		oval.setColor(Color.RED
 				);
 		add(oval);
+		
+		
+		
+	}
+	
+	public void mouseClicked(MouseEvent e){
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if(obj == oval){
+			remove(oval);
+		}
 	}
 }
