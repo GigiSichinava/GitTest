498fd792b2f00329602539a49294125788194887
S <s>
2023-11-30 23:53:40 +0400

    {"types":"C","runs":24}

--- Practice.java
+++ Practice.java
@@ -5,36 +5,53 @@ import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class Practice extends GraphicsProgram{
-	
+public class Practice extends GraphicsProgram {
+
 	private GOval oval;
-	
+
+	private static final int VXMAX = 40;
+	private static final int VXMIN = 20;
+
+	private static final int VYMAX = 60;
+	private static final int VYMIN = 30;
+
 	private static final int MAX_RADIUS = 50;
 	private static final int MIN_RADIUS = 10;
-	
+
+	private int vx = 0;
+	private int vy = 0;
+
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
-	public void run(){
-		
+
+	public void run() {
+
 		addMouseListeners();
 		addRandomCircle();
-		
+
+		while (true) {
+			oval.move(vx, vy);
+			pause(10);
+
+		}
 	}
-	
-	private void addRandomCircle(){
+
+	private void addRandomCircle() {
 		int R = rgen.nextInt(MIN_RADIUS, MAX_RADIUS);
-		
+
 		oval = new GOval(2 * R, 2 * R);
 		oval.setFilled(true);
 		oval.setColor(rgen.nextColor());
-		add(oval, getWidth() / 2 - R, getHeight() / 2 - R);	
+		add(oval, getWidth() / 2 - R, getHeight() / 2 - R);
 	}
-	
-	public void mouseClicked(MouseEvent e){
+
+	public void mouseClicked(MouseEvent e) {
 		GObject obj = getElementAt(e.getX(), e.getY());
-		if(obj == oval){
+		if (obj == oval) {
 			oval.setColor(rgen.nextColor());
 		}
+
+		vx = rgen.nextInt(VXMIN, VXMAX);
+		vy = rgen.nextInt(VYMIN, VYMAX);
 	}
-	
+
 }
