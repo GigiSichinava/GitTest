318c97e41b6498314ed749c61359ab7ad1d02929
S <s>
2023-12-01 22:04:36 +0400

    {"types":"C","runs":136}

--- Practice.java
+++ Practice.java
@@ -21,7 +21,7 @@ public class Practice extends GraphicsProgram {
 		double x2 = e.getX();
 		double y2 = e.getY();
 
-		GLine line = new GLine(x1, y1, x2, y2);
+		line = new GLine(x1, y1, x2, y2);
 		add(line);
 
 		x1 = x2;
@@ -32,7 +32,7 @@ public class Practice extends GraphicsProgram {
 	public void mouseDragged(MouseEvent e){
 		line.setEndPoint(e.getX(), e.getY());
 	}
-	public void mouseRelease(MouseEvent e){
+	public void mouseReleased(MouseEvent e){
 		x1 = e.getX();
 		y1 = e.getY();
 	}
