cc3b99e96de63d3d7c1c5606a8df8ce38ec27352
S <s>
2023-12-01 23:37:55 +0400

    {"types":"C","runs":152}

--- Practice.java
+++ Practice.java
@@ -1,24 +1,23 @@
-import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
-
+public class Practice extends GraphicsProgram{
+	public void run(){
+	
 		String mama = readLine("Mama: ");
 		String shvili = readLine("Shvili: ");
 		println(amIYourFatherLuke(mama, shvili));
+		
 	}
-
-	private boolean amIYourFatherLuke(String mama, String shvili) {
-
-		for (int i = 0; i < shvili.length(); i++) {
-			for (int j = i + 1; j <= shvili.length(); j++) {
-				if (mama.contains(shvili.substring(i, j)) && shvili.substring(i, j).length() > mama.length() / 2) {
+	
+	private boolean amIYourFatherLuke(String mama, String shvili){
+		
+		for(int i = 0; i < shvili.length(); i++){
+			for(int j = 1; j <= shvili.length(); j++){
+				if(mama.contains(shvili.substring(i, j)) && shvili.substring(i, j).length() > mama.length() / 2);
 					return true;
-				}
 			}
 		}
-
+		
 		return false;
 	}
-
 }
