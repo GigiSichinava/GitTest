07c0d4b11e168a97d70ae1e4704849d7af290d10
S <s>
2023-12-01 21:03:32 +0400

    {"types":"C","runs":118}

--- Problem44.java
+++ Problem44.java
@@ -13,7 +13,7 @@ public class Problem44 extends GraphicsProgram{
 	public void mousePressed(MouseEvent e){
 		line = new GLine (e.getX(), e.getY(), e.getX(), e.getY());
 		add(line);
-	}
+	
 	
 	public void mouseDragged (MouseEvent e){
 		line.setEndPoint(e.getX(), e.getY());
