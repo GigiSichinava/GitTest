8d76d0afc23c31065c84cb7d97f9a6ce2a52f954
S <s>
2023-12-01 21:54:38 +0400

    {"types":"C","runs":132}

--- Practice.java
+++ Practice.java
@@ -1,26 +1,29 @@
 import java.awt.event.MouseEvent;
+
 import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class Practice extends GraphicsProgram {
 
-    private double x1 = 0;
-    private double y1 = 0;
+	private double x1 = 0;
+	private double y1 = 0;
 
-    public void run() {
-        addMouseListeners();
-    }
+	public void run() {
 
-    public void mouseClicked(MouseEvent e) {
-        double x2 = e.getX();
-        double y2 = e.getY();
+		addMouseListeners();
 
-        GLine line = new GLine(x1, y1, x2, y2);
-        add(line);
+	}
 
+	public void mouseClicked(MouseEvent e) {
 
-        x1 = x2;
-        y1 = y2;
-    }
+	double x2 = e.getX();
+	double y2 = e.getY();
+		
+	GLine line = new GLine (x1, y1, x2, y2);
+	add(line);
+	
+	x2 = x1;
+	y2 = y1;
+	
+	}
 }
-
