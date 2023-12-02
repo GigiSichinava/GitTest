dec785c8cac42534ede962f424720114bc94a81e
S <s>
2023-12-02 12:55:06 +0400

    {"types":"C","runs":170}

--- Practice.java
+++ Practice.java
@@ -1,57 +1,37 @@
-import java.awt.event.MouseEvent;
-
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
+import javafx.scene.input.MouseEvent;
 
-public class Practice extends GraphicsProgram {
-
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-
-	private static final double MAX_R = 50;
-	private static final double MIN_R = 10;
-
+public class Practice extends GraphicsProgram{
+	
+	private double prevX;
+	private double prevY;
+	private static final int R = 50;
 	private GOval oval;
-
-	private static final double MIN_VX = 1;
-	private static final double MAX_VX = 5;
-	private static final double MIN_VY = 1;
-	private static final double MAX_VY = 5;
-
-	public void run() {
-		addMouseListeners();
-		addRandomCircle();
-
-		double vx = rgen.nextDouble(MIN_VX, MAX_VX);
-		double vy = rgen.nextDouble(MIN_VY, MAX_VY);
-
-		while (true) {
-			oval.move(vx, vy);
-			pause(100);
-		}
-	}
-
-	private void addRandomCircle() {
-
-		double R = rgen.nextDouble(MIN_R, MAX_R);
-
+	
+	public void run(){
+		
 		oval = new GOval(2 * R, 2 * R);
 		oval.setFilled(true);
-		oval.setColor(rgen.nextColor());
-
-		double randomX = rgen.nextDouble(0, getWidth() - oval.getWidth());
-		double randomY = rgen.nextDouble(0, getHeight() - oval.getHeight());
-
-		add(oval, randomX, randomY);
-
+		add(oval);
+		addMouseListeners();
 	}
-
-	public void mouseClicked(MouseEvent e) {
-
+	
+	public void mousePressed(MouseEvent e){
 		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj == oval) {
-			oval.setColor(rgen.nextColor());
+		if (obj != null){
+			prevX = e.getX();
+			prevY = e.getY();
 		}
 	}
+	
+	
+//	public void mouseDragged(MouseEvent e){
+//		GObject obj = getElementAt(e.getX(), e.getY());
+//		if(obj != null){
+//			prevX = e.getX();
+//			prevY = e.getY();
+//		}
+//	}
 }
