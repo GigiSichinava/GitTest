f828d8bf22c17f51d2472b1e8d024a9b08db8fd9
S <s>
2023-12-02 16:26:31 +0400

    {"types":"C","runs":211}

--- Practice.java
+++ Practice.java
@@ -6,12 +6,14 @@ public class Practice extends ConsoleProgram{
 		String text = readLine("Text :");
 		String result = "";
 		
-		for(int i = 1; i < text.length(); i++){
-			if(text.charAt(i) != text.charAt(i)){
+		for(int i = 0; i < text.length() - 1; i++){
+			if(text.charAt(i) != text.charAt(i + 1)){
 				result = result + text.charAt(i);				
 			}
 			
+			
 		}
+		result += text.charAt(text.length() - 1);
 		println(result);
 		
 	}
