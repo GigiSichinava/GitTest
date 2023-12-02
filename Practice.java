b5563fac40b130e1b1721df0dae74e76a85dec02
S <s>
2023-12-02 14:31:10 +0400

    {"types":"C","runs":190}

--- Practice.java
+++ Practice.java
@@ -1,30 +1,23 @@
-import java.util.StringTokenizer;
 import acm.program.ConsoleProgram;
 
-
-public class Practice extends ConsoleProgram {
-	public void run() {
-		while (true) {
-			String s = readLine("Enter text: ");
-			println(beautify(s));
+public class Practice extends ConsoleProgram{
+	private int count = 0;
+	
+	public void run(){
+		
+		String text = readLine("Text: ");
+		String symbol = readLine("Symbo: ");
+		
+		char ch = symbol.charAt(0);
+		
+		for(int i = 0; i < text.length(); i++){
+			if(ch == text.charAt(i)){
+				count++;
+			}
 		}
+		println(count);
+		
 	}
-
-	private String beautify(String s) {
-		StringTokenizer tokenizer = new StringTokenizer(s, " ,.", true);
-		String beautifulS = "";
-		while (tokenizer.hasMoreTokens()) {
-			String currentWord = tokenizer.nextToken();
-			beautifulS += reversed(currentWord);
-		}
-		return beautifulS;
-	}
-
-	private String reversed(String word) {
-		String reversed = "";
-		for(int i = word.length() -1; i >= 0; i--){
-			reversed += word.charAt(i);
-		}
-		return reversed;
-	}
+	
+			
 }
