3c676158e139719a170eb54f6579478ba12f2ac7
S <s>
2023-12-02 16:22:32 +0400

    {"types":"C","runs":203}

--- Practice.java
+++ Practice.java
@@ -1,58 +1,18 @@
-import java.awt.event.MouseEvent;
+import acm.program.ConsoleProgram;
 
-import acm.graphics.GObject;
-import acm.graphics.GRect;
-import acm.program.GraphicsProgram;
-
-public class Practice extends GraphicsProgram{
-	private GRect axali;
-	private GRect dzveli;
-	private GRect obj;
+public class Practice extends ConsoleProgram{
 	public void run(){
-		addMouseListeners();
-		grid();
-	}
-	private void grid(){
-		for(int i = 0; i < 10; i ++){
-			for(int j = 0; j < 10; j++){
-				GRect rect = new GRect(50, 50);
-				add(rect, i * 50, j * 50);
-			}
-		}
-	}
-	public void mouseClicked(MouseEvent e){
-		obj = (GRect)getElementAt(e.getX(), e.getY());
-		if(obj == null)return;
-		if(obj != null && axali == null && dzveli == null){
-			axali = obj;
-			axali.setFilled(true);
-			return;
-		}
-		if(axali != null && obj != null && dzveli == null){
-			dzveli = axali;
-			axali =  obj;
-			axali.setFilled(true);
-			return;
-		}
-		if(obj == dzveli){
-			dzveli.setFilled(false);
-			//dzveli = axali;
-			return;
-		}
-		if(obj == axali){
-			axali.setFilled(false);
-			axali = dzveli;
-			return;
-		}
-		if (axali != null && dzveli != null && obj != null){
-			dzveli.setFilled(false);
-			dzveli = axali;
-			axali = obj;
-			axali.setFilled(true);
-			return;
-		}
 		
-
+		String text = readLine("Text :");
+		String result = "";
+		
+		for(int i = 0; i < text.length() - 1; i++){
+			if(text.charAt(i) != text.charAt(i + 1)){
+				result = result + text.charAt(i);				
+			}
+			
+		}
+		println(result);
+		
 	}
-
 }
