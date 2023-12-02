53011d9194d2731d6ca64151f7135e54c8463394
S <s>
2023-12-02 13:28:14 +0400

    {"types":"C","runs":182}

--- Practice.java
+++ Practice.java
@@ -1,29 +1,32 @@
-import java.util.StringTokenizer;
-import acm.program.ConsoleProgram;
+import java.awt.event.MouseEvent;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
-		while (true) {
-			String s = readLine("Enter text: ");
-			println(beautify(s));
-		}
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
+
+public class Practice extends GraphicsProgram{
+	
+	private double x1;
+	private double y1;
+	private double x2;
+	private double y2;
+	
+	private GLine line;
+	
+	public void run(){
+		
+		x1 = 0;
+		y1 = 0;
+		line = new GLine(x1, y1, x2, y2);
+		addMouseListeners();
+		
 	}
-
-	private String beautify(String s) {
-		StringTokenizer tokenizer = new StringTokenizer(s, " ,.", true);
-		String beautifulS = "";
-		while (tokenizer.hasMoreTokens()) {
-			String currentWord = tokenizer.nextToken();
-			beautifulS += reversed(currentWord);
-		}
-		return beautifulS;
-	}
-
-	private String reversed(String word) {
-		String reversed = "";
-		for(int i = word.length() -1; i >= 0; i--){
-			reversed += word.charAt(i);
-		}
-		return reversed;
+	
+	
+	public void mouseClicked(MouseEvent e){
+		x2 = e.getX();
+		y2 = e.getY();
+		
+		line.setEndPoint(x2, y2);
+		add(line);
 	}
 }
