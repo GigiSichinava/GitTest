98875f8f03a36314d630c2c31dd4df8167b56dc7
S <s>
2023-12-04 20:41:14 +0400

    {"types":"C","runs":1}

--- Review.java
+++ Review.java
@@ -1,29 +1,10 @@
-import java.util.StringTokenizer;
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram {
-	public void run() {
-
-		String text = readLine("Text: ");
-		println(beautify(text));
-	}
-	
-		private String beautify(String text){
-			StringTokenizer tokenizer = new StringTokenizer(text, ",");
-			String beautifuls = "";
-			
-			while(tokenizer.hasMoreTokens()){
-				String currentWord = tokenizer.nextToken();
-				beautifuls += reversed(currentWord);
-			}
-			return beautifuls;
-		}
+public class Review extends ConsoleProgram{
+	public void run(){
 		
-		private String reversed(String word){
-			String reversed = "";
-			for(int i = word.length() - 1; i >= 0; i--){
-				reversed += word.charAt(i);
-			}
-			return reversed;
-		}
+		int[] masivi = new int[5];
+		println(masivi);
+		
+	}
 }
