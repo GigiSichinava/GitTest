23c9980fe682995ca714f0548d7e141d89950daa
S <s>
2023-12-04 20:42:48 +0400

    {"types":"C","runs":2}

--- Review.java
+++ Review.java
@@ -4,7 +4,16 @@ public class Review extends ConsoleProgram{
 	public void run(){
 		
 		int[] masivi = new int[5];
-		println(masivi);
-		
+		masivi[0] = 0;
+		masivi[1] = 1;
+		masivi[2] = 2;
+		masivi[3] = 3;
+		masivi[4] = 4;
+		println(masivi[0]);
+		println(masivi[1]);
+		println(masivi[2]);
+		println(masivi[3]);
+		println(masivi[4]);
+
 	}
 }
