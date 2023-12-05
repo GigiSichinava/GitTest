1b74be912c112923cc6227f623c455d70bf51b29
S <s>
2023-12-05 12:47:36 +0400

    {"types":"C","runs":11}

--- Problem52.java
+++ Problem52.java
@@ -9,7 +9,7 @@ public class Problem52 extends ConsoleProgram {
 	public void run() {
 
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader("PaintBrush.java")); // <-- type your file's name here
+			BufferedReader reader = new BufferedReader(new FileReader("PaintBrush.java"));
 			while (true) {
 				String line = reader.readLine();
 				if (line == null) {
