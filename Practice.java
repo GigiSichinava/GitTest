c043351a516722486397192b7b1b323eaae8d628
S <s>
2023-12-05 17:29:53 +0400

    {"types":"C","runs":33}

--- Practice.java
+++ Practice.java
@@ -4,6 +4,10 @@ public class Practice extends ConsoleProgram{
 	public void run(){
 		
 		int[] myArr = new int[10];
-		println(myArr);
+//		int[] myArr = {10, 20, 30};
+		int[] myArr2 = {0, 5, 6, 7, 8};
+	
+		println(myArr[2]);
+		println(myArr2[2]);
 	}
 }
