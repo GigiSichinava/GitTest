4409006a195c8e9ccd32ae8be6800be5aaa8a910
S <s>
2023-12-05 12:38:15 +0400

    {"types":"C","runs":7}

--- Problem52.java
+++ Problem52.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 import acm.program.ConsoleProgram;
 
@@ -8,9 +9,18 @@ public class Problem52 extends ConsoleProgram {
 	public void run() {
 
 		try {
-			BufferedReader reader = new BufferedReader(
-					new FileReader("ConsoleProgrammSample.java"));
-		} catch (Exception e) {
+			BufferedReader reader = new BufferedReader(new FileReader("PaintBrush.java")); // <-- type your file's name here
+
+			while (true) {
+				String line = reader.readLine();
+				if(line == null){
+					break;
+				}
+				println(line);
+			}
+			// System.out.println("read file");
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
