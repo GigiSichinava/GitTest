d0b4897354959ec04596a9e92f9eb1b4b915986a
S <s>
2023-12-05 12:29:56 +0400

    {"types":"C","runs":1}

--- Problem52.java
+++ Problem52.java
@@ -1,4 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
-public class Problem52 {
+import acm.program.ConsoleProgram;
 
+public class Problem52 extends ConsoleProgram {
+	public void run() {
+
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader("ConsoleProgrammSample.java"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- Review.java
+++ Review.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram {
 	public void run() {
 
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader("ConsoleProgrammSample.java"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
