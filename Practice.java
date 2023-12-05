6459044b31e128f47069278a0cf86d3be06c0344
S <s>
2023-12-05 17:17:09 +0400

    {"types":"C","runs":26}

--- Practice.java
+++ Practice.java
@@ -4,6 +4,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 import acm.program.ConsoleProgram;
 
@@ -15,6 +16,15 @@ public class Practice extends ConsoleProgram {
 			BufferedReader reader = new BufferedReader(new FileReader("FILENAME"));
 			
 			println("Correct");
+			while(true){
+				try {
+					String newLine = reader.readLine();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
 		
 		} catch (FileNotFoundException e) {  // tu try shi kodi daerorda gadmova aq
 			// TODO Auto-generated catch block
