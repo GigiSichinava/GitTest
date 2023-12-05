5817e9563bbab7c7e92bbb774d09226fa82961af
S <s>
2023-12-05 17:26:43 +0400

    {"types":"C","runs":32}

--- Practice.java
+++ Practice.java
@@ -1,38 +1,9 @@
-
-//Problem 52
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-	private static final String FILENAME = "Practice.java";
-
-	public void run() {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
-
-			println("Correct");
-			while (true) {
-				String newLine = reader.readLine();
-				if (newLine == null) {
-					break;
-				}
-				println(newLine);
-			}
-			reader.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-
-			println("Mistake");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
+		int[] myArr = new int[10];
+		println(myArr);
 	}
 }
