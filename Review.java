5d926ab203178dfafd2649ac71d9bf8bc0b31c2f
S <s>
2023-12-05 12:35:36 +0400

    {"types":"C","runs":3}

--- Review.java
+++ Review.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 import acm.program.ConsoleProgram;
 
@@ -8,12 +9,18 @@ public class Review extends ConsoleProgram {
 	public void run() {
 
 		try {
-			BufferedReader reader = new BufferedReader(
-					new FileReader("ConsoleProgrammSample.java"));
-		
-			System.out.println("read file");
-		
-		} catch (Exception e) {
+			BufferedReader reader = new BufferedReader(new FileReader("ConsoleProgrammSample.java"));
+
+			while (true) {
+				String line = reader.readLine();
+				if(line == null){
+					break;
+				}
+				println(line);
+			}
+			// System.out.println("read file");
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
