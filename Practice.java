7f4747ca43cd6d1ffdbf1044faaf0f16ef6962fc
S <s>
2023-12-05 17:51:42 +0400

    {"types":"C","runs":45}

--- Practice.java
+++ Practice.java
@@ -2,17 +2,14 @@ import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	public void run(){
-		
+public class Practice extends ConsoleProgram {
+	public void run() {
+
 		int n = readInt();
-		
-		int[] arr = new int [n];
-		
-		for(int i = 0 ; i < n; i++){
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
 			arr[i] = readInt();
 		}
-		
 		println(Arrays.toString(arr));
 	}
 }
