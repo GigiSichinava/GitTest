8081d48da869c9c18456f9d95d8a55eacebeeb5f
S <s>
2023-12-05 17:13:44 +0400

    {"types":"C","runs":24}

--- Practice.java
+++ Practice.java
@@ -8,12 +8,13 @@ import java.io.FileReader;
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram {
-	private static final String FILENAME = "tst.txt";
+	private static final String FILENAME = "Practice.java";
 
 	public void run() {
 		try {   // aq iwereba kodi da tu daerorda gadmava catchshi
 			BufferedReader reader = new BufferedReader(new FileReader("FILENAME"));
 			
+			println("Correct");
 		
 		} catch (FileNotFoundException e) {  // tu try shi kodi daerorda gadmova aq
 			// TODO Auto-generated catch block
