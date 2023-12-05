86b59219c5d21bab42c62c6b5ef14bd900bbc74c
S <s>
2023-12-05 13:15:03 +0400

    {"types":"C","runs":21}

--- Problem54.java
+++ Problem54.java
@@ -2,19 +2,18 @@ import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
 
-public class Problem54 extends ConsoleProgram{
-	public void run(){
-		
-		
+public class Problem54 extends ConsoleProgram {
+	public void run() {
+
 		int n = readInt();
 		int[] array = new int[n];
-		
-		for(int i = 0 ; i < n; i++){
+
+		for (int i = 0; i < n; i++) {
 			array[i] = readInt();
 		}
-		
+
 		Arrays.sort(array);
-	
+
 		println(Arrays.toString(array));
 	}
 }
--- Problem55.java
+++ Problem55.java
@@ -1,4 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-public class Problem55 {
+import acm.program.ConsoleProgram;
 
+public class Problem55 extends ConsoleProgram {
+	public void run() {
+
+		int n = readInt();
+		int[] array = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			array[i] = readInt();
+		}
+		int[] a = Arrays.copyOf(array, n);
+
+		Arrays.sort(array);
+		println(Arrays.equals(a, array));
+
+	}
 }
