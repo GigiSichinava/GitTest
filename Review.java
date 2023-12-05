56c85fa020f66928b6ce962b3984cac1c291bb66
S <s>
2023-12-05 12:49:59 +0400

    {"types":"C","runs":14}

--- Review.java
+++ Review.java
@@ -6,7 +6,11 @@ public class Review extends ConsoleProgram {
 	public void run() {
 
 		int[] array = new int[10];
-		println(Arrays.toString(array));
+//		int[] array2 = {10, 20, 30, 40};
+		int[] a = array;
+		println(Arrays.toString(array));  // es printavs masivs stringad
 		
+		int[] copy = Arrays.copyOf(array,  4);
+		println(Arrays.toString(copy));  
 	}
 }
