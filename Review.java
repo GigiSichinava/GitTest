483094dcab08bc3a86f710886d57b4e50a3a568a
S <s>
2023-12-07 17:47:56 +0400

    {"types":"C","runs":4}

--- Review.java
+++ Review.java
@@ -1,9 +1,10 @@
-import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class Review extends ConsoleProgram{
+public class Review extends GraphicsProgram{
 	public void run(){
-		
-		
+		addMouseListeners();
+		addBall();
 		
 	}
+	private void 
 }
