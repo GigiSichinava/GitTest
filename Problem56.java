12b5540f9c424d648870a0568f1cbf99dbfda040
S <s>
2023-12-07 17:47:56 +0400

    {"types":"C","runs":4}

--- Problem56.java
+++ Problem56.java
@@ -1,4 +1,30 @@
+import acm.program.ConsoleProgram;
 
-public class Problem56 {
-
+public class Problem56 extends ConsoleProgram{
+	
+	private int n;
+	private int minNum;
+	
+	public void run(){
+		
+		int n = readInt();
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++){
+			array[i] = readInt("Enter your number: ");
+		}
+		swap(array);
+	}
+	private void swap(int[] array){
+		int minNum = 0;
+		for(int i = 0; i < n; i++){
+			if(array[i] < array[i + 1]){
+				minNum = array[i + 1];
+			}else{
+				minNum = array[i];
+			}
+		}
+		int result = minNum;
+		println(result);
+	}
+	
 }
