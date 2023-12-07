ed21b462996547d0e6f0ccebd8c5e744721bfd0f
S <s>
2023-12-07 23:23:45 +0400

    {"types":"C","runs":42}

--- Problem56.java
+++ Problem56.java
@@ -7,9 +7,9 @@ public class Problem56 extends ConsoleProgram{
 	public void run(){
 		int n = readInt("Enter your number: ");
 		int[] Array = new int[n];
-		
 		maxNum = Array[0];
-		for( int i = 1; i < n; i++){
+		
+		for( int i = 1; i <= n; i++){
 			Array[i] = readInt();
 			if(Array[i] > maxNum){
 				maxNum = Array[i];
