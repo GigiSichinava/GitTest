55308e53bc0a2cc74298199e9c11969f8bfc426d
S <s>
2023-12-07 17:30:20 +0400

    {"types":"C","runs":3}

--- Review.java
+++ Review.java
@@ -1,20 +1,9 @@
-import java.util.Arrays;
-
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram {
-	public void run() {
-
-		int[] array = new int[10];
-//		int[] array2 = {10, 20, 30, 40};
-		int[] a = array;
-		println(Arrays.toString(array));  // es printavs masivs stringad
-		
-		int[] copy = Arrays.copyOf(array,  4); // <-- 4 ro weria, nishnavs ramdeni wevri minda wamovigo dasakopireblad
-		println(Arrays.toString(copy));  
+public class Review extends ConsoleProgram{
+	public void run(){
 		
 		
-		println(array.length);
-		println(copy.length);
+		
 	}
 }
