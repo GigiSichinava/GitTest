7287ca84a230e7444def9d84d73c3a6964080d49
S <s>
2023-12-07 23:27:32 +0400

    {"types":"C","runs":49}

--- FindMinArray.java
+++ FindMinArray.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class FindMinArray {
-
+public class FindMinArray extends ConsoleProgram{
+	
+	private int minNum;
+	
+	public void run(){
+		
+		int n = readInt();
+		int[] array = new int[n];
+		for(int i = 0; i < n; i++){
+			array[i] = readInt("Enter your number: ");
+		}
+		min(array);
+	}
+	private void min(int[] array){
+		minNum = array[0];
+		for(int i = 1; i < array.length; i++){
+			if(array[i] <= minNum){
+				minNum = array[i];
+			}
+		}
+		println(minNum);
+	}
+	
 }
