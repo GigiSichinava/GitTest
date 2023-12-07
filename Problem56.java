d63c9a56a0d08a02b9445d8d8d6db992c900c3fd
S <s>
2023-12-07 17:49:08 +0400

    {"types":"C","runs":5}

--- Problem56.java
+++ Problem56.java
@@ -18,9 +18,9 @@ public class Problem56 extends ConsoleProgram{
 		int minNum = 0;
 		for(int i = 0; i < n; i++){
 			if(array[i] < array[i + 1]){
-				minNum = array[i + 1];
-			}else{
 				minNum = array[i];
+			}else{
+				minNum = array[i + 1];
 			}
 		}
 		int result = minNum;
