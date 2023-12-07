c87ee3bf235e085b3ab319f9c819596cb08b3e44
S <s>
2023-12-07 18:06:59 +0400

    {"types":"C","runs":25}

--- Problem56.java
+++ Problem56.java
@@ -6,24 +6,22 @@ public class Problem56 extends ConsoleProgram{
 	private int minNum;
 	
 	public void run(){
-		
 		int n = readInt();
 		int[] array = new int[n];
 		for(int i = 0; i < n; i++){
 			array[i] = readInt("Enter your number: ");
 		}
-		int b = min(array);
-		println(b);
+		min(array);
 	}
-	private int min(int[] array){
+	
+	
+	private void min(int[] array){
 		minNum = array[0];
 		for(int i = 1; i < n; i++){
 			if(array[i] <= minNum){
 				minNum = array[i];
 			}
 		}
-		return minNum;
-		//println(minNum);
 	}
 	
 }
