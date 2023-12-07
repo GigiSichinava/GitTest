c776ee12ca5d743fa5ee13eccfaeea095a8d4239
S <s>
2023-12-07 23:27:35 +0400

    {"types":"C","runs":49}

--- FindMaxArray.java
+++ FindMaxArray.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class FindMaxArray {
-
+public class FindMaxArray extends ConsoleProgram{
+	
+	private int maxNum;
+	
+	public void run(){
+		int n = readInt("Enter your number: ");
+		int[] Array = new int[n];
+		
+		maxNum = Array[0];
+		for( int i = 0; i < Array.length; i++){
+			Array[i] = readInt();
+			if(Array[i] >= maxNum){
+				maxNum = Array[i];
+			}
+		}
+		println(maxNum);
+	}
 }
