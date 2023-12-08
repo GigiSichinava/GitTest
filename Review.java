f2d0f7e62b24ddb88f8c8b84c81361748f9eca6c
S <s>
2023-12-08 22:18:30 +0400

    {"types":"C","runs":36}

--- Review.java
+++ Review.java
@@ -2,13 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-		int score = 77;
-		if(score >= 90){
-			println("a");
-		}else if(score >= 80){
-			println("b");
-		}else if(score >= 70){
-			println("C");
-		}
+	int x = 5;
+	if(true){
+		int y = 5;
+	}
+		println(x + y);
 	}
 }
