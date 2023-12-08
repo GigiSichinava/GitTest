f20e2256f8733a1d84d38e0a92dde3cc1149985e
S <s>
2023-12-08 20:29:02 +0400

    {"types":"C","runs":28}

--- Review.java
+++ Review.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class Review extends ConsoleProgram{
 	public void run(){
 		int a = 5;
-		double b = 2.0;
-		println(a / b);
+		double b = a / 2;
+		println(b);
 	}
 }
