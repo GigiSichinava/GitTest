f20bb5d1788edd6c3a9aa029e1b5b63d8ece2ed6
S <s>
2023-12-08 22:30:07 +0400

    {"types":"C","runs":39}

--- Review.java
+++ Review.java
@@ -1,19 +1,22 @@
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram{
-	public void run(){
-		int x = readInt();
-		switch(x){
-		case 0:
-			println("nuli");
-			break;
-		case 2:
-			println("ori");
-			break;
-		default:
-			println("racxa sxva");
-			break;
-			
+public class Review extends ConsoleProgram {
+	public void run() {
+		while (2 > 1) {
+
+			int x = readInt();
+			switch (x) {
+			case 0:
+				println("nuli");
+				break;
+			case 2:
+				println("ori");
+				break;
+			default:
+				println("racxa sxva");
+				break;
+
+			}
 		}
 	}
 }
