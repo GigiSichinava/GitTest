a9f5d08ffdc3e8f16261bfb708176bdd3df5e462
S <s>
2023-12-08 22:25:30 +0400

    {"types":"C","runs":37}

--- Review.java
+++ Review.java
@@ -2,10 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-	int x = 5;
-	if(true){
-		int y = 5;
-	}
-		println(x + y);
+		boolean TrueOrFalse = (2 > 1);
+		println(TrueOrFalse);
 	}
 }
