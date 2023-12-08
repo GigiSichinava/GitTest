f6467b78e80ee40925f7576a96bfd10c77b13bfa
S <s>
2023-12-08 19:42:29 +0400

    {"types":"C","runs":16}

--- Review.java
+++ Review.java
@@ -1,25 +1,10 @@
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
-	
-	private int FirstNum;
-	
 	public void run(){
-		 
-		 int n = readInt("Enter your number: ");
-		 int[] Array = new int[n];
-		 
-		 for(int i = 0; i < n; i++){
-			 Array[i] = readInt();
-		 }
-		 
-		 int maxNum = Array[0];
-		 
-		 for (int i = 1; i < Array.length - 1; i++){
-			 if(Array[i] > maxNum){
-				 maxNum = Array[i];
-			 }
-		 }
-		 println(maxNum);
+		int x;
+		x = 3;
+		x = x + 4;
+		println(x);
 	}
 }
