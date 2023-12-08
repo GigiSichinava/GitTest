17502ca9725b3947698ac3cbd65d82c5cfb76466
S <s>
2023-12-08 19:11:14 +0400

    {"types":"C","runs":12}

--- Practice.java
+++ Practice.java
@@ -1,40 +1,11 @@
-import java.util.Arrays;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-import acm.program.ConsoleProgram;
-
-public class Practice extends ConsoleProgram {
-	public void run() {
-
-		int n = readInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = readInt();
-		}
-		println(Arrays.toString(arr));
-		println(isSorted(arr));
-		println(isSorted2(arr));
-
-	}
-
-	boolean isSorted(int[] arr) {
-		for(int i = 1; i < arr.length; i++){
-			
-			if(arr[i] < arr[i - 1]){
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	boolean isSorted2(int[] arr){
-		int[] arrCopy = new int[arr.length];
-		for(int i = 0 ; i < arr.length; i++){
-			arrCopy[i] = arr[i];	
-		}
+public class Practice extends GraphicsProgram{
+	public void run(){
 		
-		Arrays.sort(arrCopy);
-		return(Arrays.equals(arr, arrCopy));
-
+		GRect rect = new GRect (100, 100);
+		rect.setFilled(true);
+		add(rect, getWidth() / 2 - 50, getHeight() / 2 - 50);
 	}
-
 }
