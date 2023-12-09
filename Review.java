112ea16283d6ff12fbdc057e310100b02f62fd70
S <s>
2023-12-10 03:15:57 +0400

    {"types":"C","runs":105}

--- Review.java
+++ Review.java
@@ -1,49 +1,24 @@
-//import acm.program.ConsoleProgram;
-//
-//public class Review extends ConsoleProgram {
-//	public void run() {
-//		while (2 > 1) {
-//
-//			int x = readInt();
-//			switch (x) {
-//			case 0:
-//				println("nuli");
-//				break;
-//			case 2:
-//				println("ori");
-//				break;
-//			default:
-//				println("racxa sxva");
-//				break;
-//
-//			}
-//		}
-//	}
-//}
+import acm.program.ConsoleProgram;
 
+public class Review extends ConsoleProgram {
+	public void run() {
+		while (2 > 1) {
 
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
-import java.awt.event.MouseEvent;
+			int x = readInt();
+			switch (x) {
+			case 0:
+				println("nuli");
+				break;
+			case 2:
+				println("ori");
+				break;
+			default:
+				println("racxa sxva");
+				break;
 
-public class Review extends GraphicsProgram {
+			}
+		}
+	}
+}
 
-    private GOval draggableObject;
-
-    public void run() {
-        draggableObject = new GOval(100, 100, 50, 50);
-        draggableObject.setFilled(true);
-        add(draggableObject);
-
-        addMouseListeners();
-    }
-
-    public void mouseDragged(MouseEvent e) {
-        double dx = e.getX() - draggableObject.getX();
-        double dy = e.getY() - draggableObject.getY();
-        draggableObject.move(dx, dy);
-    }
-
-   
-    }
 
