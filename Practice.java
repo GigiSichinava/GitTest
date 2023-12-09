a4bd147cb61af08410f8a5dc472589dae0576008
S <s>
2023-12-10 02:06:50 +0400

    {"types":"C","runs":79}

--- Practice.java
+++ Practice.java
@@ -5,57 +5,66 @@ import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class Practice extends GraphicsProgram{
-	
+public class Practice extends GraphicsProgram {
+
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
+
 	private static final double PAUSE = 50;
 	private static final double R = 30;
 	private static final double D = 2 * R;
 	private GOval ball;
-	
-	public void run(){
+
+	public void run() {
 		addMouseListeners();
-		addBall();
+		addBall1();
+		addBall2();
 		double dx = 10;
 		double dy = 10;
-		while(true){
-			
-			if(ball.getX() + D > getWidth()){
+		while (true) {
+
+			if (ball.getX() + D > getWidth()) {
 				dx = -dx;
 				ball.setColor(rgen.nextColor());
 			}
-			if(ball.getY() + D > getHeight()){
+			if (ball.getY() + D > getHeight()) {
 				dy = -dy;
 				ball.setColor(rgen.nextColor());
-			} 
-			if(ball.getX() < 0){
+			}
+			if (ball.getX() < 0) {
 				dx = -dx;
 				ball.setColor(rgen.nextColor());
 			}
-			if(ball.getY() < 0){
+			if (ball.getY() < 0) {
 				dy = -dy;
 				ball.setColor(rgen.nextColor());
 			}
 			ball.move(dx, dy);
 			pause(PAUSE);
-			
+
 		}
 	}
-	private void addBall(){
+
+	private void addBall1() {
 		ball = new GOval(2 * R, 2 * R);
 		ball.setFilled(true);
 		ball.setColor(rgen.nextColor());
 		add(ball);
 	}
-	public void mouseClicked(MouseEvent e){
+	private void addBall2(){
+		ball = new GOval(2 * R, 2 * R);
+		ball.setFilled(true);
+		ball.setColor(rgen.nextColor());
+		add(ball, getWidth() - D, getHeight() - D);
+	}
+
+	public void mouseClicked(MouseEvent e) {
 		GObject obj = getElementAt(e.getX(), e.getY());
-		if(obj == null){
+		if (obj == null) {
 			ball.setColor(rgen.nextColor());
 		}
-		if(obj == ball)	{
+		if (obj == ball) {
 			remove(ball);
 		}
-		
+
 	}
 }
