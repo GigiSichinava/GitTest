bc4a6a3a06463f6dedbc8a04f9d0a6d1fe43f194
S <s>
2023-12-10 13:24:14 +0400

    {"types":"C","runs":138}

--- Practice.java
+++ Practice.java
@@ -1,63 +1,22 @@
-import java.awt.event.MouseEvent;
-
-import acm.graphics.GObject;
 import acm.graphics.GOval;
-import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
 
-public class Practice extends GraphicsProgram {
-
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-
-	private GObject ball;
-	private GObject rect;
-	private static final double D = 100;
-	private static final double x = 200;
-	private static final double y = 100;
-
-	private double prevX;
-	private double prevY;
-
-	public void run() {
-		addMouseListeners();
-		addBall();
-		addRect();
-	}
-
-	private void addBall() {
-		ball = new GOval(D, D);
-		((GOval) ball).setFilled(true);
-		add(ball, rgen.nextDouble(0, getWidth() - D), rgen.nextDouble(0, getHeight() - D));
-	}
-
-	private void addRect() {
-		rect = new GRect(x, y);
-		((GRect) rect).setFilled(true);
-		add(rect, rgen.nextDouble(0, getWidth() - x), rgen.nextDouble(0, getHeight() - y));
-
-	}
-
-	public void mousePressed(MouseEvent e) {
-		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj != null) {
-			prevX = e.getX();
-			prevY = e.getY();
+public class Practice extends GraphicsProgram{
+	
+	private GOval head;
+	private GOval body;
+	
+	public void run(){
+		head = new GOval (50, 50);
+		body = new GOval (20, 150);
+		add(head, getWidth() / 2 - 25, -50);
+		add(body, -20, getHeight() / 2 - 75);
+		while(head.getY() < 50){
+			head.move(0, 5);
+			pause(50);
 		}
-	}
-
-	public void mouseDragged(MouseEvent e) {
-		double x = e.getX() - prevX;
-		double y = e.getY() - prevY;
-		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj == rect) {
-			rect.move(x, y);
+		while(body.getX() < getWidth() / 2 - 10){
+			body.move(5, 0);
 		}
-		if (obj == ball) {
-			ball.move(x, y);
-		}
-		prevX = e.getX();
-		prevY = e.getY();
-
 	}
 }
