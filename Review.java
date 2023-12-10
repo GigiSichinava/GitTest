72febb90c10b003e50d4d83c2cb136e7197a8cab
S <s>
2023-12-11 02:07:46 +0400

    {"types":"C","runs":317}

--- Review.java
+++ Review.java
@@ -4,17 +4,15 @@ public class Review extends ConsoleProgram {
 	public void run() {
 		String str = readLine();
 		String result = "";
-		for(int i = 0; i < str.length(); i++){
-			if(Character.isLowerCase(str.charAt(i))){
-				
+		for (int i = 0; i < str.length(); i++) {
+			if (Character.isLowerCase(str.charAt(i))) {
+
 				result += Character.toUpperCase(str.charAt(i));
-			}else if(Character.isUpperCase(str.charAt(i))){
-				
+			} else if (Character.isUpperCase(str.charAt(i))) {
+
 				result += Character.toLowerCase(str.charAt(i));
 			}
 		}
 		println(result);
 	}
 }
-
-
