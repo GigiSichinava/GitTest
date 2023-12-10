bcea0175bf2acfebdc893948ceaab512033a87e1
S <s>
2023-12-11 02:17:52 +0400

    {"types":"C","runs":322}

--- Review.java
+++ Review.java
@@ -1,18 +1,12 @@
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram {
-	public void run() {
-		String str = readLine();
-		String result = "";
-		for (int i = 0; i < str.length(); i++) {
-			if (Character.isLowerCase(str.charAt(i))) {
-
-				result += Character.toUpperCase(str.charAt(i));
-			} else if (Character.isUpperCase(str.charAt(i))) {
-
-				result += Character.toLowerCase(str.charAt(i));
-			}
-		}
-		println(result);
+public class Review extends ConsoleProgram{
+	public void run(){
+		
+		String srt = readLine();
+		StringTokenizer tokenizer = new StringTokenizer(" !,.");
+		tokenizer.countTokens();
 	}
 }
