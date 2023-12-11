08814bce4e6c14ef72231c4a464acb3fbbd69a83
S <s>
2023-12-11 18:25:01 +0400

    {"types":"C","runs":328}

--- Review.java
+++ Review.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-		int x = 5;
+		int x = readInt();
 		double xToDouble = (double)x;
 		println(xToDouble);
 		
