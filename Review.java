6ee943f2a4bd41ca18332631043b76b89edcbae4
S <s>
2023-12-11 23:57:02 +0400

    {"types":"C","runs":364}

--- Review.java
+++ Review.java
@@ -1,29 +1,14 @@
-import java.util.Arrays;
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram {
-	public void run() {
-
-		int[] Array1 = { 1, 3, 5, 7, 9 };
-		int[] Array2 = { 2, 4, 6, 8 };
-
-		int maxLength = 2 * Math.max(Array1.length, Array2.length);
-
-		int[] result = new int[maxLength];
-		int numOccurance = 0;
-
-		for (int i = 0; i < maxLength; i++) {
-			if (i < Array1.length) {
-				result[numOccurance++] = Array1[i];
-			}
-			if (i < Array2.length) {
-				result[numOccurance++] = Array2[i];
+public class Review extends ConsoleProgram{
+	public void run(){
+		int[] Array = {2, 6, 3, 10, -2, 7};
+		int result = Array[0];
+		for(int i = 0; i < Array.length; i++){
+			if(result < Array[i]){
+				result = Array[i];
 			}
 		}
-
-		result = Arrays.copyOf(result, numOccurance);
-		println(Arrays.toString(result));
-
+		println(result);
 	}
 }
-
