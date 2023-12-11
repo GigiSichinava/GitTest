12a91928b366eecb2184036a52ea8cbbb85ce011
S <s>
2023-12-12 00:37:31 +0400

    {"types":"C","runs":368}

--- Problem54.java
+++ Problem54.java
@@ -1,19 +1,20 @@
-import java.util.Arrays;
-
 import acm.program.ConsoleProgram;
 
-public class Problem54 extends ConsoleProgram {
-	public void run() {
-
-		int n = readInt();
-		int[] array = new int[n];
-
-		for (int i = 0; i < n; i++) {
-			array[i] = readInt();
+public class Problem54 extends ConsoleProgram{
+	
+	private int maxNum;
+	
+	public void run(){
+		int n = readInt("Enter your number: ");
+		int[] Array = new int[n];
+		
+		maxNum = Array[0];
+		for( int i = 0; i < Array.length; i++){
+			Array[i] = readInt();
+			if(Array[i] >= maxNum){
+				maxNum = Array[i];
+			}
 		}
-
-		Arrays.sort(array);
-
-		println(Arrays.toString(array));
+		println(maxNum);
 	}
 }
