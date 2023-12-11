b93115ed59167dab0a64093f725f70985376845f
S <s>
2023-12-12 00:45:06 +0400

    {"types":"C","runs":369}

--- Problem55.java
+++ Problem55.java
@@ -1,21 +1,33 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
-
 import acm.program.ConsoleProgram;
 
 public class Problem55 extends ConsoleProgram {
 	public void run() {
-
-		int n = readInt();
-		int[] array = new int[n];
-
+		int n = readInt("Enter n: ");
+		int[] Array = new int[n];
 		for (int i = 0; i < n; i++) {
-			array[i] = readInt();
+			Array[i] = readInt("Enter number: ");
 		}
-		int[] a = Arrays.copyOf(array, n);
 
-		Arrays.sort(array);
-		println(Arrays.equals(a, array));
-
+		println(Arrays.toString(Array));
+		println(isSorted2(Array));
+		println(isSorted(Array));
 	}
+
+	boolean isSorted(int[] Array) {
+		for (int i = 1; i < Array.length; i++) {
+			if (Array[i] < Array[i - 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	boolean isSorted2(int[] Array) {
+		int[] ArrayCopy = Arrays.copyOf(Array, Array.length);
+
+		Arrays.sort(ArrayCopy);
+		return Arrays.equals(Array, ArrayCopy);
+	}
+
 }
