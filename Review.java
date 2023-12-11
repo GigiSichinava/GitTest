5fad167ed35405a87e096f69edadc8931b0802dd
S <s>
2023-12-11 18:20:58 +0400

    {"types":"C","runs":326}

--- Review.java
+++ Review.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class Review extends ConsoleProgram{
 	public void run(){
 		int x = 5;
-		String xtoStr = "" + x;
+		String xtoStr = "" + x + "beng";
 		println(xtoStr);
 		
 	}
