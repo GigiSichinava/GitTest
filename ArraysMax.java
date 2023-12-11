dbac9331e87b57717a4edf885e11d97da796068d
S <s>
2023-12-12 00:02:06 +0400

    {"types":"C","runs":366}

--- ArraysMax.java
+++ ArraysMax.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class ArraysMax {
-
+public class ArraysMax extends ConsoleProgram{
+	public void run(){
+		
+		int lengthOfArray = readInt("Enter length: ");
+		int[] Array = new int[lengthOfArray];
+			for(int j = 0; j < lengthOfArray; j++){
+				Array[j] = readInt();
+			}
+		int result = Array[0];
+		for(int i = 0; i < Array.length; i++){
+			if(result < Array[i]){
+				result = Array[i];
+			}
+		}
+		println(result);
+	}
 }
