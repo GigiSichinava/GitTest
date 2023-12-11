091517e41275345307f780a34a898a959dd60339
S <s>
2023-12-11 18:32:59 +0400

    {"types":"C","runs":332}

--- Review.java
+++ Review.java
@@ -2,8 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-		String xStr = "5";
-		int x = Integer.parseInt(xStr);
+		int x = 'C';
 		println(x);
 		
 	}
