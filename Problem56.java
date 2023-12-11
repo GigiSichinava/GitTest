5c95dd3e44a6914b9a65fa527407d7e3ada4581b
S <s>
2023-12-12 00:50:06 +0400

    {"types":"C","runs":372}

--- Problem56.java
+++ Problem56.java
@@ -1,20 +1,40 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
-public class Problem56 extends ConsoleProgram{
-	
-	private int maxNum;
-	
-	public void run(){
-		int n = readInt("Enter your number: ");
-		int[] Array = new int[n];
-		
-		maxNum = Array[0];
-		for( int i = 0; i < Array.length; i++){
-			Array[i] = readInt();
-			if(Array[i] >= maxNum){
-				maxNum = Array[i];
+public class Problem56 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter  n: ");
+		int[] nums = new int[n];
+		for (int i = 0; i < n; i++) {
+			nums[i] = readInt("Enter  nums[" + i + "]: ");
+		}
+
+		println(Arrays.toString(nums));
+		sort(nums);
+		println(Arrays.toString(nums));
+	}
+
+	private void sort(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			int j = findMinIndex(nums, i);
+			swap(nums, i, j);
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
+
+	private int findMinIndex(int[] nums, int i) {
+		int minIndexSoFar = i;
+		for (int j = i; j < nums.length; j++) {
+			if (nums[j] < nums[minIndexSoFar]) {
+				minIndexSoFar = j;
 			}
 		}
-		println(maxNum);
+		return minIndexSoFar;
 	}
 }
