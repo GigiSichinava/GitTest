d73792176a352a048eaef3732999daa7d32258d1
S <s>
2023-12-11 23:54:01 +0400

    {"types":"C","runs":363}

--- AlteringArrays.java
+++ AlteringArrays.java
@@ -1,4 +1,30 @@
+import java.util.Arrays;
+import acm.program.ConsoleProgram;
 
-public class AlteringArrays {
+public class AlteringArrays extends ConsoleProgram {
+	public void run() {
 
+		int[] Array1 = { 1, 3, 5, 7, 9 };
+		int[] Array2 = { 2, 4, 6, 8 };
+
+		int maxLength = 2 * Math.max(Array1.length, Array2.length);
+
+		int[] result = new int[maxLength];
+		int numOccurance = 0;
+
+		for (int i = 0; i < maxLength; i++) {
+			if (i < Array1.length) {
+				result[numOccurance++] = Array1[i];
+			}
+			if (i < Array2.length) {
+				result[numOccurance++] = Array2[i];
+			}
+		}
+
+		result = Arrays.copyOf(result, numOccurance);
+		println(Arrays.toString(result));
+
+	}
 }
+
+
