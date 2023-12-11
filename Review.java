fbeb30fddc44f0c2c343a9e7f7e352057e057c73
S <s>
2023-12-11 23:26:53 +0400

    {"types":"C","runs":355}

--- Review.java
+++ Review.java
@@ -1,26 +1,30 @@
 import java.util.Arrays;
-
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram {
-	public void run(){
-		
-		int[] Array1 = {1, 3, 5, 7, 9};
-		int[] Array2 = {2, 4, 6, 8};
-		
-		int maxLength = 2 * Math.max(Array1.length, Array2.length);
+    public void run() {
 
-		int[] result = new int[maxLength];
-		
-		for(int i = 0; i < maxLength; i++){
-			if( i < maxLength){
-				result[i] = Array1[i];
-			}
-			if( i < maxLength){
-				result[i] = Array2[i];
-			}
-		}
-		println(Arrays.toString(result));
-		
-	}
+        int[] Array1 = {1, 3, 5, 7, 9};
+        int[] Array2 = {2, 4, 6, 8};
+
+        int maxLength = Math.max(Array1.length, Array2.length);
+
+        int[] result = new int[maxLength * 2]; // Double the length for safety
+
+        int index = 0; // Index for the result array
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < Array1.length) {
+                result[index++] = Array1[i];
+            }
+            if (i < Array2.length) {
+                result[index++] = Array2[i];
+            }
+        }
+
+        // Trim the result array to the actual size
+        result = Arrays.copyOf(result, index);
+
+        println(Arrays.toString(result));
+    }
 }
