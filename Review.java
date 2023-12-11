b419580cc38948b899c83e5a2ee8f92e7523d783
S <s>
2023-12-11 23:01:23 +0400

    {"types":"C","runs":341}

--- Review.java
+++ Review.java
@@ -11,7 +11,7 @@ public class Review extends ConsoleProgram {
 		for (int i = 0; i < str1.length(); i++) {
 			for (int j = 0; j < str2.length(); j++) {
 				if ((int) str1.charAt(i) % 2 == 0 && (int) str2.charAt(j) % 2 != 0) {
-					result += str1.charAt(i) + str2.charAt(j) + "";
+					result += str1.charAt(i) + "" + str2.charAt(j);
 				}
 			}
 		}
@@ -23,3 +23,24 @@ public class Review extends ConsoleProgram {
   
 
 
+//import acm.program.ConsoleProgram;
+//
+//public class Review extends ConsoleProgram {
+//
+//    private String result = "";
+//
+//    public void run() {
+//        String str1 = readLine("Enter first string here: ");
+//        String str2 = readLine("Enter second string here: ");
+//
+//        for (int i = 0; i < str1.length(); i++) {
+//            for (int j = 0; j < str2.length(); j++) {
+//                if ((int) str1.charAt(i) % 2 == 0 && (int) str2.charAt(j) % 2 != 0) {
+//                    result += str1.charAt(i) + "" + str2.charAt(j);
+//                }
+//            }
+//        }
+//
+//        println("Result: " + result);
+//    }
+//}
