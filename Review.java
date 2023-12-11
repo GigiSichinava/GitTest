aaf3c6437e75af771be001eb94d9a1929d55f5c0
S <s>
2023-12-11 23:03:27 +0400

    {"types":"C","runs":345}

--- Review.java
+++ Review.java
@@ -8,8 +8,8 @@ public class Review extends ConsoleProgram {
 		String str1 = readLine("Enter first string here: ");
 		String str2 = readLine("Enter second string here: ");
 
-		for (int i = 0; i < str1.length(); i += 2) {
-			for (int j = 0; j < str2.length(); j += 2) {
+		for (int i = 0; i < str1.length(); i++) {
+			for (int j = 0; j < str2.length(); j++) {
 //				if ((int) str1.charAt(i) % 2 == 0 && (int) str2.charAt(j) % 2 != 0) {
 					result += str1.charAt(i) + "" + str2.charAt(j);
 				}
