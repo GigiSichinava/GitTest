2ebe37b68402bc2f59f7e914527d43ce3a52dfc3
S <s>
2023-12-11 22:55:34 +0400

    {"types":"C","runs":337}

--- Review.java
+++ Review.java
@@ -1,9 +1,17 @@
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram{
-	public void run(){
-		int x = 'C';
-		println((char)x);
-		
+public class Review extends ConsoleProgram {
+	public void run() {
+		String str1 = readLine("Enter first string here: ");
+		String str2 = readLine("Enter second string here: ");
+		String result = "";
+
+		for (int i = 0; i < str1.length(); i++) {
+			for (int j = 0; j < str2.length(); j++) {
+				if ((int) str1.charAt(i) % 2 == 0 && (int) str2.charAt(j) % 2 != 0) {
+					result += str1.charAt(i) + str2.charAt(j);
+				}
+			}
+		}
 	}
 }
