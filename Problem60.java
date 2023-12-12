c8490a021d9ba1d30bf26f77103553075d4e48aa
S <s>
2023-12-12 17:55:28 +0400

    {"types":"C","runs":379}

--- Problem60.java
+++ Problem60.java
@@ -2,20 +2,27 @@ import java.util.ArrayList;
 
 import acm.program.ConsoleProgram;
 
-public class Problem60 extends ConsoleProgram{
+public class Problem60 extends ConsoleProgram {
 	
 	private static final int SENTINEL = -1;
 	
-	public void run(){
+	public void run() {
 		ArrayList<Integer> myList = new ArrayList<Integer>();
 		
-		while(true){
+		while (true) {
 			int n = readInt("Enter your number: ");
-			if (n == SENTINEL){
+			if (n == SENTINEL) {
 				break;
 			}
-			myList.add(0);
+			myList.add(n);
 		}
+		
+		printReversedOrder(myList);
 	}
 
+	private void printReversedOrder(ArrayList<Integer> list) {
+		for (int i = list.size() - 1; i >= 0; i--) {
+			println(list.get(i));
+		}
+	}
 }
