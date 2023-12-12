714ed05188d9759d40cca32b5f59c87f7a4e8e19
S <s>
2023-12-12 17:29:26 +0400

    {"types":"C","runs":373}

--- Problem57.java
+++ Problem57.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
 
-public class Problem57 {
+import acm.program.ConsoleProgram;
 
+public class Problem57 extends ConsoleProgram{
+	public void run(){
+	
+	String str1 = readLine();
+	String str2 = readLine();
+	println(Arrays.equals(check(str1), check(str2)));
+	}
+	
+	
+	private int[] check(String str1){
+		int[] Array = new int [26];
+		for(int i = 0; i < str1.length(); i++){
+			Array[str1.charAt(i) - 'a']++;
+		}
+		return Array;
+	}
 }
