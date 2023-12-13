9ab2f1cdaba81a626ecc62fe0170e9a351d3f39f
S <s>
2023-12-13 20:45:34 +0400

    {"types":"C","runs":415}

--- Problem54.java
+++ Problem54.java
@@ -1,32 +1,23 @@
-
-//კონსოლიდან შეგყვავს n და შემდეგ n ცალი მთელი რიცხვი, იპოვეთ და დაბეჭდეთ ამ
-//რიცხვებს შორის ორი მაქსიმალური რიცხვი.
-
-
-import java.lang.reflect.Array;
-
 import acm.program.ConsoleProgram;
 
 public class Problem54 extends ConsoleProgram{
 	public void run(){
 		
-		int n = readInt("Enter number: ");
+		int n = readInt();
 		int[] myArray = new int[n];
-		for(int i = 0; i < myArray.length; i++){
+		for(int i = 0; i < n; i++){
 			myArray[i] = readInt();
 		}
-		
-		int maxNum = myArray[0];
+		int maxNum1 = myArray[0];
 		int maxNum2 = myArray[0];
 		
-		for(int i = 1; i < myArray.length; i++){
-			if(myArray[i] > maxNum){
-				maxNum2 = maxNum;
-				maxNum = myArray[i];
-				
+		for(int j = 1; j < myArray.length; j++){
+			if(myArray[j] > maxNum1){
+				maxNum1 = myArray[j];
+				maxNum2 = maxNum1;
 			}
 		}
-		println(maxNum);
+		println(maxNum1);
 		println(maxNum2);
 	}
 }
