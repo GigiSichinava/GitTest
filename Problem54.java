146d44a4bcafb9e1db0a731da0b0592a4c471457
S <s>
2023-12-13 20:48:31 +0400

    {"types":"C","runs":418}

--- Problem54.java
+++ Problem54.java
@@ -1,24 +1,24 @@
 import acm.program.ConsoleProgram;
 
-public class Problem54 extends ConsoleProgram{
-	public void run(){
-		
+public class Problem54 extends ConsoleProgram {
+	public void run() {
+
 		int n = readInt("Enter size of the Array: ");
 		int[] myArray = new int[n];
-		for(int i = 0; i < n; i++){
+		for (int i = 0; i < n; i++) {
 			myArray[i] = readInt();
 		}
-		
+
 		int maxNum1 = myArray[0];
 		int maxNum2 = myArray[0];
-		
-		for(int j = 1; j < myArray.length; j++){
-			if(myArray[j] > maxNum1){
+
+		for (int j = 1; j < myArray.length; j++) {
+			if (myArray[j] > maxNum1) {
 				maxNum2 = maxNum1;
 				maxNum1 = myArray[j];
-				
 			}
 		}
+
 		println("First biggest number is: " + maxNum1);
 		println("Second biggest number is: " + maxNum2);
 	}
