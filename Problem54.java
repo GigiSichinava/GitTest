87c9416307a9ba7b96b5ae3545d85fa3d7369fcd
S <s>
2023-12-13 20:19:22 +0400

    {"types":"C","runs":407}

--- Problem54.java
+++ Problem54.java
@@ -1,19 +1,21 @@
-import java.util.Arrays;
-
 import acm.program.ConsoleProgram;
 
 public class Problem54 extends ConsoleProgram {
-	public void run() {
+    public void run() {
+        int n = readInt("Enter the number of integers: ");
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
 
-		int n = readInt();
-		int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = readInt("Enter an integer: ");
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max2 = num;
+            }
+        }
 
-		for (int i = 0; i < n; i++) {
-			array[i] = readInt();
-		}
-
-		Arrays.sort(array);
-
-		println(Arrays.toString(array));
-	}
+        println("The two maximum integers are: " + max1 + " and " + max2);
+    }
 }
