ec0d106c1ad0b6e2410f2a08f18b567c40bac26b
S <s>
2023-12-15 16:13:03 +0400

    {"types":"C","runs":1}

--- Practice.java
+++ Practice.java
@@ -1,15 +1,17 @@
-import java.util.HashMap;
-import java.util.Map;
-
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
-		Map<String, Double> midtermScore = new HashMap<String,Double>();
+		int a = readInt();
+		int b = readInt();
+		int[][] myMatrix = new int[a][b];
 		
+		for(int i = 0; i < myMatrix.length; i++){
+			for(int j = 0; j < myMatrix[0].length; j++){
+				myMatrix[i][j] = readInt();
+			}
+		}
+		println(myMatrix);
 	}
 }
-
-
-// for( key:)
