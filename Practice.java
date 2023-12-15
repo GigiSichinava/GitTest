d1870e8b50dad1858ab58010310e17c9aab9e512
S <s>
2023-12-15 16:17:51 +0400

    {"types":"C","runs":3}

--- Practice.java
+++ Practice.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	public void run(){
-		
+public class Practice extends ConsoleProgram {
+	public void run() {
+
 		int a = readInt();
 		int b = readInt();
 		int[][] myMatrix = new int[a][b];
-		
-		for(int i = 0; i < myMatrix.length; i++){
-			for(int j = 0; j < myMatrix[0].length; j++){
+
+		for (int i = 0; i < myMatrix.length; i++) {
+			for (int j = 0; j < myMatrix[0].length; j++) {
 				myMatrix[i][j] = readInt();
 			}
 		}
-		println(myMatrix.toString());
+		printMatrix(myMatrix);
+	}
+
+	private void printMatrix(int[][] matrix) {
+		for (int[] row : matrix) {
+			println(Arrays.toString(row));
+		}
 	}
 }
