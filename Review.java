9176e207838c693660fad87d838aaeb90852bbe9
S <s>
2023-12-15 21:57:44 +0400

    {"types":"C","runs":23}

--- Review.java
+++ Review.java
@@ -1,19 +1,43 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
 		
-		int lengthOfArray = readInt("Enter length: ");
-		int[] Array = new int[lengthOfArray];
-			for(int j = 0; j < lengthOfArray; j++){
-				Array[j] = readInt();
-			}
-		int result = Array[0];
-		for(int i = 0; i < Array.length; i++){
-			if(result < Array[i]){
-				result = Array[i];
+		int x = readInt();
+		int y = readInt();
+		int[][] myMatrix = new int[x][y];
+		isMatrixMagic(myMatrix);
+		
+	}
+
+	private boolean  isMatrixMagic(int myMatrix[][]){
+		int a = myMatrix.length;
+		int b = myMatrix[0].length;
+		if(a != b){
+			return false;
+		}
+		
+		int k = 0;
+		int[] Array = new int[a * a];
+		for(int i = 0; i < myMatrix.length; i++){
+			for(int j = 0; j < myMatrix[0].length; j++){
+				Array[k] = myMatrix[i][j];
+				k++;
 			}
 		}
-		println(result);
+		
+		Arrays.sort(Array);
+		
+		for(int g = 0; g < Array.length; g++){
+			if(Array[g] - g != 1){
+				return false;
+			}
+		}
+		
+		
+		return true;
+		
 	}
 }
