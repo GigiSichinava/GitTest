ff2c775633b44ae7ab34ec24209f2b70166a0621
S <s>
2023-12-15 22:24:21 +0400

    {"types":"C","runs":27}

--- Review.java
+++ Review.java
@@ -5,8 +5,8 @@ import acm.program.ConsoleProgram;
 public class Review extends ConsoleProgram{
 	public void run(){
 		
-		int x = readInt();
-		int y = readInt();
+		int x = readInt("Width: ");
+		int y = readInt("Height: ");
 		int[][] myMatrix = new int[x][y];
 		for(int i = 0; i < x; i++){
 			for(int j = 0; j < y; j++){
@@ -14,10 +14,9 @@ public class Review extends ConsoleProgram{
 			}
 		}
 		println(isMatrixMagic(myMatrix));
-		
 	}
-
-	private boolean  isMatrixMagic(int myMatrix[][]){
+	
+	private boolean isMatrixMagic(int[][] myMatrix){
 		int a = myMatrix.length;
 		int b = myMatrix[0].length;
 		if(a != b){
@@ -25,24 +24,22 @@ public class Review extends ConsoleProgram{
 		}
 		
 		int k = 0;
-		int[] Array = new int[a * a];
+		int[] myArray = new int[a * a];
 		for(int i = 0; i < myMatrix.length; i++){
 			for(int j = 0; j < myMatrix[0].length; j++){
-				Array[k] = myMatrix[i][j];
+				myArray[k] = myMatrix[i][j];
 				k++;
 			}
 		}
 		
-		Arrays.sort(Array);
+		Arrays.sort(myArray);
 		
-		for(int g = 0; g < Array.length; g++){
-			if(Array[g] - g != 1){
+		for(int i = 0; i < myArray.length; i++){
+			if(myArray[i] - i != 1){
 				return false;
 			}
 		}
 		
-		
 		return true;
-		
 	}
 }
