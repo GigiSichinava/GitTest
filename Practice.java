483ea65bcff01e357816423e998713e25aead108
S <s>
2023-12-15 16:40:41 +0400

    {"types":"C","runs":3}

--- Practice.java
+++ Practice.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
@@ -21,5 +22,33 @@ public class Practice extends ConsoleProgram {
 		for (int[] row : matrix) {
 			println(Arrays.toString(row));
 		}
+	}
+	
+	private boolean magicM(int[][] Matrix){
+		
+		int n = Matrix.length;
+		int m = Matrix[0].length;
+		if ( n != m){
+			return false;
+		
+	}
+		int result = 0;
+		int[] myarray = new int[n*n];
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				int k = 0;
+				while(k < n*n){
+				myarray[k] = Matrix[i][j];
+				k++;
+				}
+			}
+		}
+		Arrays.sort(myarray);
+		for(int i = 0; i < myarray.length; i++){
+			if(myarray[i] - i == 1){
+				result++;
+			}
+		}
+		return result == n * n;
 	}
 }
