268d9dcced52550cd8631da195fa4f0cae8fff68
S <s>
2023-12-18 02:13:59 +0400

    {"types":"C","runs":65}

--- Practice.java
+++ Practice.java
@@ -1,33 +1,28 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-
-    private static final String SENTINEL = "";
-
-    public void run() {
-
-        ArrayList<String> myList = new ArrayList<String>();
-
-        while (true) {
-            String x = readLine("Enter name: ");
-            if (x.equals(SENTINEL)) {
-                break;
-            }
-            myList.add(x);
-        }
-
-        for (int i = 0; i < myList.size(); i++) {
-            String a = myList.get(i);
-            int result = 0;
-            for (int j = 0; j < myList.size(); j++) {
-                String b = myList.get(j);
-                if (a.equals(b)) {
-                    result++;
-                }
-            }
-            println("Occurrence of " + a + " is : " + result);
-        }
-    }
+public class Practice extends ConsoleProgram{
+	
+	private static final String SENTINEL = "";
+	
+	public void run(){
+		
+		HashMap<String, Integer> myMap = new HashMap<>();
+		
+		while(true){
+			String name = readLine("Enter name: ");
+			if(name.equals(SENTINEL)){
+				break;
+			}
+			myMap.put(name, myMap.getOrDefault(name, 0));
+		}
+		
+		for(Map.Entry<String, Integer> entry : myMap.entrySet()){
+			String name = entry.getKey();
+			int occurance = entry.getValue();
+            println("Occurrence of " + name + " is : " + occurance);
+		}
+	}
 }
