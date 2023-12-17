46d5f22d9f47ad48e82e09a35bf1fd9e1a708f53
S <s>
2023-12-17 20:40:21 +0400

    {"types":"C","runs":45}

--- Problem61.java
+++ Problem61.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
 
-public class Problem61 {
+import acm.program.ConsoleProgram;
 
+public class Problem61 extends ConsoleProgram{
+	public void run(){
+		
+		int m = readInt("Enter the number, that you want to check: ");
+		int n = readInt("Enter list size: ");
+		
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		for(int i = 0; i < n; i++){
+			int x = readInt();
+			myList.add(x);
+		}
+		
+		for(int i = 0; i < myList.size(); i++){
+			for(int j = 0; j < myList.size(); j++){
+				int a = myList.get(i);
+				int b = myList.get(j);
+				if(a + b == m){
+					println("Right couple is : " + a + " and " + b);
+				}
+			}
+		}
+	}
 }
