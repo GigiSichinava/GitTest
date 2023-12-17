04a82b73ab7b8ac84da3e300d3a70022d6eb6d10
S <s>
2023-12-17 20:27:54 +0400

    {"types":"C","runs":39}

--- Practice.java
+++ Practice.java
@@ -1,86 +1,30 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-	public void run() {
-
-		int a = readInt();
-		int b = readInt();
-		int[][] myMatrix = new int[a][b];
-
-		for (int i = 0; i < myMatrix.length; i++) {
-			for (int j = 0; j < myMatrix[0].length; j++) {
-				myMatrix[i][j] = readInt();
-			}
-		}
-		println(magicM(myMatrix));
-//		printMatrix(myMatrix);
-	}
-
-	private void printMatrix(int[][] matrix) {
-		for (int[] row : matrix) {
-			println(Arrays.toString(row));
-		}
-	}
-
+public class Practice extends ConsoleProgram{
 	
-	private boolean magicM(int[][] Matrix){
-		
-		int n = Matrix.length;
-		int m = Matrix[0].length;
-		if ( n != m){
-			return false;
-		
-	}
-		
-		int k = 0;
-		int[] myarray = new int[n*n];
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-			
-				myarray[k] = Matrix[i][j];
-				k++;
-				
-			}
-		}
-		Arrays.sort(myarray);
-		for(int i = 0; i < myarray.length; i++){
-			if (myarray[i] - i != 1) {
-				return false;
-			}
-		}
-		return true;
-	}
+	private static final int SENTINEL = -1;
 	
-//	
-//	private boolean magicM(int[][] Matrix) {
-//	    int n = Matrix.length;
-//	    int m = Matrix[0].length;
-//
-//	    if (n != m) {
-//	        return false;
-//	    }
-//
-//	    int[] myarray = new int[n * n];
-//	    int k = 0;
-//
-//	    for (int i = 0; i < n; i++) {
-//	        for (int j = 0; j < m; j++) {
-//	            myarray[k++] = Matrix[i][j];
-//	        }
-//	    }
-//
-//	    Arrays.sort(myarray);
-//
-//	    for (int i = 0; i < myarray.length - 1; i++) {
-//	        if (myarray[i + 1] - myarray[i] != 1) {
-//	            return false;
-//	        }
-//	    }
-//
-//	    return true;
-//	}
+	public void run(){
+		
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		int Num = readInt();
+		while(true){
+			if(Num == SENTINEL)
+				break;
+			}
+		myList.add(Num);
+		println(reversedOrder(myList));
+		}
 
+	private String reversedOrder(ArrayList<Integer> myList) {
+		String result = "";
+		for(int i = myList.size() - 1; i > 0; i--){
+			result += myList.get(i);
+		}
+		
+		
+		return result;
+	}
 }
