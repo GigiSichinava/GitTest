c41e0d50eb1f6ec94a149cfb04d1e29d0870e829
S <s>
2023-12-18 03:08:54 +0400

    {"types":"C","runs":66}

--- Practice.java
+++ Practice.java
@@ -3,26 +3,28 @@ import java.util.Map;
 
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram{
-	
-	private static final String SENTINEL = "";
-	
-	public void run(){
-		
-		HashMap<String, Integer> myMap = new HashMap<>();
-		
-		while(true){
-			String name = readLine("Enter name: ");
-			if(name.equals(SENTINEL)){
-				break;
-			}
-			myMap.put(name, myMap.getOrDefault(name, 0) + 1);
-		}
-		
-		for(Map.Entry<String, Integer> entry : myMap.entrySet()){
-			String name = entry.getKey();
-			int occurance = entry.getValue();
-            println("Occurrence of " + name + " is : " + occurance);
-		}
-	}
+public class Practice extends ConsoleProgram {
+
+    private static final String SENTINEL = "";
+
+    public void run() {
+        
+    	Map<String, Integer> myMap = new HashMap<>();
+
+        while (true) {
+            String name = readLine("Enter name (or press Enter to stop): ");
+            if (name.equals(SENTINEL)) {
+                break;
+            }
+            myMap.put(name, myMap.getOrDefault(name, 0) + 1);
+        }
+
+        println("Name Occurrences:");
+
+        for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
+            String name = entry.getKey();
+            int occurrences = entry.getValue();
+            println("Occurrence of " + name + " is : " + occurrences);
+        }
+    }
 }
