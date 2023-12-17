c4ed82aa6a2862e7178a6dfb3d1afe096bca0787
S <s>
2023-12-17 20:46:03 +0400

    {"types":"C","runs":46}

--- Practice.java
+++ Practice.java
@@ -3,25 +3,28 @@ import java.util.ArrayList;
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
+	
+	private static final int SENTINEL = -1;
+	
 	public void run(){
 		
-		int m = readInt();
-		int n = readInt();
-		
 		ArrayList<Integer> myList = new ArrayList<Integer>();
-		for(int i = 0; i < n; i++){
+		
+		while(true){
 			int x = readInt();
+			if(x == SENTINEL){
+				break;
+			}
 			myList.add(x);
 		}
 		
 		for(int i = 0; i < myList.size(); i++){
-			for(int j = 0; j < myList.size(); j++){
-				int a = myList.get(i);
-				int b = myList.get(j);
-				if(a + b == m){
-					println("Right couple is : " + a + " and " + b);
-				}
+			int result = 0;
+			if(myList.get(i) == 0){
+				result++;
 			}
+			println("Occurance of " + i + " is : " + result);
 		}
+		
 	}
 }
