0512efb54a3dd8a8c9251d87e2845f99f5ef2824
S <s>
2023-12-18 20:42:07 +0400

    {"types":"C","runs":16}

--- Review.java
+++ Review.java
@@ -4,7 +4,7 @@ public class Review extends ConsoleProgram {
     public void run() {
         String line = readLine();
         for (int i = 0; i < line.length(); i++) {
-            if (line.charAt(i) == line.charAt(line.length() - 1)) {
+            if (line.charAt(i) == line.charAt(line.length() - 1 - i)) {
                 println("+");
             } else {
                 println("-");
