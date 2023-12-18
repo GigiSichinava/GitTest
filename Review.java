aa19676cc295ee3e04167803fba2cd52722f1cbf
S <s>
2023-12-18 20:39:00 +0400

    {"types":"C","runs":12}

--- Review.java
+++ Review.java
@@ -1,19 +1,15 @@
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram{
-	public void run(){
-		
+public class Review extends ConsoleProgram {
+	public void run() {
+
 		String line = readLine();
-		char maxChar = line.charAt(0);
-		int maxOccur = 0;
-		
-		for(int i = 0; i < line.length(); i++){
-			if(maxChar == line.charAt(i)){
-				maxChar = line.charAt(i);
-//				maxOccur++;
+		for (int i = 0; i < line.length(); i++) {
+			if (line.charAt(i) == line.charAt(line.length()) - 1) {
+				println("+");
+			} else {
+				println("-");
 			}
 		}
-		println(maxChar);
-//		println(maxOccur);
 	}
 }
