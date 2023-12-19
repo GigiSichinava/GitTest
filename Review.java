a706220907872260436ba35aa39b11dd55a58116
S <s>
2023-12-19 19:12:11 +0400

    {"types":"C","runs":23}

--- Review.java
+++ Review.java
@@ -2,23 +2,13 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
+		String number = readLine();
+		int result = 0;
 		
-		String line = readLine();
-		String used = "";
-		
-		for(int i = 0; i < line.length(); i++){
-			char ch = line.charAt(i);
-			if(!used.contains("" + ch));
-			int amount = 0;
-			for(int j = 0; j < line.length(); j++){
-				if(ch == line.charAt(i)){
-					amount++;
-				}
-			}
-			println(ch + " : " + amount);
-
-			used = used + ch;
-
+		for(int i = 0; i < number.length(); i++){
+			int n = number.charAt(i) - '0';
+			result = n * 10 + n;
 		}
+		println(result);
 	}
 }
