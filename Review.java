a5e93aa6cf170fecd60d7662924a6c65bffb07db
S <s>
2023-12-19 19:24:24 +0400

    {"types":"C","runs":27}

--- Review.java
+++ Review.java
@@ -2,10 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-		String a = readLine();
-		String b = readLine();
-		if(a.contains(b)){
-			println(true);
-		}
+	String a = readLine();
+	String b = readLine();
+	if(a.equals(b)){
+		println(true);
+	}
 	}
 }
