1f1aa7df5835498759ccf61a3bc14c000090da37
S <s>
2023-12-21 18:03:37 +0400

    {"types":"C","runs":22}

--- Review.java
+++ Review.java
@@ -6,30 +6,31 @@ import javax.swing.JTextField;
 
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram{
-	
+public class Review extends ConsoleProgram {
+
 	private JTextField field;
-	
-	public void run(){
-		
+
+	public void run() {
+
 		JLabel label = new JLabel("Hello!");
 		add(label, SOUTH);
-		
-		field= new JTextField(10);
+
+		field = new JTextField(10);
 		add(field, SOUTH);
-		
+
 		JButton button = new JButton("Press me!");
 		add(button, SOUTH);
-		
+
 		addActionListeners();
 	}
-	
-	public void actionPerformed(ActionEvent e){		
-		if(e.getActionCommand().equals("Press me!")){
-			if(e.getActionCommand().equals("")){
-				break;
-			}else{
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("Press me!")) {
+			if (e.getActionCommand().equals("")) {
+				println("_");
+			} else {
 				println(field.getText());
+
 			}
 		}
 	}
