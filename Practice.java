5ccbffe280a5a6b794630ca7531a4d987a073c16
S <s>
2023-12-21 17:56:42 +0400

    {"types":"C","runs":17}

--- Practice.java
+++ Practice.java
@@ -9,22 +9,40 @@ public class Practice extends ConsoleProgram {
 
     public void run() {
         
-    	Map<String, Integer> myMap = new HashMap<>();
-
-        while (true) {
-            String name = readLine("Enter name (or press Enter to stop): ");
-            if (name.equals(SENTINEL)) {
-                break;
-            }
-            myMap.put(name, myMap.getOrDefault(name, 0) + 1);
-        }
-
-        println("Name Occurrences:");
-
-        for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
-            String name = entry.getKey();
-            int occurrences = entry.getValue();
-            println("Occurrence of " + name + " is : " + occurrences);
-        }
+//    	Map<String, Integer> myMap = new HashMap<>();
+//
+//        while (true) {
+//            String name = readLine("Enter name (or press Enter to stop): ");
+//            if (name.equals(SENTINEL)) {
+//                break;
+//            }
+//            myMap.put(name, myMap.getOrDefault(name, 0) + 1);
+//        }
+//
+//        println("Name Occurrences:");
+//
+//        for (Map.Entry<String, Integer> entry : myMap.entrySet()) {
+//            String name = entry.getKey();
+//            int occurrences = entry.getValue();
+//            println("Occurrence of " + name + " is : " + occurrences);
+//        }
+    	
+    	int a = readInt();
+    	int b = readInt();
+    	int c = readInt();
+    	
+    	if(a > b){
+    		println(">");
+    	}else{
+    		println("<");
+    	}
+    	
+    	if(a > b){
+    		println(">");
+    	}else if(a > c){
+    		println("a beze meti araa mara c ze metia");
+    	}
+    	
+    	
     }
 }
