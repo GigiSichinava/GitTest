ba868568a8ec129133f55d35c365e1cc95f2835b
S <s>
2023-12-21 17:37:40 +0400

    {"types":"C","runs":7}

--- Review.java
+++ Review.java
@@ -1,5 +1,4 @@
 import java.awt.event.ActionEvent;
-import java.text.MessageFormat.Field;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -8,25 +7,24 @@ import javax.swing.JTextField;
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram {
+    private JTextField field; // Declare the field as an instance variable
 
-	private JTextField field;
+    public void run() {
+        JLabel label = new JLabel("Hello!");
+        add(label, SOUTH);
 
-	public void run() {
-		JLabel label = new JLabel("Hello!");
-		add(label, SOUTH);
+        field = new JTextField(10);
+        add(field, SOUTH);
 
-		JTextField field = new JTextField(10);
-		add(field, SOUTH);
+        JButton button = new JButton("Press me!");
+        add(button, SOUTH);
 
-		JButton button = new JButton("Press me!");
-		add(button, SOUTH);
+        addActionListeners(); // Add this line to enable action events
+    }
 
-		addActionListeners();
-	}
-
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equals("Press me!")) {
-			println(field.getText());
-		}
-	}
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Press me!")) { // Check the action command
+            println(field.getText());
+        }
+    }
 }
