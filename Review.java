ee4163dd188ceeda93bf6cb535a1ab8fccea1ac3
S <s>
2023-12-21 17:11:32 +0400

    {"types":"C","runs":2}

--- Review.java
+++ Review.java
@@ -1,10 +1,15 @@
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
+		
 	public void run(){
 		 JLabel label = new JLabel("Hello!");
 		 add(label, SOUTH);
+		 
+		 JTextField field = new JTextField(10);
+		 add(field, SOUTH);
 	}
 }
