f20e0ad7a43ee590d2ebc7ebe50596b0a5e74012
S <s>
2023-12-21 17:09:38 +0400

    {"types":"C","runs":1}

--- Review.java
+++ Review.java
@@ -1,13 +1,10 @@
+import javax.swing.JLabel;
+
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-	String a = readLine();
-	String b = readLine();
-	if(a.equals(b)){
-		println(true);
-	}else{
-		println("tleo");
-	}
+		 JLabel label = new JLabel("Hello!");
+		 add(label, SOUTH);
 	}
 }
