0e5212d0a3566251be450510b3c98aff5d1f0c95
S <s>
2023-12-22 19:51:13 +0400

    {"types":"C","runs":42}

--- Problem54.java
+++ Problem54.java
@@ -1,25 +1,38 @@
 import acm.program.ConsoleProgram;
 
 public class Problem54 extends ConsoleProgram {
-	public void run() {
+    public void run() {
+       
+    	int n = readInt("Enter numbers (more than one): ");
+    	if(n < 2){
+    		println("Array has to contain minimum 2 numbers!");
+    		return;
+    	}
+    	
+    	int[] myArray = new int[n];
+    	for(int i = 0; i < myArray.length; i++){
+    		myArray[i] = readInt("Enter number " + (i + 1) + " : " );
+    	}
 
-		int n = readInt("Enter size of the Array: ");
-		int[] myArray = new int[n];
-		for (int i = 0; i < n; i++) {
-			myArray[i] = readInt();
-		}
-
-		int maxNum1 = myArray[0];
-		int maxNum2 = myArray[0];
-
-		for (int j = 1; j < myArray.length; j++) {
-			if (myArray[j] > maxNum1) {
-				maxNum2 = maxNum1;
-				maxNum1 = myArray[j];
-			}
-		}
-
-		println("First biggest number is: " + maxNum1);
-		println("Second biggest number is: " + maxNum2);
-	}
+    	int maxNum1, maxNum2;
+    	
+    	if(myArray[0] > myArray[1]){
+    		maxNum1 = myArray[0];
+    		maxNum2 = myArray[1];
+    	}else{
+    		maxNum1 = myArray[1];
+    		maxNum2 = myArray[0];
+    	}
+    	
+    	for(int j  = 2 ; j < myArray.length; j++){
+    		if(myArray[j] > maxNum1){
+    			maxNum1 = myArray[j];
+    			maxNum2 = maxNum1; 
+    		}else if(myArray[j] > maxNum2 && myArray[j] != maxNum1){
+    			maxNum2 = myArray[j];
+    		}
+    	}
+    	println("First biggest number is: " + maxNum1);
+    	println("Second biggest number is: " + maxNum2);
+    }
 }
