79464bb15e4179020fed62438ad35d836d3ccfd7
S <s>
2023-12-22 20:00:15 +0400

    {"types":"C","runs":50}

--- Review.java
+++ Review.java
@@ -13,7 +13,7 @@ public class Review extends ConsoleProgram{
 	}
 	
 	private boolean isSorted (int[] Array){
-		for(int j = 0; j < Array.length - 1; j++){
+		for(int j = 0; j < Array.length; j++){
 			if(Array[j] < Array[j + 1]){
 				return true;
 			}else{
