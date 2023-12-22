248be2e4dffd086e2bc2af57c4af5c6a81174d20
S <s>
2023-12-22 19:49:20 +0400

    {"types":"A","runs":41}

---
+++ Practice.java
@@ -0,0 +1,39 @@
+import acm.program.ConsoleProgram;
+
+public class Practice extends ConsoleProgram {
+    public void run() {
+        int n = readInt("Enter size of the Array: ");
+        if (n < 2) {
+            println("Array must contain at least two elements.");
+            return;
+        }
+
+        int[] myArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            myArray[i] = readInt("Enter number " + (i + 1) + ": ");
+        }
+
+        int maxNum1, maxNum2;
+
+        // Initialize maxNum1 and maxNum2 to the first two elements
+        if (myArray[0] > myArray[1]) {
+            maxNum1 = myArray[0];
+            maxNum2 = myArray[1];
+        } else {
+            maxNum1 = myArray[1];
+            maxNum2 = myArray[0];
+        }
+
+        for (int j = 2; j < n; j++) {
+            if (myArray[j] > maxNum1) {
+                maxNum2 = maxNum1;
+                maxNum1 = myArray[j];
+            } else if (myArray[j] > maxNum2 && myArray[j] != maxNum1) {
+                maxNum2 = myArray[j];
+            }
+        }
+
+        println("First biggest number is: " + maxNum1);
+        println("Second biggest number is: " + maxNum2);
+    }
+}
