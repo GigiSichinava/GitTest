fd22d708181e12b66e6aac82b0d5b49b5e4381a6
S <s>
2023-12-22 20:34:07 +0400

    {"types":"C","runs":80}

--- Problem56EasyVersion.java
+++ Problem56EasyVersion.java
@@ -1,4 +1,24 @@
+import java.util.Arrays;
 
-public class Problem56EasyVersion {
+import acm.program.ConsoleProgram;
 
+public class Problem56EasyVersion extends ConsoleProgram {
+	public void run() {
+
+		int n = readInt("Size of the Array: ");
+		int[] Array = new int[n];
+
+		if (Array.length < 2) {
+			println("Array size must be greater than 1");
+			return;
+
+		}
+
+		for (int i = 0; i < Array.length; i++) {
+			Array[i] = readInt("Enter number " + (i + 1) + " : ");
+		}
+
+		Arrays.sort(Array);
+		println(Arrays.toString(Array));
+	}
 }
