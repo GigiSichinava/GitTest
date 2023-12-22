fc49b85741544d06076b9e1a687063a4be4222f5
S <s>
2023-12-22 19:17:11 +0400

    {"types":"C","runs":31}

--- Review.java
+++ Review.java
@@ -1,14 +1,25 @@
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram{
-	public void run(){
-		
+public class Review extends ConsoleProgram {
+	public void run() {
+
 		int n = readInt("Enter size of the Array: ");
 		int[] Array = new int[n];
-		
-		for(int i = 0; i < Array.length; i++){
+
+		for (int i = 0; i < Array.length; i++) {
 			Array[i] = readInt();
-			println(Array);
 		}
+
+		int maxNum1 = Array[0];
+		int maNum2 = Array[0];
+
+		for (int j = 1; j < Array.length; j++) {
+			if (Array[j] > maxNum1) {
+				maNum2 = maxNum1;
+				maxNum1 = Array[j];
+			}
+		}
+		println("First biggest number is: " + maxNum1);
+		println("Second biggest number is: " + maxNum2);
 	}
 }
