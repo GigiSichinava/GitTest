daa510253275eb413bcbe05c24747af1897d1bb6
S <s>
2023-12-25 01:14:02 +0400

    {"types":"C","runs":81}

--- Review.java
+++ Review.java
@@ -4,21 +4,15 @@ import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
-	
-		int n = readInt("Size of the Array: ");
+		
+		int n = readInt();
 		int[] Array = new int[n];
-	
-		if(Array.length < 2){
-			println("Array size must be greater than 1");
-			return;
-			
+		
+		for(int i = 0; i < n; i++){
+			Array[i] = readInt();
 		}
 		
-		for(int i = 0; i < Array.length; i++){
-			Array[i] = readInt("Enter number " + (i +1) + " : ");
-		}
-		
-		Arrays.sort(Array);
 		println(Arrays.toString(Array));
+		
 	}
 }
