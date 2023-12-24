05a14cda7e5f3dc26fe3af2e91f5bdf050d799fa
S <s>
2023-12-25 03:14:29 +0400

    {"types":"C","runs":82}

--- Review.java
+++ Review.java
@@ -12,7 +12,32 @@ public class Review extends ConsoleProgram{
 			Array[i] = readInt();
 		}
 		
-		println(Arrays.toString(Array));
+		sort(Array);
+		println(Array);
+	}
+
+	private void sort(int[] array) {
+		for(int i = 0; i < array.length; i++){
+			int j = findMinIndex(array , i);
+			swap(array, i, j);
+			println(array);
+		}
 		
+	}
+
+	private void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[i] = temp;
+	}
+
+	private int findMinIndex(int[] array, int i) {
+		int minIndexSofar = i;
+		for(int a = 0; a < array.length; a++){
+			if(array[a] < array[minIndexSofar]){
+				minIndexSofar = a;
+			}
+		}
+		return minIndexSofar;
 	}
 }
