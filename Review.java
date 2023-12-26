c66a534bf8cc0f500b183fc2233f89bd1a429ee3
S <s>
2023-12-26 19:59:24 +0400

    {"types":"C","runs":120}

--- Review.java
+++ Review.java
@@ -1,32 +1,39 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
 		
-		while(true){
-			String str1= readLine();
-			String str2 = readLine();
-			println(isAnagram(str1, str2));
+		int rows = readInt("Enter number of rows");
+		int collumns = readInt("Enter number of collumns");
+		int[][] Matrix = new int[rows][collumns];
+		if(rows != collumns){
+			println("Matrix is not Magical");
+		}
+		for(int i = 0; i < rows; i++){
+			for(int j = 0; j < collumns; j++){
+				Matrix[i][j] = readInt();
 			}
 		}
+		println(isMagicMatrix(Matrix, rows, collumns));
+	}
+
+	private boolean isMagicMatrix(int[][] matrix, int rows, int collumns) {
+		int[] Array = new int[rows * collumns];
+		int k = 0;
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				Array[k] = matrix[i][j];
+				k++;
+			}
+		}
+		Arrays.sort(Array);
 		
-	
-		private boolean isAnagram (String str1, String str2){
-			
-			int[] string1 = new int[str1.length()]; 
-			int[] string2 = new int[str2.length()];
-			
-			for(int i = 0; i < string1.length; i++){
-				string1[i] = str1.charAt(i);
-				string2[i] = str2.charAt(i);
-			}
-			
-			for(int j = 0; j < string1.length; j++){
-				if(string1[j] == string2[string2.length - 1 - j]){
-					return true;
-				}
-			}
-			
+		for(int i = 0; i < Array.length; i++){
+			if(Array[i] - i != 1);
 			return false;
 		}
+		return true;
+	}
 }
