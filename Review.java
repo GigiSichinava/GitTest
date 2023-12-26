184223be7cb9363acb79e1a321dbe0bcbc02e551
S <s>
2023-12-26 20:08:41 +0400

    {"types":"C","runs":121}

--- Review.java
+++ Review.java
@@ -1,39 +1,27 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram{
-	public void run(){
-		
-		int rows = readInt("Enter number of rows");
-		int collumns = readInt("Enter number of collumns");
-		int[][] Matrix = new int[rows][collumns];
-		if(rows != collumns){
-			println("Matrix is not Magical");
-		}
-		for(int i = 0; i < rows; i++){
-			for(int j = 0; j < collumns; j++){
-				Matrix[i][j] = readInt();
+public class Review extends ConsoleProgram {
+
+	private static final int SENETINEL = -1;
+
+	public void run() {
+		ArrayList<Integer> myList = new ArrayList<Integer>();
+		while (true) {
+			int n = readInt();
+			if (n == SENETINEL) {
+				break;
 			}
+			myList.add(n);
 		}
-		println(isMagicMatrix(Matrix, rows, collumns));
+		printReversedOrder(myList);
+
 	}
 
-	private boolean isMagicMatrix(int[][] matrix, int rows, int collumns) {
-		int[] Array = new int[rows * collumns];
-		int k = 0;
-		for(int i = 0; i < matrix.length; i++){
-			for(int j = 0; j < matrix[0].length; j++){
-				Array[k] = matrix[i][j];
-				k++;
-			}
+	private void printReversedOrder(ArrayList<Integer> myList) {
+		for (int i = myList.size() - 1; i >= 0; i--) {
+			println(myList.get(i));
 		}
-		Arrays.sort(Array);
-		
-		for(int i = 0; i < Array.length; i++){
-			if(Array[i] - i != 1);
-			return false;
-		}
-		return true;
 	}
 }
