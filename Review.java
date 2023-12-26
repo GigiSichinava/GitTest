7d41d3f4c61726a3aed9002b5e5b28d5ae081f1b
S <s>
2023-12-26 16:40:36 +0400

    {"types":"A","runs":103}

---
+++ Review.java
@@ -0,0 +1,24 @@
+import acm.program.ConsoleProgram;
+
+public class Review extends ConsoleProgram{
+	public void run(){
+		
+		String str1= readLine();
+		String str2 = readLine();
+		isAnagram(str1, str2);
+	}
+	
+		private boolean isAnagram (String str1, String str2){
+			
+			int[] string1 = new int[str1.length()]; 
+			int[] string2 = new int[str2.length()];
+			
+			for(int i = 0; i < string1.length; i++){
+				if(string1[i] == string2[string2.length - 1]){
+					return true;
+				}
+			}
+			
+			return false;
+		}
+}
