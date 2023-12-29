d01fb54842056b40d00e8279cd528ec9b5b0cc63
S <s>
2023-12-30 02:11:56 +0400

    {"types":"C","runs":56}

--- JButtonBall.java
+++ JButtonBall.java
@@ -1,4 +1,78 @@
 
-public class JButtonBall {
+import java.awt.event.ActionEvent;
 
+import javax.swing.JButton;
+
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class JButtonBall extends GraphicsProgram {
+
+	private static final int NUM_ROWS = 5;
+	private static final int NUM_COLS = 5;
+	private GOval oval;
+	private int ovalWidth, ovalHeight;
+
+	public void init() {
+		JButton right = new JButton("Move right");
+		add(right, EAST);
+		JButton left = new JButton("Move left");
+		add(left, WEST);
+		JButton down = new JButton("Move down");
+		add(down, SOUTH);
+		JButton up = new JButton("Move up");
+		add(up, NORTH);
+		addActionListeners();
+	}
+
+	public void run() {
+
+		int height = getHeight() / NUM_ROWS;
+		int width = getWidth() / NUM_COLS;
+		for (int i = 0; i < NUM_ROWS; i++) {
+			for (int j = 0; j < NUM_COLS; j++) {
+				GRect rect = new GRect(width, height);
+				add(rect, i * width, j * height);
+			}
+		}
+		ovalHeight = height;
+		ovalWidth = width;
+		oval = new GOval(ovalWidth, ovalHeight);
+		add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String temp = e.getActionCommand();
+		if (temp.equals("Move right")) {
+			if (oval.getX() > 4 * ovalWidth) {
+				return;
+			} else {
+				oval.move(ovalWidth, 0);
+
+			}
+		} else if (temp.equals("Move left")) {
+			if (oval.getX() < ovalWidth / 2) {
+				return;
+			} else {
+				oval.move(-ovalWidth, 0);
+
+			}
+		} else if (temp.equals("Move down")) {
+			if (oval.getY() > 4 * ovalHeight) {
+				return;
+			} else {
+				oval.move(0, ovalHeight);
+
+			}
+		} else if (temp.equals("Move up")) {
+			if (oval.getY() < ovalHeight / 2) {
+				return;
+			} else {
+				oval.move(0, -ovalHeight);
+
+			}
+		}
+	}
 }
