b02e49cc60f8d9ecc495d3a5c987334433ce48c9
S <s>
2023-12-30 01:47:52 +0400

    {"types":"C","runs":34}

--- Review.java
+++ Review.java
@@ -42,7 +42,6 @@ public class Review extends GraphicsProgram{
 		add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
 	}
 	
-	@Override
 	public void actionPerformed(ActionEvent e){
 		String temp = e.getActionCommand();
 		if(temp.equals("Move right")){
