52c8d870dfb6bfb76c313eb944ec2678254f81a8
S <s>
2023-12-30 01:22:28 +0400

    {"types":"C","runs":14}

--- Review.java
+++ Review.java
@@ -1,8 +1,13 @@
 import javax.swing.JButton;
 
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Review extends GraphicsProgram{
+	
+	private static final int NUM_ROWS = 8;
+	private static final int NUM_COLS = 10;
+	
 	
 	public void init(){
 		JButton right = new JButton("Move right");
@@ -17,5 +22,13 @@ public class Review extends GraphicsProgram{
 	
 	public void run(){
 		
+		int height = getHeight() / NUM_ROWS;
+		int width = getWidth() / NUM_COLS;
+		for(int i = 0; i < NUM_ROWS; i++){
+			for(int j = 0; j < NUM_COLS; j++){
+				GRect rect = new GRect(i + height, j + width);
+				add(rect);
+			}
+		}
 	}
 }
