08b27e6603b74e63206e00ea9a2a6afdc1721b03
S <s>
2023-12-30 02:21:48 +0400

    {"types":"C","runs":65}

--- JButtonBall.java
+++ JButtonBall.java
@@ -42,6 +42,7 @@ public class JButtonBall extends GraphicsProgram {
 		ovalHeight = height;
 		ovalWidth = width;
 		oval = new GOval(ovalWidth, ovalHeight);
+		oval.setFilled(true);
 		add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
 	}
 
