987a6c6dc15154f8ba206db14f1d1e895495c836
S <s>
2023-12-30 02:10:24 +0400

    {"types":"C","runs":54}

--- Review.java
+++ Review.java
@@ -7,14 +7,14 @@ import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
-public class Review extends GraphicsProgram{
-	
+public class Review extends GraphicsProgram {
+
 	private static final int NUM_ROWS = 5;
 	private static final int NUM_COLS = 5;
 	private GOval oval;
 	private int ovalWidth, ovalHeight;
-	
-	public void init(){
+
+	public void init() {
 		JButton right = new JButton("Move right");
 		add(right, EAST);
 		JButton left = new JButton("Move left");
@@ -25,13 +25,13 @@ public class Review extends GraphicsProgram{
 		add(up, NORTH);
 		addActionListeners();
 	}
-	
-	public void run(){
-		
+
+	public void run() {
+
 		int height = getHeight() / NUM_ROWS;
 		int width = getWidth() / NUM_COLS;
-		for(int i = 0; i < NUM_ROWS; i++){
-			for(int j = 0; j < NUM_COLS; j++){
+		for (int i = 0; i < NUM_ROWS; i++) {
+			for (int j = 0; j < NUM_COLS; j++) {
 				GRect rect = new GRect(width, height);
 				add(rect, i * width, j * height);
 			}
@@ -41,30 +41,38 @@ public class Review extends GraphicsProgram{
 		oval = new GOval(ovalWidth, ovalHeight);
 		add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
 	}
-	
+
 	@Override
-	public void actionPerformed(ActionEvent e){
+	public void actionPerformed(ActionEvent e) {
 		String temp = e.getActionCommand();
-		if(temp.equals("Move right")){
-			if(oval.getX() > 5 * ovalWidth){
+		if (temp.equals("Move right")) {
+			if (oval.getX() > 5 * ovalWidth) {
 				return;
+			} else {
+				oval.move(ovalWidth, 0);
+
 			}
-			oval.move(ovalWidth, 0);
-		}else if(temp.equals("Move left")){
-			if(oval.getX() < ovalWidth /2 ){
+		} else if (temp.equals("Move left")) {
+			if (oval.getX() < ovalWidth / 2) {
 				return;
+			} else {
+				oval.move(-ovalWidth, 0);
+
 			}
-			oval.move(-ovalWidth, 0);
-		}else if(temp.equals("Move down")){
-			if(oval.getY() > 4.5 * ovalHeight){
+		} else if (temp.equals("Move down")) {
+			if (oval.getY() > 4.5 * ovalHeight) {
 				return;
+			} else {
+				oval.move(0, ovalHeight);
+
 			}
-			oval.move(0, ovalHeight);
-		}else if(temp.equals("Move up")){
-			if(oval.getY() < ovalHeight / 2){
+		} else if (temp.equals("Move up")) {
+			if (oval.getY() < ovalHeight / 2) {
 				return;
+			} else {
+				oval.move(0, -ovalHeight);
+
 			}
-			oval.move(0, -ovalHeight);
 		}
 	}
 }
