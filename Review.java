76ddae387b8643940f45cf31734b2768772006ba
S <s>
2023-12-30 15:46:24 +0400

    {"types":"C","runs":92}

--- Review.java
+++ Review.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 
 import acm.graphics.GLabel;
@@ -10,6 +12,7 @@ public class Review extends GraphicsProgram{
 	private static final int ROW_NUM = 5;
 	private static final int COL_NUM = 5;
 	private int ovalWidth, ovalHeight;
+	private GOval oval;
 	
 	public void init(){
 		JButton down = new JButton ("Move down");
@@ -20,6 +23,7 @@ public class Review extends GraphicsProgram{
 		add(right, EAST);
 		JButton left = new JButton ("Move left");
 		add(left, WEST);
+		addActionListeners();
 	}
 	
 	public void run(){
@@ -35,5 +39,13 @@ public class Review extends GraphicsProgram{
 		ovalHeight = height;
 		GOval oval = new GOval(ovalWidth, ovalHeight);
 		add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		String temp = e.getActionCommand();
+		if(temp.equals("Move down")){
+			oval.move(0, -ovalHeight);
+		}
+		
 	}
 }
