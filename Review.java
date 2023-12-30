72976ce93d72b7baf59d66d8c32ae970691a176c
S <s>
2023-12-30 15:33:01 +0400

    {"types":"C","runs":80}

--- Review.java
+++ Review.java
@@ -1,7 +1,9 @@
+import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 public class Review extends GraphicsProgram{
 	public void run(){
-		
+	GLabel label = new GLabel ("Chemi yle")	;
+	add(label);
 	}
 }
