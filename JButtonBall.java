9ed983cf5213cd75e04055b78b617991b5f1b445
S <s>
2023-12-30 16:17:52 +0400

    {"types":"C","runs":114}

--- JButtonBall.java
+++ JButtonBall.java
@@ -1,121 +1,102 @@
-
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-
 import javax.swing.JButton;
-
 import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class JButtonBall extends GraphicsProgram implements ComponentListener{
+public class JButtonBall extends GraphicsProgram implements ComponentListener {
 
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int NUM_ROWS = 5;
-	private static final int NUM_COLS = 5;
-	private GOval oval;
-	private int ovalWidth, ovalHeight;
+    private RandomGenerator rgen = RandomGenerator.getInstance();
+    private static final int NUM_ROWS = 5;
+    private static final int NUM_COLS = 5;
+    private GOval oval;
+    private int ovalWidth, ovalHeight;
 
-	public void init() {
-		JButton right = new JButton("Move right");
-		add(right, EAST);
-		JButton left = new JButton("Move left");
-		add(left, WEST);
-		JButton down = new JButton("Move down");
-		add(down, SOUTH);
-		JButton up = new JButton("Move up");
-		add(up, NORTH);
-		addActionListeners();
-	}
+    public void init() {
+        JButton right = new JButton("Move right");
+        add(right, EAST);
+        JButton left = new JButton("Move left");
+        add(left, WEST);
+        JButton down = new JButton("Move down");
+        add(down, SOUTH);
+        JButton up = new JButton("Move up");
+        add(up, NORTH);
+        addActionListeners();
 
-	public void run() {
-		updateGrid();
-	}
+        // Register component listener
+        addComponentListener(this);
+    }
 
-	@Override
-	public void componentHidden(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void run() {
+        updateGrid();
+    }
 
-	@Override
-	public void componentMoved(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void componentResized(ComponentEvent e) {
+        updateGrid();
+    }
 
-	@Override
-	public void componentResized(ComponentEvent e) {
-		 updateGrid();		
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (command.equals("Move right")) {
+            moveOval(ovalWidth, 0);
+        } else if (command.equals("Move left")) {
+            moveOval(-ovalWidth, 0);
+        } else if (command.equals("Move down")) {
+            moveOval(0, ovalHeight);
+        } else if (command.equals("Move up")) {
+            moveOval(0, -ovalHeight);
+        }
+    }
 
-	private void updateGrid() {
-		int height = getHeight() / NUM_ROWS;
-		int width = getWidth() / NUM_COLS;
-		for (int i = 0; i < NUM_ROWS; i++) {
-			for (int j = 0; j < NUM_COLS; j++) {
-				GRect rect = new GRect(width, height);
-				rect.setFilled(true);
-				rect.setFillColor(Color.white);
-				add(rect, i * width, j * height);
-			}
-		}
-		ovalHeight = height;
-		ovalWidth = width;
-		oval = new GOval(ovalWidth, ovalHeight);
-		oval.setFilled(true);
-		add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
-	}
+    private void moveOval(int dx, int dy) {
+        if (oval != null) {
+            oval.move(dx, dy);
+            oval.setFilled(true);
+            oval.setColor(rgen.nextColor());
+        }
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String temp = e.getActionCommand();
-		if (temp.equals("Move right")) {
-			if (oval.getX() > 4 * ovalWidth) {
-				return;
-			} else {
-				oval.setFilled(true);
-				oval.setColor(rgen.nextColor());
-				oval.move(ovalWidth, 0);
-			}
-		} else if (temp.equals("Move left")) {
+    private void updateGrid() {
+        removeAll(); // Remove all existing elements
 
-			if (oval.getX() < ovalWidth / 2) {
-				return;
-			} else {
-				oval.setFilled(true);
-				oval.setColor(rgen.nextColor());
-				oval.move(-ovalWidth, 0);
+        int height = getHeight() / NUM_ROWS;
+        int width = getWidth() / NUM_COLS;
 
-			}
-		} else if (temp.equals("Move down")) {
-			if (oval.getY() > 4 * ovalHeight) {
-				return;
-			} else {
-				oval.setFilled(true);
-				oval.setColor(rgen.nextColor());
-				oval.move(0, ovalHeight);
-			}
-		} else if (temp.equals("Move up")) {
-			if (oval.getY() < ovalHeight / 2) {
-				return;
-			} else {
-				oval.setFilled(true);
-				oval.setColor(rgen.nextColor());
-				oval.move(0, -ovalHeight);
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < NUM_COLS; j++) {
+                GRect rect = new GRect(width, height);
+                rect.setFilled(true);
+                rect.setFillColor(Color.white);
+                add(rect, i * width, j * height);
+            }
+        }
 
-			}
-		}
-	}
-		
-	
+        ovalWidth = width;
+        ovalHeight = height;
 
-	@Override
-	public void componentShown(ComponentEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+        // Recreate the oval or reposition it based on new dimensions
+        if (oval == null) {
+            oval = new GOval(ovalWidth, ovalHeight);
+        } else {
+            oval.setSize(ovalWidth, ovalHeight);
+        }
+        oval.setFilled(true);
+
+        // Adjust oval position - center or within new grid bounds
+        add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
+    }
+
+    // Empty implementations for other ComponentListener methods
+    @Override
+    public void componentMoved(ComponentEvent e) {}
+    @Override
+    public void componentShown(ComponentEvent e) {}
+    @Override
+    public void componentHidden(ComponentEvent e) {}
 }
