77903d4f865d1c019c0583a099e84dafc83cc527
S <s>
2023-12-30 15:34:33 +0400

    {"types":"C","runs":83}

--- Review.java
+++ Review.java
@@ -8,6 +8,8 @@ public class Review extends GraphicsProgram{
 	public void init(){
 		JButton down = new JButton ("Move down");
 		add(down, SOUTH);
+		JButton up = new JButton ("Move up");
+		add(up, NORTH);
 	}
 	
 	public void run(){
