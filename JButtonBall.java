482f7eba09d326adad23be8133e8b0f7c7990d1f
S <s>
2023-12-30 16:11:26 +0400

    {"types":"C","runs":108}

--- JButtonBall.java
+++ JButtonBall.java
@@ -53,9 +53,66 @@ public class JButtonBall extends GraphicsProgram implements ComponentListener{
 	}
 
 	private void updateGrid() {
-		// TODO Auto-generated method stub
-		
+		int height = getHeight() / NUM_ROWS;
+		int width = getWidth() / NUM_COLS;
+		for (int i = 0; i < NUM_ROWS; i++) {
+			for (int j = 0; j < NUM_COLS; j++) {
+				GRect rect = new GRect(width, height);
+				rect.setFilled(true);
+				rect.setFillColor(Color.white);
+				add(rect, i * width, j * height);
+			}
+		}
+		ovalHeight = height;
+		ovalWidth = width;
+		oval = new GOval(ovalWidth, ovalHeight);
+		oval.setFilled(true);
+		add(oval, getWidth() / 2 - ovalWidth / 2, getHeight() / 2 - ovalHeight / 2);
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		removeAll();
+		String temp = e.getActionCommand();
+		if (temp.equals("Move right")) {
+			if (oval.getX() > 4 * ovalWidth) {
+				return;
+			} else {
+				oval.setFilled(true);
+				oval.setColor(rgen.nextColor());
+				oval.move(ovalWidth, 0);
+			}
+		} else if (temp.equals("Move left")) {
+
+			if (oval.getX() < ovalWidth / 2) {
+				return;
+			} else {
+				oval.setFilled(true);
+				oval.setColor(rgen.nextColor());
+				oval.move(-ovalWidth, 0);
+
+			}
+		} else if (temp.equals("Move down")) {
+			if (oval.getY() > 4 * ovalHeight) {
+				return;
+			} else {
+				oval.setFilled(true);
+				oval.setColor(rgen.nextColor());
+				oval.move(0, ovalHeight);
+			}
+		} else if (temp.equals("Move up")) {
+			if (oval.getY() < ovalHeight / 2) {
+				return;
+			} else {
+				oval.setFilled(true);
+				oval.setColor(rgen.nextColor());
+				oval.move(0, -ovalHeight);
+
+			}
+		}
+	}
+		
+	
 
 	@Override
 	public void componentShown(ComponentEvent e) {
