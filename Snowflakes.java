87df7876f98fcc2751fbb7eb1878b323ae0ca430
S <s>
2024-01-02 14:11:54 +0400

    {"types":"C","runs":160}

--- Snowflakes.java
+++ Snowflakes.java
@@ -1,36 +1,39 @@
+import java.awt.Color;
+
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class Snowflakes extends GraphicsProgram {
 
-	private static final int NUM_FLAKES = 50;
-	private static final int FLAKES_R = 5;
+	private static final int NUM = 100;
+	private static final int D = 5;
+	private static final int DELAY = 50;
+
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 
 	public void run() {
+		GOval[] snowFlakes = new GOval[NUM];
 
-		GOval[] snowFlakes = new GOval[NUM_FLAKES];
-
-		for (int i = 0; i < NUM_FLAKES; i++) {
-			snowFlakes[i] = new GOval(FLAKES_R, FLAKES_R);
+		for (int i = 0; i < NUM; i++) {
+			snowFlakes[i] = new GOval(D, D);
 			snowFlakes[i].setFilled(true);
-			int x = rgen.nextInt(0, getWidth() - (2 * FLAKES_R));
-			int y = rgen.nextInt(-FLAKES_R, getHeight() - (2 * FLAKES_R));
-			snowFlakes[i].setLocation(x, y);
+			snowFlakes[i].setColor(Color.CYAN);
+			int startX = rgen.nextInt(0, getWidth() - D);
+			int startY = rgen.nextInt(0, getHeight() - D);
+			snowFlakes[i].setLocation(startX, startY);
 			add(snowFlakes[i]);
 		}
 
 		while (true) {
-			for (int j = 0; j < NUM_FLAKES; j++) {
-				snowFlakes[j].move(0, rgen.nextInt(5, 10));
-				if (snowFlakes[j].getY() > getHeight()) {
-					snowFlakes[j].setLocation(rgen.nextInt(0, getWidth() - (2 * FLAKES_R)),
-							rgen.nextInt(-FLAKES_R, getHeight() - (2 * FLAKES_R)));
+			for (int i = 0; i < NUM; i++) {
+				snowFlakes[i].move(0, rgen.nextInt(1, 5));
+
+				if (snowFlakes[i].getY() > getHeight()) {
+					snowFlakes[i].setLocation(rgen.nextInt(0, getWidth() - D), -D);
 				}
 			}
-			pause(50);
-
+			pause(DELAY);
 		}
 	}
 }
