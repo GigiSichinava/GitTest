2be8166dcf9b2d14b3d4a7863ee16c89064a6fc5
S <s>
2024-01-02 13:51:57 +0400

    {"types":"C","runs":141}

--- Review.java
+++ Review.java
@@ -4,33 +4,31 @@ import acm.util.RandomGenerator;
 
 public class Review extends GraphicsProgram {
 
-    private static final int NUM_SNOWFLAKES = 100; // Total number of snowflakes
-    private static final int SNOWFLAKE_SIZE = 5;   // Size of each snowflake
-    private static final int DELAY = 50;           // Animation delay in milliseconds
+    private static final int NUM = 100; 
+    private static final int R = 5;   
+    private static final int DELAY = 50;            
 
     private RandomGenerator rgen = RandomGenerator.getInstance();
 
     public void run() {
-        GOval[] snowflakes = new GOval[NUM_SNOWFLAKES];
+        GOval[] snowflakes = new GOval[NUM];
 
-        // Create and add snowflakes to the screen
-        for (int i = 0; i < NUM_SNOWFLAKES; i++) {
-            snowflakes[i] = new GOval(SNOWFLAKE_SIZE, SNOWFLAKE_SIZE);
+        for (int i = 0; i < NUM; i++) {
+            snowflakes[i] = new GOval(R, R);
             snowflakes[i].setFilled(true);
-            int startX = rgen.nextInt(0, getWidth() - SNOWFLAKE_SIZE);
-            int startY = rgen.nextInt(0, getHeight() - SNOWFLAKE_SIZE);
+            int startX = rgen.nextInt(0, getWidth() - R);
+            int startY = rgen.nextInt(0, getHeight() - R);
             snowflakes[i].setLocation(startX, startY);
             add(snowflakes[i]);
         }
 
-        // Animate snowflakes
         while (true) {
-            for (int i = 0; i < NUM_SNOWFLAKES; i++) {
+            for (int i = 0; i < NUM; i++) {
                 snowflakes[i].move(0, rgen.nextInt(1, 5)); // Move down randomly
 
                 // Reset to top if it goes off the screen
                 if (snowflakes[i].getY() > getHeight()) {
-                    snowflakes[i].setLocation(rgen.nextInt(0, getWidth() - SNOWFLAKE_SIZE), -SNOWFLAKE_SIZE);
+                    snowflakes[i].setLocation(rgen.nextInt(0, getWidth() - R), - R);
                 }
             }
             pause(DELAY);
