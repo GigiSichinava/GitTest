94afce13d7e0898234890f2229f506c2ac46db43
S <s>
2024-01-02 13:48:12 +0400

    {"types":"C","runs":137}

--- Review.java
+++ Review.java
@@ -1,29 +1,34 @@
 import acm.graphics.GOval;
-import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class Review extends GraphicsProgram {
 
-	private static final int R = 5;
-	private static final int NUM_OVAlS = 20;
-	private RandomGenerator rgen = RandomGenerator.getInstance();
+    private static final int R = 5;
+    private static final int NUM_OVALS = 20;
+    private RandomGenerator rgen = RandomGenerator.getInstance();
 
-	public void run() {
+    public void run() {
+        GOval[] ovalArray = new GOval[NUM_OVALS];
 
-		GOval[] ovalArray = new GOval[NUM_OVAlS];
+        // Create and add all GOvals
+        for (int i = 0; i < NUM_OVALS; i++) {
+            ovalArray[i] = new GOval(2 * R, 2 * R);
+            ovalArray[i].setFilled(true);
+            add(ovalArray[i], rgen.nextInt(R, getWidth() - R), -2 * R);
+        }
 
-		for (int i = 0; i < NUM_OVAlS; i++) {
-			while(ovalArray[i].getY() < getHeight()){
-
-			ovalArray[i] = new GOval(2 * R, 2 * R);
-			ovalArray[i].setFilled(true);
-			add(ovalArray[i], rgen.nextInt(R, getWidth() - R), -2 * R);
-			
-				ovalArray[i].move(0, 10);
-				pause(100);
-			}
-		}
-		
-	}
+        // Move all GOvals
+        boolean ovalsMoving = true;
+        while (ovalsMoving) {
+            ovalsMoving = false;
+            for (int i = 0; i < NUM_OVALS; i++) {
+                if (ovalArray[i].getY() < getHeight()) {
+                    ovalArray[i].move(0, 10);
+                    ovalsMoving = true;
+                }
+            }
+            pause(100);
+        }
+    }
 }
