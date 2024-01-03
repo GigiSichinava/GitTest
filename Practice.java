657bccc5df0d4047fca7fc980eadf4c4bc30f3c8
S <s>
2024-01-03 20:52:42 +0400

    {"types":"C","runs":19}

--- Practice.java
+++ Practice.java
@@ -1,27 +1,20 @@
 import acm.program.ConsoleProgram;
 
-public class Practice extends ConsoleProgram {
-    public void run() {
-        int n = readInt("Size of the Array: ");
-        int[] array = new int[n];
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
+		int x = readInt("Check if number is prime: ");
+		isPrime(x);
+	}
 
-        for (int i = 0; i < array.length; i++) {
-            array[i] = readInt("Enter element " + (i + 1) + ": ");
-        }
-        println("Array is sorted: " + isSorted(array));
-    }
-
-    private boolean isSorted(int[] array) {
-        // Check if the array has less than 2 elements
-        if (array.length < 2) {
-            return true;
-        }
-
-        for (int j = 0; j < array.length - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
+	private boolean isPrime(int x) {
+		double checkBefore = Math.sqrt(x);
+		for(int i = 0; i <= checkBefore; i++){
+			if(x % i == 0){
+				return false;
+			}
+		}
+		
+		return true;		
+	}
 }
