3f1db4d650517dc20d1f0a71a1bb5ad2f05a52a7
S <s>
2024-01-03 20:39:55 +0400

    {"types":"C","runs":5}

--- Review.java
+++ Review.java
@@ -1,15 +1,28 @@
-import javax.swing.JButton;
-import javax.swing.JTextField;
+import java.awt.Color;
 
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class Review extends GraphicsProgram{
+	
+	private static final int NUM = 100;
+	private static final int R = 5;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	
 	public void run(){
 		
-		JButton button = new JButton("Press me!");
-		add(button, SOUTH);
-		JTextField text = new JTextField(10);
-		add(text, SOUTH);
+		GOval[] flakes = new GOval[NUM];
+		
+		for(int i = 0; i < NUM; i++){
+			flakes[i] = new GOval(2 * R, 2 * R);
+			flakes[i].setFilled(true);
+			flakes[i].setColor(Color.BLUE);
+			add(flakes[i]);
+			flakes[i].setLocation(rgen.nextInt(0, getWidth() - 2 * R), rgen.nextInt(0, getHeight() - 2 * R));
+		}
+	
 		
 	}
 }
