a5089942f6a5068e1492f46162e18837c718c34d
S <s>
2024-01-03 21:56:37 +0400

    {"types":"AR","runs":45}

---
+++ Problem63.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.StringTokenizer;
+
+import acm.program.ConsoleProgram;
+
+public class Problem63 extends ConsoleProgram {
+	
+	public void run() {
+	
+		Map <String, Collection<String>> map =
+				new HashMap<String,Collection<String>>();
+		
+		while (true) {
+			String line = readLine();
+			if (line.isEmpty()) {
+				break;
+			}
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			String a = tokenizer.nextToken();
+			String b = tokenizer.nextToken();
+			map.putIfAbsent(b, new HashSet<String>());
+			map.get(b).add(a);
+		}
+		
+		println(map);
+		
+	}
+}
