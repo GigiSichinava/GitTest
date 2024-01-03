ccf290621ab7ccbef8a5e13a46faec95680acc27
S <s>
2024-01-03 23:35:56 +0400

    {"types":"C","runs":52}

--- Practice.java
+++ Practice.java
@@ -1,31 +1,24 @@
+import java.awt.event.ActionEvent;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
-import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class Practice extends ConsoleProgram{
-	private static final String SENTINEL = "";
-	public void run() {
-		Map<String, Integer> myMap = 
-				new HashMap<String, Integer>();
-		
-		while(true) {
-			String name = readLine("Enter name: ");
-			if(name.equals(SENTINEL)) {
-				break;
-			}
-			
-			if(!myMap.containsKey(name)) {
-				myMap.put(name, 0);
-			}
-			
-			int newCount = myMap.get(name) + 1;
-			myMap.put(name, newCount);
-		}
-		
-		println(myMap);
-		
+public class Practice extends GraphicsProgram{
+
+	@Override
+	public void init(){
+		JButton button = new JButton("Enter");
+		add(button, SOUTH);
+		button.addActionListener(this);
+		JTextField textField = new JTextField(30);
+		add(textField, SOUTH);
+		textField.addActionListener(this);
 	}
-
+	
+	public void actionPerformed(ActionEvent e){
+		String line = e.getActionCommand();
+		println(line);
+	}
 }
