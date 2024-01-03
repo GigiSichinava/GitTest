4ed1c4b2f13ff4b68691dd37aaa31aa62f5f1a65
S <s>
2024-01-03 23:25:19 +0400

    {"types":"C","runs":51}

--- Problem67.java
+++ Problem67.java
@@ -1,4 +1,26 @@
+import java.awt.event.ActionEvent;
 
-public class Problem67 {
+import javax.swing.JTextField;
 
+import acm.graphics.GImage;
+import acm.program.GraphicsProgram;
+import acm.util.ErrorException;
+
+public class Problem67 extends GraphicsProgram {
+
+	public void init() {
+		JTextField textField = new JTextField(30);
+		add(textField, SOUTH);
+		textField.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		String adress = e.getActionCommand();
+		try {
+			GImage image = new GImage(adress);
+			add(image);
+		} catch (ErrorException fix) {
+			println("No Image Found!");
+		}
+	}
 }
