567d270a3a8ff69e786e3234e38c986214c93880
S <s>
2024-01-03 23:48:43 +0400

    {"types":"C","runs":56}

--- Practice.java
+++ Practice.java
@@ -3,12 +3,15 @@ import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
+import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
 public class Practice extends GraphicsProgram{
 
 	private JButton button;
 	private JTextField textField;
+	private GLabel lastLabel;
+
 	
 	@Override
 	public void init(){
@@ -27,6 +30,18 @@ public class Practice extends GraphicsProgram{
 			return;
 		}
 		textField.setText("");
-		
+		addLabel(line);
+	}
+
+
+	private void addLabel(String line) {
+		if(lastLabel == null){
+			lastLabel = new GLabel(line);
+			add(lastLabel, 0, lastLabel.getAscent());
+		}else{
+			GLabel newLabel = new GLabel(line);
+			add(newLabel, 0, lastLabel.getY() + newLabel.getAscent());
+			lastLabel = newLabel;
+		}
 	}
 }
