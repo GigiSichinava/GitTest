929ad29044fbea3fdaf44567d7466885711d4f1f
S <s>
2024-01-03 21:52:47 +0400

    {"types":"C","runs":40}

--- Review.java
+++ Review.java
@@ -1,26 +1,7 @@
-import java.util.HashMap;
-
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
-	
-	private String SENTINEL = "";
-	
 	public void run(){
 		
-		HashMap<String, Integer> myMap = new HashMap<String, Integer>();
-		
-		while(true){
-			String name = readLine();
-			if(name.equals(SENTINEL)){
-				break;
-			}
-			if(!myMap.containsKey(name)){
-				myMap.put(name, 0);
-			}
-			int newCount = myMap.get(name) + 1;
-			myMap.put(name, newCount);
-		}
-		println(myMap);
 	}
 }
