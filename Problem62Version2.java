8ee781040c94a7027805eedcd59a79200cff6315
S <s>
2024-01-03 21:22:51 +0400

    {"types":"C","runs":36}

--- Problem62Version2.java
+++ Problem62Version2.java
@@ -1,4 +1,27 @@
+import java.util.HashMap;
 
-public class Problem62Version2 {
+import acm.program.ConsoleProgram;
 
+public class Problem62Version2 extends ConsoleProgram {
+
+	private String SENTINEL = "";
+
+	public void run() {
+
+		HashMap<String, Integer> myMap = new HashMap<String, Integer>();
+
+		while (true) {
+			String name = readLine("Enter name: ");
+			if (name.equals(SENTINEL)) {
+				break;
+			}
+			if (!myMap.containsKey(name)) {
+				myMap.put(name, 0);
+			}
+			int newCount = myMap.get(name) + 1;
+			myMap.put(name, newCount);
+
+		}
+		println(myMap);
+	}
 }
