f054c2b6dd475488ace87d6fbd23d4761df3657b
S <s>
2024-01-03 21:55:37 +0400

    {"types":"C","runs":41}

--- Problem63.java
+++ Problem63.java
@@ -1,37 +1,32 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.StringTokenizer;
-
+import java.util.*;
 import acm.program.ConsoleProgram;
 
 public class Problem63 extends ConsoleProgram {
-	
-	public void run() {
-	
-		Map <String, Collection<String>> map =
-				new HashMap<String,Collection<String>>();
-		
-		while (true) {
-			String line = readLine();
-			if (line.isEmpty()) {
-				break;
-			}
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			String a = tokenizer.nextToken();
-			String b = tokenizer.nextToken();
-			map.putIfAbsent(b, new HashSet<String>());
-			map.get(b).add(a);
-		}
-		
-		println(map);
-		
-	}
+    
+    public void run() {
+        Map<String, Collection<String>> map = new HashMap<>();
+
+        while (true) {
+            String line = readLine("Enter key and value separated by space (or just press Enter to finish): ");
+            if (line.isEmpty()) {
+                break;
+            }
+
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            if (tokenizer.countTokens() < 2) {
+                println("Invalid input. Please enter a key and a value separated by space.");
+                continue;
+            }
+
+            String key = tokenizer.nextToken();
+            String value = tokenizer.nextToken();
+
+            map.putIfAbsent(key, new HashSet<>());
+            map.get(key).add(value);
+        }
+
+        for (Map.Entry<String, Collection<String>> entry : map.entrySet()) {
+            println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 }
