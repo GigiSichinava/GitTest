9e6421d7f010c9a1a3a674f8472e953ab25337f7
S <s>
2024-01-06 01:56:34 +0400

    {"types":"C","runs":54}

--- evalMidterm.java
+++ evalMidterm.java
@@ -6,7 +6,7 @@ public class evalMidterm extends ConsoleProgram{
 	public void run(){
 		
 		String line = "27 + 3 - 20";
-		println("Your equasion is: " + line);
+		println(line);
 		
 		int result = 0;
 		int currentNum = 0;
@@ -28,6 +28,6 @@ public class evalMidterm extends ConsoleProgram{
 				}
 			}
 		}
-		println("Your final result is: " + result);
+		println(result);
 	}
 }
