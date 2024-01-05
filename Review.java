e3a8a3a3c24b701e53049fdf5b75f731b3e1ec7b
S <s>
2024-01-05 20:41:36 +0400

    {"types":"C","runs":2}

--- Review.java
+++ Review.java
@@ -1,27 +1,9 @@
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.StringTokenizer;
-
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
 		
-		Map<String, Collection<String>> myMap = new HashMap<String, Collection<String>>();
-	
-		while(true){
-			String line = readLine();
-			if(line.isEmpty()){
-				break;
-			}
-			StringTokenizer tokenizer = new StringTokenizer(line);
-			String a = tokenizer.nextToken();
-			String b = tokenizer.nextToken();
-			myMap.putIfAbsent(b, new HashSet<String>());
-			myMap.get(b).add(a);
-		}
-		println(myMap);
+		String line = "10 + 2 - 5";
+		println(line);
 	}
 }
