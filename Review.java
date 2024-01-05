73af5c235f4e1840283c6c3932c6aae75b4186ec
S <s>
2024-01-06 03:32:57 +0400

    {"types":"C","runs":78}

--- Review.java
+++ Review.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -16,6 +18,7 @@ public class Review extends GraphicsProgram{
 	public void init(){
 		label1 = new JLabel("User 1");
 		field1 = new JTextField(10);	
+		field1.addActionListener(this);
 		button1 = new JButton("Send");
 	
 		add(label1, WEST);
@@ -26,12 +29,21 @@ public class Review extends GraphicsProgram{
 
 		label2 = new JLabel("User 2");
 		field2 = new JTextField(10);
+		field2.addActionListener(this);
 		button2 = new JButton("Send");
 		
 		add(label2, EAST);
 		add(field2, EAST);
 		add(button2, EAST);
 
-		
+		addActionListeners();
 	}
+	
+	public void ActionPerformed(ActionEvent e){
+		String temp = e.getActionCommand();
+		if(e.getSource() == field1 || e.getSource() == button1){
+			println(field1.getText());
+		}
+	}
+	
 }
