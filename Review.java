3472fe6e6ecb4267d5ecb45f28393f2d5ac530fd
S <s>
2024-01-05 22:15:12 +0400

    {"types":"C","runs":39}

--- Review.java
+++ Review.java
@@ -2,25 +2,32 @@ import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram{
-	public void run(){
-		
+public class Review extends ConsoleProgram {
+	public void run() {
+
 		String line = "10 + 2 - 5";
 		println(line);
-		
+
 		int result = 0;
 		int currentNumber = 0;
 		char opperation = (Character) null;
-		
+
 		StringTokenizer tokenizer = new StringTokenizer(line, " +-");
-		
-		while(tokenizer.hasMoreTokens()){
+
+		while (tokenizer.hasMoreTokens()) {
 			String part = tokenizer.nextToken();
-			if(part.equals("+") || part.equals("-")){
+			if (part.equals("+") || part.equals("-")) {
 				opperation = part.charAt(0);
-			}else{
+			} else {
 				currentNumber = Integer.parseInt(part);
+
+				if (opperation == '+') {
+					result += currentNumber;
+				} else if (opperation == '-') {
+					result -= currentNumber;
+				}
 			}
 		}
+		println(result);
 	}
 }
