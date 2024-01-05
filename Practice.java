c151b91eeb1d049797e774fc8c7cf25a3bf7e51b
S <s>
2024-01-05 22:12:35 +0400

    {"types":"C","runs":38}

--- Practice.java
+++ Practice.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 	
-		String line = "g";
+		String line = "12";
 		int a = Integer.parseInt(line);
 		println(a);
 	}
