e6705f8dfbdd8cf2ce543e60568bf8aa06bc6f8c
S <s>
2024-01-05 22:12:08 +0400

    {"types":"C","runs":34}

--- Practice.java
+++ Practice.java
@@ -1,47 +1,10 @@
-import java.awt.event.ActionEvent;
+import acm.program.ConsoleProgram;
 
-import javax.swing.JButton;
-import javax.swing.JTextField;
-
-import acm.graphics.GLabel;
-import acm.program.GraphicsProgram;
-
-public class Practice extends GraphicsProgram{
-
-	private JButton button;
-	private JTextField textField;
-	private GLabel lastLabel;
-
+public class Practice extends ConsoleProgram{
+	public void run(){
 	
-	@Override
-	public void init(){
-		JButton button = new JButton("Enter");
-		add(button, SOUTH);
-		button.addActionListener(this);
-		JTextField textField = new JTextField(30);
-		add(textField, SOUTH);
-		textField.addActionListener(this);
-	}
-	
-	
-	public void actionPerformed(ActionEvent e){
-		String line = textField.getText();
-		if(line.isEmpty()){
-			return;
-		}
-		textField.setText("");
-		addLabel(line);
-	}
-
-
-	private void addLabel(String line) {
-		if(lastLabel == null){
-			lastLabel = new GLabel(line);
-			add(lastLabel, 0, lastLabel.getAscent());
-		}else{
-			GLabel newLabel = new GLabel(line);
-			add(newLabel, 0, lastLabel.getY() + newLabel.getAscent());
-			lastLabel = newLabel;
-		}
+		String line = "23";
+		int a = Integer.parseInt(line);
+		println(a);
 	}
 }
--- Review.java
+++ Review.java
@@ -1,32 +1,26 @@
 import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
-public class Review extends ConsoleProgram {
-    public void run() {
-        String line = "11 + 2 - 5";
-        println("Expression: " + line);
-
-        StringTokenizer tokenizer = new StringTokenizer(line, " ");
-        int result = 0;
-        int currentNumber = 0;
-        char operation = '+';
-
-        while (tokenizer.hasMoreTokens()) {
-            String part = tokenizer.nextToken();
-
-            if (part.equals("+") || part.equals("-")) {
-                operation = part.charAt(0);
-            } else {
-                currentNumber = Integer.parseInt(part);
-
-                if (operation == '+') {
-                    result += currentNumber;
-                } else if (operation == '-') {
-                    result -= currentNumber;
-                }
-            }
-        }
-
-        println("Result: " + result);
-    }
+public class Review extends ConsoleProgram{
+	public void run(){
+		
+		String line = "10 + 2 - 5";
+		println(line);
+		
+		int result = 0;
+		int currentNumber = 0;
+		char opperation = (Character) null;
+		
+		StringTokenizer tokenizer = new StringTokenizer(line, " +-");
+		
+		while(tokenizer.hasMoreTokens()){
+			String part = tokenizer.nextToken();
+			if(part.equals("+") || part.equals("-")){
+				opperation = part.charAt(0);
+			}else{
+				currentNumber = Integer.parseInt(part);
+			}
+		}
+	}
 }
