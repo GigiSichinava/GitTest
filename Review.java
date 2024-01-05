9336043a05d3c5fd8db6e13ea720f72a100e4290
S <s>
2024-01-06 03:26:21 +0400

    {"types":"C","runs":73}

--- Review.java
+++ Review.java
@@ -1,7 +1,35 @@
-import acm.program.ConsoleProgram;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
-public class Review extends ConsoleProgram{
-	public void run(){
+import acm.program.GraphicsProgram;
+
+public class Review extends GraphicsProgram{
+	
+	private JTextField field1;
+	private JTextField field2;
+	private JLabel label1;
+	private JLabel label2;
+	private JButton button1;
+	private JButton button2;
+	
+	public void init(){
+		field1 = new JTextField(30);
+		add(field1, WEST);
 		
+		field2 = new JTextField(30);
+		add(field2, EAST);
+		
+		label1 = new JLabel("User 1");
+		add(label1, WEST);
+		
+		label2 = new JLabel("User 2");
+		add(label2, EAST);
+		
+		button1 = new JButton("Send");
+		add(button1, WEST);
+		
+		button2 = new JButton("Send");
+		add(button2, EAST);
 	}
 }
