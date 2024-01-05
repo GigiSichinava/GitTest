6d1e08633a726189df7c884f05a021e9e43fb8e0
S <s>
2024-01-06 01:54:13 +0400

    {"types":"C","runs":48}

--- Review.java
+++ Review.java
@@ -1,32 +1,7 @@
-import java.util.StringTokenizer;
-
 import acm.program.ConsoleProgram;
 
 public class Review extends ConsoleProgram{
 	public void run(){
 		
-		String line = "50 + 11 - 43";
-		println(line);
-		
-		int result = 0;
-		char operation = '+';
-		int currentNumber = 0;
-		
-		StringTokenizer tokenizer = new StringTokenizer(line, " ");
-		while(tokenizer.hasMoreTokens()){
-			String part = tokenizer.nextToken();
-			if(part.equals("+") || part.equals("-")){
-				operation = part.charAt(0);
-			}else{
-				currentNumber = Integer.parseInt(part);
-				
-				if(operation == '+'){
-					result += currentNumber;
-				}else if(operation == '-'){
-					result -= currentNumber;
-				}
-			}
-		}
-		println(result);
 	}
 }
--- evalMidterm.java
+++ evalMidterm.java
@@ -1,4 +1,33 @@
+import java.util.StringTokenizer;
 
-public class evalMidterm {
+import acm.program.ConsoleProgram;
 
+public class evalMidterm extends ConsoleProgram{
+	public void run(){
+		
+		String line = "50 + 14 - 67";
+		println(line);
+		
+		int result = 0;
+		int currentNum = 0;
+		char symbol = '+';
+		
+		
+		StringTokenizer tokenizer = new StringTokenizer(line, " ");
+		while(tokenizer.hasMoreTokens()){
+			String part = tokenizer.nextToken();
+			if(part.equals("+") || part.equals("-")){
+				symbol = part.charAt(0);
+			}else{
+				currentNum = Integer.parseInt(part);
+				
+				if(symbol == '+'){
+					result += currentNum;
+				}else if(symbol == '-'){
+					result -= currentNum;
+				}
+			}
+		}
+		println(result);
+	}
 }
