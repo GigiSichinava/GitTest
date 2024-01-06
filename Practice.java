ac87596d74d344832b9b178532c5d6d383fb6f69
S <s>
2024-01-06 17:48:24 +0400

    {"types":"C","runs":147}

--- Practice.java
+++ Practice.java
@@ -4,22 +4,31 @@ import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class Practice extends GraphicsProgram{
-	
+public class Practice extends GraphicsProgram {
+
 	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
+
 	private static final int R = 5;
 	private static final int NUM = 100;
 	private GOval oval;
-	
-	public void run(){
+
+	public void run() {
 		GOval[] myArray = new GOval[NUM];
-		for(int i = 0; i < NUM; i++){
+		for (int i = 0; i < NUM; i++) {
 			myArray[i] = new GOval(2 * R, 2 * R);
 			myArray[i].setFilled(true);
 			myArray[i].setColor(Color.CYAN);
-			add(myArray[i],(rgen.nextInt(0, getWidth() - 2 * R)), rgen.nextInt(0, getHeight() - 2 * R));
+			add(myArray[i], (rgen.nextInt(0, getWidth() - 2 * R)), rgen.nextInt(0, getHeight() - 2 * R));
 		}
-		
+		for (int i = 0; i < NUM; i++) {
+			while (true) {
+				myArray[i].move(0, 3);
+				pause(50);
+				if(myArray[i].getY() > getHeight()){
+					add(myArray[i], rgen.nextInt(0, getWidth() - 2 * R), rgen.nextInt(0, getHeight() - 2 * R));
+				}
+			}
+		}
 	}
+
 }
