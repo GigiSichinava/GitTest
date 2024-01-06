49a11594d9b594ed3939ae0ae0101c5c57e66bf3
S <s>
2024-01-07 03:05:45 +0400

    {"types":"C","runs":232}

--- Practice.java
+++ Practice.java
@@ -6,54 +6,52 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
 public class Practice extends GraphicsProgram {
-    
-    private RandomGenerator rgen = RandomGenerator.getInstance();
-    private static final int N_COLS = 9;
-    private static final int N_ROWS = 9;
-    
-    private GOval oval1;
-    private GOval oval2;
-    
-    private boolean isRemoved = false;
-    
-    public void run() {
-        setSize(500, 500);
-        int rectWidth = getWidth() / N_COLS;
-        int rectHeight = getHeight() / N_ROWS;
-        
-        for (int i = 0; i < N_COLS; i++) {
-            for (int j = 0; j < N_ROWS; j++) {
-                GRect rect = new GRect(rectWidth, rectHeight);
-                add(rect, rectWidth * i, rectHeight * j);
-            }
-        }
-        
-        oval1 = new GOval(rectWidth, rectHeight); // use the class-level variable
-        oval1.setFilled(true);
-        oval1.setFillColor(rgen.nextColor());
-        add(oval1);
-        
-        
-        addMouseListeners();
-    }
-    
-    public void mouseClicked(MouseEvent e) {
-        GObject obj = getElementAt(e.getX(), e.getY());
-        if (obj == oval1 && !isRemoved) {
-            remove(oval1);
-            isRemoved = true;
-        } else {
-            // Calculate the nearest grid position for the oval
-            int rectWidth = getWidth() / N_COLS;
-            int rectHeight = getHeight() / N_ROWS;
-            
-            int gridX = (e.getX() / rectWidth) * rectWidth;
-            int gridY = (e.getY() / rectHeight) * rectHeight;
 
-            // Add oval to the nearest grid position
-            add(oval1, gridX, gridY); 
-            isRemoved = false;
-        }
-    }
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int N_COLS = 9;
+	private static final int N_ROWS = 9;
+
+	private GOval oval1;
+
+	private boolean isRemoved = false;
+
+	public void run() {
+		setSize(500, 500);
+		int rectWidth = getWidth() / N_COLS;
+		int rectHeight = getHeight() / N_ROWS;
+
+		for (int i = 0; i < N_COLS; i++) {
+			for (int j = 0; j < N_ROWS; j++) {
+				GRect rect = new GRect(rectWidth, rectHeight);
+				add(rect, rectWidth * i, rectHeight * j);
+			}
+		}
+
+		oval1 = new GOval(rectWidth, rectHeight); // use the class-level
+													// variable
+		oval1.setFilled(true);
+		oval1.setFillColor(rgen.nextColor());
+		add(oval1);
+
+		addMouseListeners();
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		GObject obj = getElementAt(e.getX(), e.getY());
+		if (obj == oval1 && !isRemoved) {
+			remove(oval1);
+			isRemoved = true;
+		} else {
+
+			int rectWidth = getWidth() / N_COLS;
+			int rectHeight = getHeight() / N_ROWS;
+
+			int gridX = (e.getX() / rectWidth) * rectWidth;
+			int gridY = (e.getY() / rectHeight) * rectHeight;
+
+			add(oval1, gridX, gridY);
+			isRemoved = false;
+		}
+	}
 
 }
