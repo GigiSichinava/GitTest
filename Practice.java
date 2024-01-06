9b7d5acf217e040af89dd512dde1b0f0ec9ee835
S <s>
2024-01-07 01:52:21 +0400

    {"types":"C","runs":180}

--- Practice.java
+++ Practice.java
@@ -1,3 +1,4 @@
+import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
@@ -5,6 +6,8 @@ public class Practice extends GraphicsProgram{
 	
 	private static final int N_COLS = 9;
 	private static final int N_ROWS = 9;
+	int rectWidth = getWidth() / N_COLS;
+	int rectHeight = getHeight() / N_ROWS;
 	
 	public void run(){
 		
@@ -12,12 +15,13 @@ public class Practice extends GraphicsProgram{
 		
 		for(int i = 0; i < N_COLS; i++){
 			for(int j = 0; j < N_ROWS; j++){
-				int rectWidth = getWidth() / N_COLS;
-				int rectHeight = getHeight() / N_ROWS;
+				
 				GRect rect = new GRect(rectWidth, rectHeight);
 				add(rect, rectWidth * i, rectHeight * j);
 			}
 		}
 		
+		GOval oval = new GOval(rectWidth, rectHeight);
+		add(oval);
 	}
 }
