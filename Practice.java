7cc4a92fba4f860f3db75617683598a40e702114
S <s>
2024-01-07 01:49:10 +0400

    {"types":"C","runs":174}

--- Practice.java
+++ Practice.java
@@ -1,8 +1,23 @@
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Practice extends GraphicsProgram{
+	
+	private static final int N_COLS = 5;
+	private static final int N_ROWS = 8;
+	
 	public void run(){
 		
 		setSize(300, 500);
+		
+		for(int i = 0; i < N_COLS; i++){
+			for(int j =0; j < N_ROWS; j++){
+				int rectWidth = getWidth() / N_COLS;
+				int rectHeight = getHeight() / N_ROWS;
+				GRect rect = new GRect(rectWidth, rectHeight);
+				add(rect, rectWidth * i, rectHeight * j);
+			}
+		}
+		
 	}
 }
