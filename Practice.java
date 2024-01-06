cb843401c7d51cec8454c8419dee3770634506d7
S <s>
2024-01-06 17:04:35 +0400

    {"types":"C","runs":135}

--- Practice.java
+++ Practice.java
@@ -3,12 +3,9 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
-		String line = "10 + 5 - 6";
+		String line = "10 + 5 - 6 + 124 - 345 + 3254";
 		println(line);
 		
-		int a = 5;
-		int b = 6;
-		int result = a + b;
-		println(result);
+		
 	}
 }
