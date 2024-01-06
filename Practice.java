23c56712676d35f8147f2182909ef9adb90c4b89
S <s>
2024-01-06 17:17:04 +0400

    {"types":"C","runs":136}

--- Practice.java
+++ Practice.java
@@ -1,11 +1,32 @@
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
-		String line = "10 + 5 - 6 + 124 - 345 + 3254";
+		String line = "1 + 4 - 2";
 		println(line);
+		int result = 0;
+		int currentNumber = 0;
+		char opperation = '+';
 		
+		StringTokenizer tokenizer = new StringTokenizer(line ," ");
+		while(tokenizer.hasMoreTokens()){
+			String part = tokenizer.nextToken();
+			if(part.equals("+") || part.equals("-")){
+				opperation = part.charAt(0);
+			}else{
+				currentNumber = Integer.parseInt(part);
+				
+				if(opperation == '+'){
+					result += currentNumber;
+				}else if(opperation == '-'){
+					result -= currentNumber;
+				}
+			}
+		}
+		println(result);
 		
 	}
 }
