606bbe64fd61e5f885d1feb716093f96f68ce110
S <s>
2024-01-06 04:33:21 +0400

    {"types":"C","runs":110}

--- Review.java
+++ Review.java
@@ -50,7 +50,7 @@ public class Review extends GraphicsProgram{
 		if(e.getSource() == field1 || e.getSource() == button1){
 			text1 = new GLabel(field1.getText());
 			text1Y += text1.getAscent() * 2;
-			add(text1, 0, text1Y);
+			add(text1, 0, 0);
 			field1.setText("");
 		}
 		if(e.getSource() == field2 || e.getSource() == button2){
