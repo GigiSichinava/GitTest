b0800f4906be3e83d4a137eabe4cc87856ce2ba7
S <s>
2024-01-06 17:02:31 +0400

    {"types":"C","runs":133}

--- Practice.java
+++ Practice.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class Practice extends ConsoleProgram{
 	public void run(){
 		
-		String line = "Ema";
+		String line = "10 + 5 - 6";
 		println(line);
 	}
 }
