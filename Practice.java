ed0771aac5c1efb631910fb4ccfd74376b3aa283
S <s>
2024-01-07 02:15:19 +0400

    {"types":"C","runs":193}

--- Practice.java
+++ Practice.java
@@ -1,54 +1,53 @@
 import java.awt.event.MouseEvent;
-
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class Practice extends GraphicsProgram{
-	
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int N_COLS = 9;
-	private static final int N_ROWS = 9;
-	
-	private GOval oval1;
-	private GOval oval2;
-	
-	public void run(){
-		
-		setSize(500, 500);
-		int rectWidth = getWidth() / N_COLS;
-		int rectHeight = getHeight() / N_ROWS;
-		
-		for(int i = 0; i < N_COLS; i++){
-			for(int j = 0; j < N_ROWS; j++){
-				
-				GRect rect = new GRect(rectWidth, rectHeight);
-				add(rect, rectWidth * i, rectHeight * j);
-			}
-		}
-		
-		oval1 = new GOval(rectWidth, rectHeight);
-		oval1.setFilled(true);
-		oval1.setColor(rgen.nextColor());
-		add(oval1);
-		
-		oval2 = new GOval(rectWidth, rectHeight);
-		oval2.setFilled(true);
-		oval2.setColor(rgen.nextColor());
-		add(oval2, rectWidth, rectHeight);	
-	
-		addMouseListeners();
-	}
-	
-	public void mouseClicked1(MouseEvent e1){
-		GObject obj = getElementAt(e1.getX(), e1.getY());
-		if(obj == oval1){
-			remove(oval1);
-		}
-	}
-	public void mouseClicked2(MouseEvent e2){
-		add(oval1, e2.getX(), e2.getY());
-	}
+public class Practice extends GraphicsProgram {
+    
+    private RandomGenerator rgen = RandomGenerator.getInstance();
+    private static final int N_COLS = 9;
+    private static final int N_ROWS = 9;
+    
+    private GOval oval1;
+    private GOval oval2;
+    private boolean oval1Removed = false;
+    
+    public void run() {
+        setSize(500, 500);
+        int rectWidth = getWidth() / N_COLS;
+        int rectHeight = getHeight() / N_ROWS;
+        
+        for (int i = 0; i < N_COLS; i++) {
+            for (int j = 0; j < N_ROWS; j++) {
+                GRect rect = new GRect(rectWidth, rectHeight);
+                add(rect, rectWidth * i, rectHeight * j);
+            }
+        }
+        
+        oval1 = new GOval(rectWidth, rectHeight);
+        oval1.setFilled(true);
+        oval1.setColor(rgen.nextColor());
+        add(oval1);
+        
+        oval2 = new GOval(rectWidth, rectHeight);
+        oval2.setFilled(true);
+        oval2.setColor(rgen.nextColor());
+        add(oval2, rectWidth, rectHeight);
+        
+        addMouseListeners();
+    }
+    
+    public void mouseClicked(MouseEvent e) {
+        GObject obj = getElementAt(e.getX(), e.getY());
+        if (obj == oval1 && !oval1Removed) {
+            remove(oval1);
+            oval1Removed = true;
+        } else if (oval1Removed) {
+            add(oval1, e.getX() - oval1.getWidth() / 2, e.getY() - oval1.getHeight() / 2);
+            oval1Removed = false;
+        }
+    }
 }
