071dba41fa205c4e98daf045ed63c668c1525784
S <s>
2024-01-06 04:46:43 +0400

    {"types":"C","runs":129}

--- Review.java
+++ Review.java
@@ -7,63 +7,61 @@ import javax.swing.JTextField;
 import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 
-public class Review extends GraphicsProgram{
-	
+public class Review extends GraphicsProgram {
+
 	private JTextField field1;
 	private JTextField field2;
 	private JLabel label1;
 	private JLabel label2;
 	private JButton button1;
 	private JButton button2;
-	
+
 	private GLabel text1;
 	private GLabel text2;
-	
+
 	private int text1Y;
 	private int text2Y;
-	
+
 	private String friend1;
 	private String frind2;
-	
-	public void init(){
+
+	public void init() {
 		label1 = new JLabel("User 1");
-		field1 = new JTextField(20);	
+		field1 = new JTextField(15);
 		field1.addActionListener(this);
 		button1 = new JButton("Send");
-	
+
 		add(label1, WEST);
 		add(field1, WEST);
 		add(button1, WEST);
-		
-		
 
 		label2 = new JLabel("User 2");
-		field2 = new JTextField(20);
+		field2 = new JTextField(15);
 		field2.addActionListener(this);
 		button2 = new JButton("Send");
-		
+
 		add(label2, EAST);
 		add(field2, EAST);
 		add(button2, EAST);
 
 		addActionListeners();
 	}
-	
-	public void actionPerformed(ActionEvent e){
-		if(e.getSource() == field1 || e.getSource() == button1){
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == field1 || e.getSource() == button1) {
 			text1 = new GLabel("User 1: " + field1.getText());
 			add(text1, 0, text1.getAscent() + text1Y);
 			text1Y += text1.getAscent() * 2;
-            text1.setFont("Dialog-15");
+			text1.setFont("Dialog-18");
 			field1.setText("");
 		}
-		if(e.getSource() == field2 || e.getSource() == button2){
+		if (e.getSource() == field2 || e.getSource() == button2) {
 			text2 = new GLabel("User 2: " + field2.getText());
 			text2Y += text2.getAscent() * 2;
 			add(text2, getWidth() - text2.getWidth() * 1.5, text2Y);
-            text2.setFont("Dialog-15");
+			text2.setFont("Dialog-18");
 			field2.setText("");
 		}
 	}
-	
+
 }
