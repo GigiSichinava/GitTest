5566addb97d0d89b3134f928f7a6582b29079e15
S <s>
2024-01-07 23:09:25 +0400

    {"types":"C","runs":242}

--- Practice.java
+++ Practice.java
@@ -1,15 +1,22 @@
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram {
-	public void run() {
+    public void run() {
+        int[][] myMatrix = new int[5][5];
 
-		int[][] myMatrix = new int[5][5];
+        // Fill the matrix
+        for (int i = 0; i < myMatrix.length; i++) {
+            for (int j = 0; j < myMatrix[i].length; j++) {
+                myMatrix[i][j] = 3;
+            }
+        }
 
-		for (int i = 0; i < myMatrix[0].length; i++) {
-			for (int j = 0; j < myMatrix.length; j++) {
-
-				myMatrix[i][j] = 3;
-			}
-		}
-	}
+        // Print the matrix
+        for (int i = 0; i < myMatrix.length; i++) {
+            for (int j = 0; j < myMatrix[i].length; j++) {
+                print(myMatrix[i][j] + " ");
+            }
+            println(); // New line after each row
+        }
+    }
 }
