8d596d1cd8b5fac71f6a58860d25b576e74ab848
S <s>
2024-01-07 23:06:18 +0400

    {"types":"C","runs":237}

--- Practice.java
+++ Practice.java
@@ -1,57 +1,16 @@
-import java.awt.event.MouseEvent;
-import acm.graphics.GObject;
-import acm.graphics.GOval;
-import acm.graphics.GRect;
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
+import acm.program.ConsoleProgram;
 
-public class Practice extends GraphicsProgram {
-
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	private static final int N_COLS = 3;
-	private static final int N_ROWS = 3;
-
-	private GOval oval1;
-
-	private boolean isRemoved = false;
-
-	public void run() {
-		setSize(400, 400);
-		int rectWidth = getWidth() / N_COLS;
-		int rectHeight = getHeight() / N_ROWS;
-
-		for (int i = 0; i < N_COLS; i++) {
-			for (int j = 0; j < N_ROWS; j++) {
-				GRect rect = new GRect(rectWidth, rectHeight);
-				add(rect, rectWidth * i, rectHeight * j);
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
+		int[][] myMatrix = new int[5][5];
+		
+		for(int i = 0; i < myMatrix[0].length; i++){
+			for(int j = 0; j < myMatrix.length; j++){
+				
+				myMatrix[i][j] = 3;
 			}
 		}
-
-		oval1 = new GOval(rectWidth, rectHeight); 
-													
-		oval1.setFilled(true);
-		oval1.setFillColor(rgen.nextColor());
-		add(oval1);
-
-		addMouseListeners();
+		println(myMatrix);
 	}
-
-	public void mouseClicked(MouseEvent e) {
-		GObject obj = getElementAt(e.getX(), e.getY());
-		if (obj == oval1 && !isRemoved) {
-			remove(oval1);
-			isRemoved = true;
-		} else {
-
-			int rectWidth = getWidth() / N_COLS;
-			int rectHeight = getHeight() / N_ROWS;
-
-			int gridX = (e.getX() / rectWidth) * rectWidth;
-			int gridY = (e.getY() / rectHeight) * rectHeight;
-
-			add(oval1, gridX, gridY);
-			isRemoved = false;
-		}
-	}
-
 }
