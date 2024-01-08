edbd4f3e0602641866fb1a1dfb0e0a6a04917e36
S <s>
2024-01-08 18:39:07 +0400

    {"types":"C","runs":290}

--- Practice.java
+++ Practice.java
@@ -1,45 +1,76 @@
 import java.awt.Color;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import acm.graphics.GLabel;
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class Practice extends GraphicsProgram{
+public class Practice extends GraphicsProgram implements ComponentListener {
 
-	private static final int R = 7;
-	private  static final int NUM = 500;
+    private static final int R = 7;
+    private static final int NUM = 500;
 
-	
-	private RandomGenerator rgen = RandomGenerator.getInstance();
-	
-	public void  run(){
-		
-		setBackground(Color.BLUE);
-		GOval[] ovals = new GOval[NUM];
-		for(int i = 0; i < NUM; i++){
-			ovals[i] = new GOval(R, R);
-			ovals[i].setFilled(true);
-			ovals[i].setColor(Color.WHITE);
-			add(ovals[i], rgen.nextInt(0,getWidth()), rgen.nextInt(0, getHeight()));
-			
-			
-			GLabel label = new GLabel("Happy new 2024 Year!");
-			label.setColor(Color.RED);
-			label.setFont("SansSerif-50");
-			add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getAscent() / 2);
-			
-		}
-		
-		while(true){
-			for(int i = 0; i < NUM; i++){
-				ovals[i].move(rgen.nextInt(-2, 2), 5);
-				if(ovals[i].getY() > getHeight()){
-					add(ovals[i], rgen.nextInt(0,getWidth()), - 2 * R );
-				}
-			}
-			pause(50);
-		}
-		
-	}
+    private RandomGenerator rgen = RandomGenerator.getInstance();
+    private GOval[] ovals = new GOval[NUM];
+    private GLabel label;
+
+    public void run() {
+        addComponentListener(this);
+        drawOvals();
+        drawLabel();
+
+        while (true) {
+            moveOvals();
+            pause(50);
+        }
+    }
+
+    private void drawOvals() {
+        for (int i = 0; i < NUM; i++) {
+            ovals[i] = new GOval(R, R);
+            ovals[i].setFilled(true);
+            ovals[i].setColor(Color.WHITE);
+            add(ovals[i], rgen.nextInt(0, getWidth()), rgen.nextInt(0, getHeight()));
+        }
+    }
+
+    private void drawLabel() {
+        label = new GLabel("Happy new 2024 Year!");
+        label.setColor(Color.RED);
+        label.setFont("SansSerif-50");
+        add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getAscent() / 2);
+    }
+
+    private void moveOvals() {
+        for (int i = 0; i < NUM; i++) {
+            ovals[i].move(rgen.nextInt(-2, 2), 5);
+            if (ovals[i].getY() > getHeight()) {
+                add(ovals[i], rgen.nextInt(0, getWidth()), -2 * R);
+            }
+        }
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+        removeAll();
+        drawOvals();
+        drawLabel();
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+        // Do nothing
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+        // Do nothing
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+        // Do nothing
+    }
 }
