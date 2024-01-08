74d7ad7bbdafdc423a815a2a16c987d438a0413b
S <s>
2024-01-08 15:20:49 +0400

    {"types":"C","runs":265}

--- Practice.java
+++ Practice.java
@@ -1,38 +1,7 @@
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
-    public  void run(){
-        int[] array = {64, 34, 25, 12, 22, 11, 90};
-
-        println("Original array: ");
-        printArray(array);
-
-        bubbleSort(array);
-
-        println("\nSorted array: ");
-        printArray(array);
-    }
-
-    // Bubble Sort implementation
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    // Utility method to print an array
-    static void printArray(int[] arr) {
-        for (int value : arr) {
-            System.out.print(value + " ");
-        }
-        System.out.println();
-    }
+	public void run(){
+		
+	}
 }
