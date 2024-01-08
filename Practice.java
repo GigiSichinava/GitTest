442a0f770c89123dd13590f23479ce6c46cbad18
S <s>
2024-01-08 09:41:26 +0400

    {"types":"C","runs":257}

--- Practice.java
+++ Practice.java
@@ -3,26 +3,20 @@ import java.util.ArrayList;
 import acm.program.ConsoleProgram;
 
 public class Practice extends ConsoleProgram{
-	
-	private static final int SENTINEL = -1;
-	
 	public void run(){
 		
-		ArrayList<Integer> myArrayList = new ArrayList<Integer>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		
 		while(true){
-			int n = readInt();
-			if(n == SENTINEL){
+			int x = readInt();
+			if(x == -1){
 				break;
 			}
-			myArrayList.add(n);
+			list.add(x);
 		}
-		printInReversedOrder(myArrayList);
-	}
-
-	private void printInReversedOrder(ArrayList<Integer> myArrayList) {
-		for(int i = myArrayList.size() - 1; i >= 0; i-- ){
-			println(myArrayList.get(i));
+		
+		for(int i = list.size() - 1; i >= 0; i--){
+			println(list.get(i));
 		}
 	}
 }
