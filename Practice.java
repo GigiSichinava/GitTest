74004ddaddb05a936479de71b0a5b6abea66407f
S <s>
2024-01-08 15:46:03 +0400

    {"types":"C","runs":277}

--- Practice.java
+++ Practice.java
@@ -6,18 +6,20 @@ import acm.util.RandomGenerator;
 
 public class Practice extends GraphicsProgram{
 
-	private static final int R = 5;
+	private static final int R = 3;
 	private  static final int NUM = 300;
 ;
 	
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	public void  run(){
+		
+		setBackground(Color.BLUE);
 		GOval[] ovals = new GOval[NUM];
 		for(int i = 0; i < NUM; i++){
 			ovals[i] = new GOval(R, R);
 			ovals[i].setFilled(true);
-			ovals[i].setColor(Color.CYAN);
+			ovals[i].setColor(Color.WHITE);
 			add(ovals[i], rgen.nextInt(0,getWidth()), rgen.nextInt(0, getHeight()));
 		}
 		
