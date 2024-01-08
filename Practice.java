732bacdeba2e92ca3b378a345c8fd3c6f31405da
S <s>
2024-01-08 15:44:13 +0400

    {"types":"C","runs":274}

--- Practice.java
+++ Practice.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
@@ -15,7 +17,18 @@ public class Practice extends GraphicsProgram{
 		for(int i = 0; i < NUM; i++){
 			ovals[i] = new GOval(R, R);
 			ovals[i].setFilled(true);
+			ovals[i].setColor(Color.CYAN);
 			add(ovals[i], rgen.nextInt(0,getWidth()), 0);
+		}
+		
+		while(true){
+			for(int i = 0; i < NUM; i++){
+				ovals[i].move(0, 5);
+				if(ovals[i].getY() > getHeight()){
+					add(ovals[i], rgen.nextInt(0,getWidth()), - 2 * R );
+				}
+			}
+			pause(50);
 		}
 	}
 }
