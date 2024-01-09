bd030a6ba5d3b69c447445bf657d88e73948cccf
S <s>
2024-01-09 17:36:13 +0400

    {"types":"C","runs":378}

--- ProgramingMethodolohyClass.java
+++ ProgramingMethodolohyClass.java
@@ -1,4 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProgramingMethodolohyClass {
-
+	
+	private HashMap<String, Double> studentScores;
+	
+	public ProgramingMethodolohyClass(){
+		studentScores = new HashMap<String, Double>();
+		
+	}
+	
+	public ProgramingMethodolohyClass(ArrayList<String> students){
+		studentScores = new HashMap<String, Double>();
+		
+		for(String student: students){
+			studentScores.put(student, null);
+		}	
+	}
+	
+	public Double getScore(String student){
+		if(!studentScores.containsKey(student)){
+			System.out.println("No such student: " + student);
+			return null;
+		}
+		return studentScores.get(student);
+	}
+	
+	public void getScore(double score){
+		
+	}
 }
--- testestestest.java
+++ testestestest.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
-public class testestestest {
+import acm.program.ConsoleProgram;
+
+public class testestestest extends ConsoleProgram{
+	public void run(){
+		
+		ArrayList<String> students = new ArrayList<>();
+		students.add("ali");
+		students.add("gio");
+		ProgramingMethodolohyClass classB = new ProgramingMethodolohyClass();
+	//	ProgramingMethodolohyClass classB = new ProgramingMethodolohyClass(students);
+		
+		
+		println(classB.getScore("ali"));
+	}
 
 }
