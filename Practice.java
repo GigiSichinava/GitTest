4355133ff6044217e5b173e6d27d5b7ec7feaaf5
S <s>
2024-01-09 16:11:29 +0400

    {"types":"C","runs":367}

--- Practice.java
+++ Practice.java
@@ -1,91 +1,30 @@
-import java.awt.Color;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
+import java.util.Arrays;
 
-import acm.graphics.GLabel;
-import acm.graphics.GOval;
-import acm.program.GraphicsProgram;
-import acm.util.RandomGenerator;
+import acm.program.ConsoleProgram;
 
-public class Practice extends GraphicsProgram implements ComponentListener {
+public class Practice extends ConsoleProgram{
+	public void run(){
+		
+		int[] firstNum = {1, 2, 3};
+		int[] secondNum = {9, 9, 9,0};
+		addBigInteger(firstNum, secondNum);
+		
+	}
 
-    private static final int R = 5;
-    private static final int NUM = 400;
-    private static final int DELAY = 50;
-    private static final int SHADOW_GAP = 5;
-
-    private RandomGenerator rgen = RandomGenerator.getInstance();
-    private GOval[] ovals = new GOval[NUM];
-    private GLabel label;
-    private GLabel shadowLabel;
-
-    public void run() {
-        setBackground(Color.BLUE);
-        addComponentListener(this);
-        drawOvals();
-        drawShadowLabel();
-        drawLabel();
-
-        while (true) {
-            moveOvals();
-            pause(DELAY);
-        }
-    }
-
-
-    private void drawOvals() {
-        for (int i = 0; i < NUM; i++) {
-            ovals[i] = new GOval(R, R);
-            ovals[i].setFilled(true);
-            ovals[i].setColor(Color.WHITE);
-            add(ovals[i], rgen.nextInt(0, getWidth() - 2 * R), rgen.nextInt(0, getHeight()));
-        }
-    }
-
-    private void drawLabel() {
-        label = new GLabel("Happy new 2024 Year!");
-        label.setColor(Color.RED);
-        label.setFont("SansSerif-50");
-        add(label, getWidth() / 2 - label.getWidth() / 2, getHeight() / 2 - label.getAscent() / 2);
-    }
-
-    private void drawShadowLabel() {
-        shadowLabel = new GLabel("Happy new 2024 Year!");
-        shadowLabel.setColor(Color.BLACK);
-        shadowLabel.setFont("SansSerif-50");
-        add(shadowLabel, getWidth() / 2 - shadowLabel.getWidth() / 2 + SHADOW_GAP,
-                getHeight() / 2 - shadowLabel.getAscent() / 2 + SHADOW_GAP);
-    }
-
-    private void moveOvals() {
-        for (int i = 0; i < NUM; i++) {
-            ovals[i].move(rgen.nextDouble(-1.5, 1.5), 4);
-            if (ovals[i].getY() > getHeight()) {
-                add(ovals[i], rgen.nextInt(0, getWidth() - 2 * R), -2 * R);
-            }
-        }
-    }
-
-    @Override
-    public void componentHidden(ComponentEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void componentMoved(ComponentEvent arg0) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void componentResized(ComponentEvent arg0) {
-        removeAll();
-        drawOvals();
-        drawShadowLabel();
-        drawLabel();
-    }
-
-    @Override
-    public void componentShown(ComponentEvent arg0) {
-        // TODO Auto-generated method stub
-    }
+	private void addBigInteger(int[] firstNum, int[] secondNum) {
+		  
+		String a = Arrays.toString(firstNum);
+		String b = Arrays.toString(secondNum);
+		
+		int x = Integer.parseInt(a);
+		int y = Integer.parseInt(b);
+		
+		int res = x + y;
+		int resSize = 
+		
+		int[] result = new int[];
+		
+		
+		
+	}
 }
