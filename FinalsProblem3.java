0831ca48beba01b148a3606d6524b0ba8fe9f0c6
S <s>
2024-01-10 22:01:48 +0400

    {"types":"C","runs":427}

--- FinalsProblem3.java
+++ FinalsProblem3.java
@@ -1,4 +1,20 @@
+import java.util.StringTokenizer;
 
-public class FinalsProblem3 {
+import acm.program.ConsoleProgram;
 
+public class FinalsProblem3 extends ConsoleProgram {
+	public void run() {
+
+		String line = readLine("Enter string: ");
+		String temp = "";
+
+		for (int i = 0; i < line.length() - 1; i++) {
+			if (Character.isDigit(line.charAt(i))) {
+				for (int j = 0; j < Integer.parseInt(Character.toString(line.charAt(i))); i++) {
+					temp += Character.toString(line.charAt(i + 1));
+				}
+			}
+		}
+		println(temp);
+	}
 }
