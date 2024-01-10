6acc853e7707911282c03b2be2d1c16712f336be
S <s>
2024-01-10 19:39:54 +0400

    {"types":"C","runs":383}

--- FinalsProblem1.java
+++ FinalsProblem1.java
@@ -2,41 +2,41 @@ import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
 
-public class FinalsProblem1 extends ConsoleProgram{
-	public void run(){
-		
+public class FinalsProblem1 extends ConsoleProgram {
+	public void run() {
+
 		// Create first Array
 		int a = readInt("");
 		int[] firstNum = new int[a];
-		for(int i = 0 ; i < firstNum.length; i++){
+		for (int i = 0; i < firstNum.length; i++) {
 			firstNum[i] = readInt();
 		}
 		int firstInteger = transferToInteger(firstNum);
-		
+
 		// Create second Array
 		int b = readInt();
 		int[] secondNum = new int[b];
-		for(int j = 0; j < secondNum.length; j++){
+		for (int j = 0; j < secondNum.length; j++) {
 			secondNum[j] = readInt();
 		}
 		int secondInteger = transferToInteger(secondNum);
-		
-		
+
 		int SUM = firstInteger + secondInteger;
-		
+
 		int size = getIntegerSize(SUM);
-		
+
 		int[] finalArray = new int[size];
-		for(int i = finalArray.length - 1 ; i >= 0; i--){
+		for (int i = finalArray.length - 1; i >= 0; i--) {
 			finalArray[i] = SUM % 10;
 			SUM /= 10;
 		}
+		
 		println(Arrays.toString(finalArray));
 	}
 
 	private int getIntegerSize(int sum) {
 		int size = 0;
-		while(sum >= 0){
+		while (sum > 0) {
 			size++;
 			sum /= 10;
 		}
@@ -45,11 +45,10 @@ public class FinalsProblem1 extends ConsoleProgram{
 
 	private int transferToInteger(int[] number) {
 		int result = 0;
-		for(int i = number.length - 1; i >= 0; i--){
+		for (int i = number.length - 1; i >= 0; i--) {
 			result += i * Math.pow(10, number.length - 1 - i);
 		}
 		return result;
 	}
-
 
 }
