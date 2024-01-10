2731f5a30e385b96956a6d99c61bfde3f6185934
S <s>
2024-01-10 23:17:40 +0400

    {"types":"C","runs":459}

--- FinalsProblem4.java
+++ FinalsProblem4.java
@@ -1,33 +1,30 @@
-import java.util.Arrays;
-
 import acm.program.ConsoleProgram;
 
 public class FinalsProblem4 extends ConsoleProgram {
 	public void run() {
 
-		String line = readLine();
+		String line = readLine("Enter string: ");
 
 		char[] chars = line.toCharArray();
 
 		int left = 0;
 		int right = line.length() - 1;
 
-		while(left < right) {
+		while (left < right) {
 			if (!Character.isAlphabetic(chars[left])) {
 				left++;
+			} else if (!Character.isAlphabetic(chars[right])) {
+				right--;
 			} else {
-				if (!Character.isAlphabetic(chars[right])) {
-					right--;
-				} else {
-					char temp = chars[left];
-					chars[left] = chars[right];
-					chars[right] = temp;
-					left++;
-					right++;
-				}
-
+				char temp = chars[left];
+				chars[left] = chars[right];
+				chars[right] = temp;
+				left++;
+				right--;
 			}
 		}
-		println(Arrays.toString(chars));
+
+		String result = new String(chars);
+		println("Result: " + result);
 	}
 }
