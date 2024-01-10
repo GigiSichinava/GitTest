0c2a94f14984b45cb43c9fa356d3077880c98fd8
S <s>
2024-01-10 23:28:30 +0400

    {"types":"C","runs":463}

--- FinalsProblem4.java
+++ FinalsProblem4.java
@@ -4,11 +4,14 @@ public class FinalsProblem4 extends ConsoleProgram {
 	public void run() {
 
 		String line = readLine();
+
+		// Transform string to array of characters
 		char[] chars = line.toCharArray();
 
 		int left = 0;
 		int right = chars.length - 1;
 
+		// Skin non-characters but swap characters
 		while (left < right) {
 			if (!Character.isAlphabetic(chars[left])) {
 				left++;
