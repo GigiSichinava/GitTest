3080ae858aab94db0de0a9b580018034c0a69902
S <s>
2024-01-10 19:35:15 +0400

    {"types":"C","runs":382}

--- FinalsProblem1.java
+++ FinalsProblem1.java
@@ -4,47 +4,52 @@ import acm.program.ConsoleProgram;
 
 public class FinalsProblem1 extends ConsoleProgram{
 	public void run(){
-	
+		
+		// Create first Array
 		int a = readInt("");
-		int[] firstNumber = new int[a];
-		for(int i = 0; i < firstNumber.length; i++){
-			firstNumber[i] = readInt();
+		int[] firstNum = new int[a];
+		for(int i = 0 ; i < firstNum.length; i++){
+			firstNum[i] = readInt();
 		}
+		int firstInteger = transferToInteger(firstNum);
 		
+		// Create second Array
 		int b = readInt();
-		int[] secondNumber = new int[b];
-		for(int i = 0; i < secondNumber.length; i++){
-			secondNumber[i] = readInt();
+		int[] secondNum = new int[b];
+		for(int j = 0; j < secondNum.length; j++){
+			secondNum[j] = readInt();
 		}
+		int secondInteger = transferToInteger(secondNum);
 		
-		String strA = transformToString(firstNumber);
-		String strB = transformToString(secondNumber);
 		
-		int intA = Integer.parseInt(strA);
-		int intB = Integer.parseInt(strB);
+		int SUM = firstInteger + secondInteger;
 		
-		int SUM = intA + intB;
-	
-		int length = 0;
-		while(SUM > 0){
-			length++;
+		int size = getIntegerSize(SUM);
+		
+		int[] finalArray = new int[size];
+		for(int i = finalArray.length - 1 ; i >= 0; i--){
+			finalArray[i] = SUM % 10;
 			SUM /= 10;
 		}
-		int[] result = new int[length];
-		for(int i = length - 1; i >= 0; i--){
-			result[i] = SUM % 10;
-			SUM /= 10;
-		}
-		
-		println(Arrays.toString(result));
-		
+		println(Arrays.toString(finalArray));
 	}
 
-	private String transformToString(int[] firstNumber) {
-		int result = 0;
-		for(int i = firstNumber.length - 1; i >= 0; i--){
-			result += firstNumber[i] * Math.pow(10, firstNumber.length - 1 - i);
+	private int getIntegerSize(int sum) {
+		int size = 0;
+		while(sum >= 0){
+			size++;
+			sum /= 10;
 		}
-		return Integer.toString(result);
+		return size;
 	}
+
+	private int transferToInteger(int[] number) {
+		int result = 0;
+		for(int i = number.length - 1; i >= 0; i--){
+			result += i * Math.pow(10, number.length - 1 - i);
+		}
+		return result;
+	}
+
+
 }
