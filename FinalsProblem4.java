228552c864330e91e90811035671823150896bae
S <s>
2024-01-10 22:42:11 +0400

    {"types":"C","runs":443}

--- FinalsProblem4.java
+++ FinalsProblem4.java
@@ -3,13 +3,13 @@ import acm.program.ConsoleProgram;
 public class FinalsProblem4 extends ConsoleProgram{
 	public void run(){
 		 
-		String line = readLine("Enter string : ");
+		String line = readLine("Enter string: ");
 		String result = "";
 		
 		for(int i = 0; i < line.length(); i++){
 			result += Character.toString(line.charAt(line.length() - i - 1));
 		}
-		println(result);
+		println("Result: " + result);
 	}
 
 }
