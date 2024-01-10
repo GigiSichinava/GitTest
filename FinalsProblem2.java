be088f3057b989cd34b3713e2411544cb6052725
S <s>
2024-01-10 20:35:48 +0400

    {"types":"C","runs":408}

--- FinalsProblem2.java
+++ FinalsProblem2.java
@@ -1,10 +1,20 @@
+import java.awt.event.ActionEvent;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
 public class FinalsProblem2 extends GraphicsProgram{
+	
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	private JTextField widthField;
+	private JTextField heightField;
+	
 	public void init(){
 		
 		JLabel width = new JLabel("width:");
@@ -21,8 +31,22 @@ public class FinalsProblem2 extends GraphicsProgram{
 		
 		JButton add = new JButton("Add");
 		add(add, SOUTH);
+		add.addActionListener(this);
 		
 		JButton filter = new JButton("Filter");
 		add(filter, SOUTH);
+		
+		addActionListeners();
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if(e.getActionCommand().equals("Add")){
+			int width = Integer.parseInt(widthField.getText());
+			int height = Integer.parseInt(heightField.getText());
+			
+			GRect rect = new GRect(width, height);
+			add(rect, rgen.nextInt(0, getWidth() - width), rgen.nextInt(getHeight() - height));
+			
+		}
 	}
 }
