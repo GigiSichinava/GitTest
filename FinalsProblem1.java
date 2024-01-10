6330df6fb4fb2df4d53b7f4b0a4490e5f4f9fc74
S <s>
2024-01-10 19:49:07 +0400

    {"types":"C","runs":392}

--- FinalsProblem1.java
+++ FinalsProblem1.java
@@ -6,7 +6,7 @@ public class FinalsProblem1 extends ConsoleProgram {
 	public void run() {
 
 		// Create first Array
-		int a = readInt("");
+		int a = readInt("Size of the first number: ");
 		int[] firstNum = new int[a];
 		for (int i = 0; i < firstNum.length; i++) {
 			firstNum[i] = readInt();
@@ -14,26 +14,27 @@ public class FinalsProblem1 extends ConsoleProgram {
 		int firstInteger = transferToInteger(firstNum);
 
 		// Create second Array
-		int b = readInt();
+		int b = readInt("Size of the second number :");
 		int[] secondNum = new int[b];
 		for (int j = 0; j < secondNum.length; j++) {
 			secondNum[j] = readInt();
 		}
 		int secondInteger = transferToInteger(secondNum);
 
+		// Get result as an integer and its size
 		int SUM = firstInteger + secondInteger;
-
 		int size = getIntegerSize(SUM);
 
+		// Put integer into array
 		int[] finalArray = new int[size];
 		for (int i = finalArray.length - 1; i >= 0; i--) {
 			finalArray[i] = SUM % 10;
 			SUM /= 10;
 		}
-		
 		println(Arrays.toString(finalArray));
 	}
 
+	// Get size of the integer
 	private int getIntegerSize(int sum) {
 		int size = 0;
 		if(sum == 0){
@@ -46,6 +47,7 @@ public class FinalsProblem1 extends ConsoleProgram {
 		return size;
 	}
 
+	// Transfer array to integer
 	private int transferToInteger(int[] number) {
 		int result = 0;
 		for (int i = number.length - 1; i >= 0; i--) {
