071f9b657e622f0502bed44b85e436857fb2269d
S <s>
2024-01-10 23:01:41 +0400

    {"types":"C","runs":452}

--- FinalsProblem4.java
+++ FinalsProblem4.java
@@ -1,19 +1,29 @@
 import acm.program.ConsoleProgram;
 
-public class FinalsProblem4 extends ConsoleProgram{
-	public void run(){
-		 
-		String line = readLine("Enter string: ");
-		String result = "";
-		
-		for(int i = 0; i < line.length(); i++){
-			if(Character.isAlphabetic(line.charAt(i))){
-				result += Character.toString(line.charAt(line.length() - i - 1));
-			}else{
-				result += line.charAt(line.length() - i);
-			}
-		}
-		println("Result: " + result);
-	}
+public class FinalsProblem4 extends ConsoleProgram {
+    public void run() {
+        String line = readLine("Enter string: ");
+        char[] chars = line.toCharArray();
 
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            // Skip non-alphabetic characters from the left
+            if (!Character.isAlphabetic(chars[left])) {
+                left++;
+            // Skip non-alphabetic characters from the right
+            } else if (!Character.isAlphabetic(chars[right])) {
+                right--;
+            // Swap alphabetic characters
+            } else {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        String result = new String(chars);
+        println("Result: " + result);
+    }
 }
