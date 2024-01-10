74202223480cc53a21b87a49bd66985ffd60bb9b
S <s>
2024-01-10 22:38:29 +0400

    {"types":"C","runs":438}

--- FinalsProblem4.java
+++ FinalsProblem4.java
@@ -6,7 +6,7 @@ public class FinalsProblem4 extends ConsoleProgram{
 		String line = readLine();
 		String result = "";
 		
-		for(int i = 0; i < line.length(); i++){
+		for(int i = 0; i < line.length() - 1; i++){
 			result += Character.toString(line.charAt(line.length() - i));
 		}
 		println(result);
