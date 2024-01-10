e41b98fb802ccfd16169c3d021d2419b6ee07265
S <s>
2024-01-10 20:59:46 +0400

    {"types":"C","runs":415}

--- FinalsProblem2.java
+++ FinalsProblem2.java
@@ -1,73 +1,70 @@
 import java.awt.event.ActionEvent;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class FinalsProblem2 extends GraphicsProgram implements ComponentListener {
-
-    private RandomGenerator rgen = RandomGenerator.getInstance();
-    private JTextField widthField, heightField;
+public class FinalsProblem2 extends GraphicsProgram{
+	
+	private GRect rect;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
     private ArrayList<GRect> rectangles;
 
-    public void init() {
-        rectangles = new ArrayList<>();
-
-        JLabel widthLabel = new JLabel("Width:");
-        add(widthLabel, SOUTH);
-        widthField = new JTextField(10);
-        add(widthField, SOUTH);
-
-        JLabel heightLabel = new JLabel("Height:");
-        add(heightLabel, SOUTH);
-        heightField = new JTextField(10);
-        add(heightField, SOUTH);
-
-        JButton addButton = new JButton("Add");
-        add(addButton, SOUTH);
-        addButton.addActionListener(this);
-
-        // Add ComponentListener to listen for window resize events
-        addComponentListener(this);
-
-        addActionListeners();
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Add")) {
-            int width = Integer.parseInt(widthField.getText());
-            int height = Integer.parseInt(heightField.getText());
-
-            GRect rect = new GRect(width, height);
-            add(rect, rgen.nextInt(0, getWidth() - width), rgen.nextInt(0, getHeight() - height));
-            rectangles.add(rect);
-        }
-    }
-
-    @Override
-    public void componentResized(ComponentEvent e) {
-        for (GRect rect : rectangles) {
-            // Update the position and potentially size for each rectangle
-            int newWidth = (int) Math.min(rect.getWidth(), getWidth() - (int) rect.getX());
-            int newHeight = (int) Math.min(rect.getHeight(), getHeight() - (int) rect.getY());
-            rect.setSize(newWidth, newHeight);
-        }
-    }
-
-    @Override
-    public void componentMoved(ComponentEvent e) {
-    }
-
-    @Override
-    public void componentShown(ComponentEvent e) {
-    }
-
-    @Override
-    public void componentHidden(ComponentEvent e) {
-    }
+	
+	private JTextField widthField;
+	private JTextField heightField;
+	
+	public void init(){
+		
+		JLabel width = new JLabel("width:");
+		add(width, SOUTH);
+		
+		widthField = new JTextField(10);
+		add(widthField, SOUTH);
+		
+		JLabel height = new JLabel("height");
+		add(height, SOUTH);
+		
+		heightField = new JTextField(10);
+		add(heightField, SOUTH);
+		
+		JButton add = new JButton("Add");
+		add(add, SOUTH);
+		add.addActionListener(this);
+		
+		JButton filter = new JButton("Filter");
+		add(filter, SOUTH);
+		
+		addActionListeners();
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+		int width = Integer.parseInt(widthField.getText());
+		int height = Integer.parseInt(heightField.getText());
+		
+		if(e.getActionCommand().equals("Add")){
+			
+			rect = new GRect(width, height);
+			add(rect, rgen.nextInt(0, getWidth() - width), rgen.nextInt(getHeight() - height));
+			rectangles.add(rect);
+		}
+		
+		if(e.getActionCommand().isEmpty()){
+			return;
+		}
+		
+		if(e.getActionCommand().equals("Filter")){
+			for(GRect curRect : rectangles){
+				if(curRect.getWidth() > Integer.parseInt(widthField.getText()) || curRect.getHeight() > Integer.parseInt(heightField.getText())){
+					rectangles.remove(curRect);
+				}
+			}
+		}
+	}
 }
