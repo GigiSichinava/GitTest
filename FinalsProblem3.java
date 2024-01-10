e1b21d75fce2a7fb9d3cfb28fd73ae655b5f4d6d
S <s>
2024-01-10 22:02:59 +0400

    {"types":"C","runs":428}

--- FinalsProblem3.java
+++ FinalsProblem3.java
@@ -8,13 +8,15 @@ public class FinalsProblem3 extends ConsoleProgram {
 		String line = readLine("Enter string: ");
 		String temp = "";
 
-		for (int i = 0; i < line.length() - 1; i++) {
+		for (int i = 0; i < line.length() - 2; i++) {
 			if (Character.isDigit(line.charAt(i))) {
 				for (int j = 0; j < Integer.parseInt(Character.toString(line.charAt(i))); i++) {
 					temp += Character.toString(line.charAt(i + 1));
 				}
 			}
 		}
+		temp += Character.toString(line.charAt(line.length() - 1));
+
 		println(temp);
 	}
 }
