d08cbc7600794cce768cda7b5c81f24d1d3eff0c
S <s>
2024-01-11 17:24:59 +0400

    {"types":"C","runs":496}

--- FinalsProblem6.java
+++ FinalsProblem6.java
@@ -1,38 +1,42 @@
-import acm.program.ConsoleProgram;
 import java.util.HashSet;
 import java.util.Set;
 
+import acm.program.ConsoleProgram;
+
 public class FinalsProblem6 extends ConsoleProgram{
-    public void run(){
-        
-        String line = readLine();
+	public void run(){
+		
+		String line = readLine();
         Set<String> uniqueSubstrings = new HashSet<>();
 
-        for(int i = 0; i < line.length(); i++){
-            for(int j = i + 1; j <= line.length(); j++){
-                String part = line.substring(i, j);
-                if(hasSameAmount(part)){
-                    uniqueSubstrings.add(part); // Adds to set if condition is met
-                }
-            }
-        }
+		
+		for(int i = 0; i < line.length(); i++){
+			for(int j = i + 1; j <= line.length(); j++){
+				String part = line.substring(i, j);
+				if(hasSameAmount(part)){
+					uniqueSubstrings.add(part);
+					
+				}
+			}
+		}
+		
+		
+	}
 
-        println(uniqueSubstrings.size()); // Prints the count of unique substrings
-    }
-
-    private boolean hasSameAmount(String part) {
-        int countA = 0;
-        int countB = 0;
-
-        for(int i = 0; i < part.length(); i++){
-            char ch = part.charAt(i);
-            if(ch == 'a'){
-                countA++;
-            }
-            if(ch == 'b'){
-                countB++;
-            }
-        }
-        return countA == countB;
-    }
+	private boolean hasSameAmount(String part) {
+		int countA = 0;
+		int countB = 0;
+		
+		for(int i = 0; i < part.length(); i++){
+			char ch = part.charAt(i);
+			if(ch == 'a'){
+				countA++;
+			}
+			if(ch == 'b'){
+				countB++;
+			}
+		}
+		return countA == countB  ;
+	}
 }
+
