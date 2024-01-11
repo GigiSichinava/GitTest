7665bacfa83da3b35475f5d66077b5a5e4a5e24d
S <s>
2024-01-11 18:26:14 +0400

    {"types":"C","runs":510}

--- FinalsProblem11.java
+++ FinalsProblem11.java
@@ -14,12 +14,12 @@ public class FinalsProblem11 extends ConsoleProgram {
 			String part = tokenizer.nextToken();
 
 			String result = "";
-			for (int i = 0; i < part.length(); i++) {
+			
 				char firstChar = Character.toUpperCase(part.charAt(0));
 				String lastChars = part.substring(1, part.length()).toLowerCase();
 
 				result += Character.toString(firstChar) + lastChars + " ";
-			}
+			
 			finalResult += result;
 		}
 		
