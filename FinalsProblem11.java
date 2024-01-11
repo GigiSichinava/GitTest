eb07c8adc63db988612e507d486246703369b905
S <s>
2024-01-11 18:22:27 +0400

    {"types":"C","runs":504}

--- FinalsProblem11.java
+++ FinalsProblem11.java
@@ -1,4 +1,25 @@
+import java.util.StringTokenizer;
 
-public class FinalsProblem11 {
+import acm.program.ConsoleProgram;
 
+public class FinalsProblem11 extends ConsoleProgram {
+	public void run() {
+
+		String line = "hElLo WORld";
+		String finalResult = "";
+
+		StringTokenizer tokenizer = new StringTokenizer(line, " ");
+		while (tokenizer.hasMoreTokens()) {
+			String part = tokenizer.nextToken();
+
+			String result = "";
+			for (int i = 0; i < part.length(); i++) {
+				char firstChar = Character.toUpperCase(part.charAt(0));
+				String lastChars = part.substring(1, part.length()).toLowerCase();
+
+				result += Character.toString(firstChar) + lastChars + " ";
+			}
+			finalResult += result;
+		}
+	}
 }
