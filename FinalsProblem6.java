d17450e7b4b1cba3df21c300ebaea1f3d0ca39b3
S <s>
2024-01-11 16:45:32 +0400

    {"types":"C","runs":485}

--- FinalsProblem6.java
+++ FinalsProblem6.java
@@ -5,8 +5,9 @@ public class FinalsProblem6 extends ConsoleProgram {
         String line = readLine();
         int count = 0;
 
+        // Iterating through all possible substrings
         for (int i = 0; i < line.length(); i++) {
-            for (int j = i + 2; j <= line.length(); j += 2) { // j starts from i+2 and increments by 2
+            for (int j = i + 2; j <= line.length(); j += 2) { // Consider only even length substrings
                 String substring = line.substring(i, j);
                 if (hasEqualAandB(substring)) {
                     count++;
@@ -18,9 +19,9 @@ public class FinalsProblem6 extends ConsoleProgram {
     }
 
     private boolean hasEqualAandB(String substring) {
-        int countA = 0;
-        int countB = 0;
+        int countA = 0, countB = 0;
 
+        // Counting 'a's and 'b's in the substring
         for (int i = 0; i < substring.length(); i++) {
             if (substring.charAt(i) == 'a') {
                 countA++;
@@ -29,7 +30,7 @@ public class FinalsProblem6 extends ConsoleProgram {
             }
         }
 
-        // Returns true if the number of 'a's and 'b's are equal
+        // Only return true if the counts are exactly equal
         return countA == countB;
     }
 }
