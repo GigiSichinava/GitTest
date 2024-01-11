13acb10512e7ca4b96768ec2679c2b872eec82d8
S <s>
2024-01-11 13:05:10 +0400

    {"types":"C","runs":471}

--- FinalsProblem6.java
+++ FinalsProblem6.java
@@ -3,25 +3,37 @@ import acm.program.ConsoleProgram;
 public class FinalsProblem6 extends ConsoleProgram{
 	public void run(){
 		 
-		String line = "abab";
-		String input = readLine();
-		String result = "";
-	
-		int left = 0;
-		int right = line.length() - 1;
+		String line = readLine();
+		int count = 0;
 		
+		for(int i = 0; i < line.length(); i++){
+			for(int j = i + 1; j <= line.length(); j++){
+				String substring = line.substring(i, j);
+				if(hasEquallAandB(substring)){
+					count++;
+				}
+			}
+		}
+	}
+
+	private boolean hasEquallAandB(String substring) {
 		int countA = 0;
 		int countB = 0;
 		
-		for(int i = 0 ; i < input.length(); i++){
-			if(line.charAt(i) == 'a'){
+		for(int i = 0; i < substring.length(); i++){
+			if(substring.charAt(i) == 'a'){
 				countA++;
 			}
-			if(line.charAt(i) == 'b'){
+			if(substring.charAt(i) == 'b'){
 				countB++;
 			}
+			
+			if(countA == countB){
+				return true;
+			}
+			
 		}
-		println(countA);
-		println(countB);
+		return false;
 	}
 }
+
