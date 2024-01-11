a148fc808cc2eafdfa5f64f540e4eb4a181b6452
S <s>
2024-01-11 17:20:10 +0400

    {"types":"C","runs":491}

--- FinalsProblem6.java
+++ FinalsProblem6.java
@@ -1,36 +1,36 @@
 import acm.program.ConsoleProgram;
 
-public class FinalsProblem6 extends ConsoleProgram {
-    public void run() {
-        String line = readLine();
-        int count = 0;
+public class FinalsProblem6 extends ConsoleProgram{
+	public void run(){
+		
+		String line = readLine();
+		int count = 0;
+		
+		for(int i = 0; i < line.length(); i++){
+			for(int j = i + 1; j <= line.length(); j++){
+				String part = line.substring(i, j);
+				if(hasSameAmount(part)){
+					count++;
+				}
+			}
+		}
+		
+		println(count);
+	}
 
-        // Iterate over all substrings, but only consider those of even length
-        for (int i = 0; i < line.length(); i++) {
-            for (int j = i + 2; j <= line.length(); j += 2) { // Increment by 2 to maintain even length
-                String substring = line.substring(i, j);
-                if (hasEqualAandB(substring)) {
-                    count++;
-                }
-            }
-        }
-
-        println(count);
-    }
-
-    private boolean hasEqualAandB(String substring) {
-        int countA = 0, countB = 0;
-
-        // Count 'a's and 'b's in the substring
-        for (int i = 0; i < substring.length(); i++) {
-            if (substring.charAt(i) == 'a') {
-                countA++;
-            } else if (substring.charAt(i) == 'b') {
-                countB++;
-            }
-        }
-
-        // Return true if the number of 'a's and 'b's are equal
-        return countA == countB;
-    }
+	private boolean hasSameAmount(String part) {
+		int countA = 0;
+		int countB = 0;
+		
+		for(int i = 0; i < part.length(); i++){
+			char ch = part.charAt(i);
+			if(ch == 'a'){
+				countA++;
+			}
+			if(ch == 'b'){
+				countB++;
+			}
+		}
+		return countA == countB;
+	}
 }
