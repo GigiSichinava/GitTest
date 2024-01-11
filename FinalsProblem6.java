3c8da7151f991cfe4f7db87deb2a733058a34430
S <s>
2024-01-11 12:49:29 +0400

    {"types":"C","runs":468}

--- FinalsProblem6.java
+++ FinalsProblem6.java
@@ -2,6 +2,25 @@ import acm.program.ConsoleProgram;
 
 public class FinalsProblem6 extends ConsoleProgram{
 	public void run(){
+		 
+		String line = "abab";
+		String result = "";
+	
+		int left = 0;
+		int right = line.length() - 1;
 		
+		int countA = 0;
+		int countB = 0;
+		
+		for(int i = 0 ; i < line.length(); i++){
+			if(line.charAt(i) == 'a'){
+				countA++;
+			}
+			if(line.charAt(i) == 'b'){
+				countB++;
+			}
+		}
+		println(countA);
+		println(countB);
 	}
 }
