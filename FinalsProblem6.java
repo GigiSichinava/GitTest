63f7804ebec0c432ea576337c0140f6a2980e382
S <s>
2024-01-11 16:35:21 +0400

    {"types":"C","runs":481}

--- FinalsProblem6.java
+++ FinalsProblem6.java
@@ -1,44 +1,35 @@
-import java.util.Arrays;
-
 import acm.program.ConsoleProgram;
 
-public class FinalsProblem6 extends ConsoleProgram{
-	public void run(){
-		 
-		String line = readLine();
-		int count = 0;
-		
-		for(int i = 0; i < line.length(); i++){
-			for(int j = i + 1; j <= line.length(); j++){
-				String substring = line.substring(i, j);
-				if(hasEquallAandB(substring)){
-					count++;
-				}
-			}
-		}
-		
-		println(count);
-		
-	}
+public class FinalsProblem6 extends ConsoleProgram {
+    public void run() {
+        String line = readLine();
+        int count = 0;
 
-	private boolean hasEquallAandB(String substring) {
-		int countA = 0;
-		int countB = 0;
-		
-		for(int i = 0; i < substring.length(); i++){
-			if(substring.charAt(i) == 'a'){
-				countA++;
-			}else if(substring.charAt(i) == 'b'){
-				countB++;
-			
-			}
-			
-			
-			}
-		
-		return countA == countB;
-	}
-	
+        for (int i = 0; i < line.length(); i++) {
+            for (int j = i + 1; j <= line.length(); j++) {
+                String substring = line.substring(i, j);
+                if (hasEqualAandB(substring)) {
+                    count++;
+                }
+            }
+        }
 
+        println(count);
+    }
+
+    private boolean hasEqualAandB(String substring) {
+        int countA = 0;
+        int countB = 0;
+
+        for (int i = 0; i < substring.length(); i++) {
+            if (substring.charAt(i) == 'a') {
+                countA++;
+            } else if (substring.charAt(i) == 'b') {
+                countB++;
+            }
+        }
+
+        // Returns true if the number of 'a's and 'b's are equal
+        return countA == countB;
+    }
 }
-
