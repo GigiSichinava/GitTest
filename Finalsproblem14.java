c59b884006a5946805130f3ba525865113e99fe9
S <s>
2024-01-13 02:32:39 +0400

    {"types":"C","runs":514}

--- Finalsproblem14.java
+++ Finalsproblem14.java
@@ -1,4 +1,35 @@
+import java.util.StringTokenizer;
 
-public class Finalsproblem14 {
+import acm.program.ConsoleProgram;
 
+public class Finalsproblem14 extends ConsoleProgram{
+	public void run(){
+		
+		String line = readLine();
+		
+		StringTokenizer tokenizer = new StringTokenizer(line, " ");
+		while(tokenizer.hasMoreTokens()){
+			String part = tokenizer.nextToken();
+			
+			int curLength = part.length();
+			int maxLength = curLength;
+			if(curLength >= maxLength){
+				maxLength = curLength;
+			}
+			
+			String result = "";
+			for(int i = 0; i < part.length(); i++){
+				char ch1 = part.charAt(i);
+				for(int j = 1; j < part.length(); j++){
+					char ch2 = part.charAt(j);
+					if(ch1 != ch2){
+						result += Character.toString(ch1);
+					}
+				}
+			}
+			
+			println(result);
+		}
+		
+	}
 }
