f6fcdc988203cf5dff6b13ca9bca4d24c6a6796b
S <s>
2024-01-13 02:34:09 +0400

    {"types":"C","runs":515}

--- Finalsproblem14.java
+++ Finalsproblem14.java
@@ -6,6 +6,7 @@ public class Finalsproblem14 extends ConsoleProgram{
 	public void run(){
 		
 		String line = readLine();
+		String result = "";
 		
 		StringTokenizer tokenizer = new StringTokenizer(line, " ");
 		while(tokenizer.hasMoreTokens()){
@@ -17,7 +18,7 @@ public class Finalsproblem14 extends ConsoleProgram{
 				maxLength = curLength;
 			}
 			
-			String result = "";
+			
 			for(int i = 0; i < part.length(); i++){
 				char ch1 = part.charAt(i);
 				for(int j = 1; j < part.length(); j++){
