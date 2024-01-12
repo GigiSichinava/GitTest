d33ed1ca2dfed5a17d9b3369d661a230772e84c0
S <s>
2024-01-13 02:43:04 +0400

    {"types":"C","runs":516}

--- Finalsproblem14.java
+++ Finalsproblem14.java
@@ -2,35 +2,45 @@ import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
-public class Finalsproblem14 extends ConsoleProgram{
-	public void run(){
-		
+public class Finalsproblem14 extends ConsoleProgram {
+	public void run() {
+
 		String line = readLine();
 		String result = "";
-		
+
 		StringTokenizer tokenizer = new StringTokenizer(line, " ");
-		while(tokenizer.hasMoreTokens()){
+		while (tokenizer.hasMoreTokens()) {
 			String part = tokenizer.nextToken();
-			
+
 			int curLength = part.length();
-			int maxLength = curLength;
-			if(curLength >= maxLength){
+			int maxLength = 0;
+
+			if (curLength >= maxLength) {
 				maxLength = curLength;
+				result = part;
 			}
-			
-			
-			for(int i = 0; i < part.length(); i++){
+
+			for (int i = 0; i < part.length(); i++) {
 				char ch1 = part.charAt(i);
-				for(int j = 1; j < part.length(); j++){
+				boolean isUnique = true;
+
+				for (int j = i + 1; j < part.length(); j++) {
 					char ch2 = part.charAt(j);
-					if(ch1 != ch2){
-						result += Character.toString(ch1);
+					isUnique = true;
+
+					if (ch1 == ch2) {
+						isUnique = false;
+						break;
 					}
 				}
+				if (isUnique) {
+					result = part;
+				}
+
 			}
-			
+
 			println(result);
 		}
-		
+
 	}
 }
