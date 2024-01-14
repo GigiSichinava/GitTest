7b9b6fe2623eee36da486cbd8171f69463eee174
S <s>
2024-01-14 23:45:52 +0400

    {"types":"C","runs":532}

--- FinalsProblem10.java
+++ FinalsProblem10.java
@@ -1,6 +1,7 @@
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
@@ -8,6 +9,8 @@ public class FinalsProblem10 extends GraphicsProgram{
 	
 	private static final int N_ROWS = 8;
 	private static final int N_COLS = 8;
+	private int width = getWidth() / N_COLS;
+	private int height = getHeight() / N_ROWS;
 	
 	public void init(){
 		setSize(600,600);
@@ -15,17 +18,21 @@ public class FinalsProblem10 extends GraphicsProgram{
 		add(label, SOUTH);
 		JTextField textField = new JTextField(10);
 		add(textField, SOUTH);
+		textField.addActionListener(this);
+		
+		addActionListeners();
 	}
 	
 	public void run(){
 		for(int i = 0; i < N_ROWS; i++){
 			for(int j = 0; j < N_COLS; j++){
-				int rectWidth  = getWidth() / N_COLS;
-				int rectHeight = getHeight() / N_ROWS;
-				GRect rect = new GRect(rectWidth, rectHeight);
-				add(rect, i * rectWidth, j * rectHeight);
+				GRect rect = new GRect(width, height);
+				add(rect, i * width, j * height);
 			}
 		}
+		
+		GOval oval = new GOval(width, height);
+		add(oval, 5 * width, 5 * height);
 	}
 
 }
