fbf2024a2ebc3a50f02c3b0d8eea57f1190724f9
S <s>
2024-01-15 00:02:12 +0400

    {"types":"C","runs":545}

--- FinalsProblem10.java
+++ FinalsProblem10.java
@@ -40,8 +40,8 @@ public class FinalsProblem10 extends GraphicsProgram {
 		oval.setColor(Color.RED);
 		add(oval, 4 * ovalWidth, 4 * ovalHeight);
 		
-		double stoneWidth = (getWidth() / N_COLS) * 0.6;
-		double stoneHeight = (getHeight() / N_ROWS) * 0.6;
+		double stoneWidth = (getWidth() / N_COLS) * 0.7;
+		double stoneHeight = (getHeight() / N_ROWS) * 0.7;
 		GRect stone = new GRect(stoneWidth, stoneHeight);
 		stone.setFilled(true);
 		stone.setColor(Color.GREEN);
