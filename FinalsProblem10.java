855035ceacab625ce8274d1cc55daff3e5fe11f0
S <s>
2024-01-15 00:21:45 +0400

    {"types":"C","runs":567}

--- FinalsProblem10.java
+++ FinalsProblem10.java
@@ -12,10 +12,10 @@ public class FinalsProblem10 extends GraphicsProgram {
 
 	private static final int N_ROWS = 8;
 	private static final int N_COLS = 8;
-	
+
 	int Width = getHeight() / N_COLS;
 	int Height = getHeight() / N_ROWS;
-	
+
 	private JTextField textField;
 	private GOval oval;
 
@@ -30,8 +30,7 @@ public class FinalsProblem10 extends GraphicsProgram {
 	}
 
 	public void run() {
-		
-		
+
 		for (int i = 0; i < N_ROWS; i++) {
 			for (int j = 0; j < N_COLS; j++) {
 				int rectWidth = getHeight() / N_COLS;
@@ -40,54 +39,49 @@ public class FinalsProblem10 extends GraphicsProgram {
 				add(rect, j * rectWidth, i * rectHeight);
 			}
 		}
-		
+
 		int ovalWidth = getHeight() / N_COLS;
 		int ovalHeight = getHeight() / N_ROWS;
 		oval = new GOval(ovalWidth, ovalHeight);
 		oval.setFilled(true);
 		oval.setColor(Color.RED);
 		add(oval, 4 * ovalWidth, 4 * ovalHeight);
-		
+
 		/*
-		double stoneWidth = (getWidth() / N_COLS) * 0.7;
-		double stoneHeight = (getHeight() / N_ROWS) * 0.7;
-		GRect stone = new GRect(stoneWidth, stoneHeight);
-		stone.setFilled(true);
-		stone.setColor(Color.GREEN);
-		add(stone, 4 * ovalWidth + 10, 4 * ovalHeight + 10);
-		*/
+		 * double stoneWidth = (getWidth() / N_COLS) * 0.7; double stoneHeight =
+		 * (getHeight() / N_ROWS) * 0.7; GRect stone = new GRect(stoneWidth,
+		 * stoneHeight); stone.setFilled(true); stone.setColor(Color.GREEN);
+		 * add(stone, 4 * ovalWidth + 10, 4 * ovalHeight + 10);
+		 */
 	}
-	
-	public void actionPerformed(ActionEvent e){
+
+	public void actionPerformed(ActionEvent e) {
 		int Width = getHeight() / N_COLS;
 		int Height = getHeight() / N_ROWS;
-		
-		
+
 		String line = e.getActionCommand();
-		if(line.equals("right")){
-			if(oval.getX() > 6 * Width){
+		if (line.equals("right")) {
+			if (oval.getX() > 6 * Width) {
 				return;
 			}
 			oval.move(Width, 0);
-		}
-		if(line.equals("left")){
-			if(oval.getX() < Width){
+		} else if (line.equals("left")) {
+			if (oval.getX() < Width) {
 				return;
 			}
 			oval.move(-Width, 0);
-		}
-		if(line.equals("up")){
-			if(oval.getX() < Height){
+		} else if (line.equals("up")) {
+			if (oval.getX() < Height) {
 				return;
 			}
 			oval.move(0, -Height);
-		}
-		if(line.equals("down")){
-			if(oval.getX() > 6 * Height){
+		} else if (line.equals("down")) {
+			if (oval.getX() > 6 * Height) {
 				return;
 			}
 			oval.move(0, Height);
 		}
+
 	}
 
 }
