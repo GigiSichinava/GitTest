68a115f9cefa05b02bb6981acf21e9f06422386f
S <s>
2024-01-15 02:00:11 +0400

    {"types":"C","runs":579}

--- FinalsProblem05.java
+++ FinalsProblem05.java
@@ -13,19 +13,24 @@ public class FinalsProblem05 extends ConsoleProgram {
 
 		int rows = myMatrix.length;
 		int cols = myMatrix[0].length;
-		int size = 0;
+		int maxPlusSize = 0;
 		
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < cols; j++){
+				int size = 0;
 				if(myMatrix[i][j] == 1){
 					size = 1;
 					while(size <= i && size <= j && size <= cols - 1 - j && size <= rows - 1 - i && myMatrix[i - size][j] == 1 && myMatrix[i + size][j] == 1 && myMatrix[i][j - size] == 1 && myMatrix[i][j + size] == 1){
 						size++;
 					}
+					if(size >= maxPlusSize){
+						maxPlusSize = size;
+						size = 0;
+					}
 				}
 			}
 		}
 		
-		println(size);
+		println(maxPlusSize - 1);
 	}
 }
