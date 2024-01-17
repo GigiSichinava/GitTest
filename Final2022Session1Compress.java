fefdaf1ee88c358103c0259cc9853e7927527d70
S <s>
2024-01-17 23:31:48 +0400

    {"types":"C","runs":638}

--- Final2022Session1Compress.java
+++ Final2022Session1Compress.java
@@ -4,8 +4,22 @@ public class Final2022Session1Compress extends ConsoleProgram{
 	public void run(){
 		
 		String line = "aabcccccaa";
-		String result = "a2b1c5a2";
+		String expected = "a2b1c5a2";
 		println(line);
+		println(expected);
+		
+		String result = "";
+		
+		for(int i = 0; i < line.length() - 1; i++){
+			char ch = line.charAt(i);
+			int count = 1;
+			if(ch != line.charAt(i + 1)){
+				count++;
+				result += count + line.charAt(i);
+			}
+			
+		}
+		
 		println(result);
 	}
 
