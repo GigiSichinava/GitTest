ec882667fe22189dc36739e2cb7441d05b6b1fc5
S <s>
2024-01-17 19:56:54 +0400

    {"types":"C","runs":630}

--- FinalsProblem09.java
+++ FinalsProblem09.java
@@ -1,8 +1,8 @@
 import acm.program.ConsoleProgram;
 
-public class FinalsProblem09 extends ConsoleProgram{
-	public void run(){
-		
+public class FinalsProblem09 extends ConsoleProgram {
+	public void run() {
+
 		int[][] myMatrix = {
 				{3, 5, 4, 0},
 				{4, 6, 7, 0},
@@ -12,35 +12,33 @@ public class FinalsProblem09 extends ConsoleProgram{
 		
 		int cols = myMatrix[0].length;
 		int rows = myMatrix.length;
-		
-		for(int i = 0; i < cols; i++){
+
+		for (int i = 0; i < cols; i++) {
 			int maxInColumn = myMatrix[0][i];
-
-			for(int j = 0; j < rows ; j++){
-
-				if(myMatrix[j][i] >= maxInColumn){			
+			for (int j = 0; j < rows; j++) {
+				if (myMatrix[j][i] >= maxInColumn) {
 					maxInColumn = myMatrix[j][i];
 				}
 			}
 			myMatrix[rows - 1][i] = maxInColumn;
-			
+
 		}
-		
+
 		int lastRow = myMatrix.length - 1;
-		
-		for(int col = 0; col < cols; col++){
-			int curInt = myMatrix[lastRow][col];
-			if(myMatrix[lastRow][col] >= curInt){
-				curInt = myMatrix[lastRow][col];
+
+		for (int col = 0; col < cols; col++) {
+			int curMaxInt = myMatrix[lastRow][col];
+			if (myMatrix[lastRow][col] >= curMaxInt) {
+				curMaxInt = myMatrix[lastRow][col];
 			}
-			myMatrix[lastRow][myMatrix[0].length - 1] = curInt;
+			myMatrix[lastRow][myMatrix[0].length - 1] = curMaxInt;
 		}
-		
-		 for (int[] row : myMatrix) {
-	            for (int value : row) {
-	                System.out.print(value + " ");
-	            }
-	            System.out.println();
-	        }
+
+		for (int[] row : myMatrix) {
+			for (int value : row) {
+				System.out.print(value + " ");
+			}
+			System.out.println();
+		}
 	}
 }
