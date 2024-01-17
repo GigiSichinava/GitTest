ba20c01edd5c9ca77a4f4aac8be1ef8c728803ee
S <s>
2024-01-18 00:22:17 +0400

    {"types":"C","runs":660}

--- Final2022Session1Compress.java
+++ Final2022Session1Compress.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class Final2022Session1Compress extends ConsoleProgram {
 	public void run() {
 
-		String line = "aabcccccaa";
+		String line = "aabcccccaaa";
 		String expected = "a2b1c5a2";
 		println(line);
 		println(expected);
