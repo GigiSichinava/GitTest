9166c14d5d894ad5fbb1196aa7d09b313eedc65d
S <s>
2024-01-17 23:17:59 +0400

    {"types":"C","runs":637}

--- Final2022Session1Compress.java
+++ Final2022Session1Compress.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Final2022Session1Compress {
+public class Final2022Session1Compress extends ConsoleProgram{
+	public void run(){
+		
+		String line = "aabcccccaa";
+		String result = "a2b1c5a2";
+		println(line);
+		println(result);
+	}
 
 }
