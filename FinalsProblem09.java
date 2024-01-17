b4ade5a29673d260004068d76fc86422bc544228
S <s>
2024-01-17 19:39:25 +0400

    {"types":"C","runs":619}

--- FinalsProblem09.java
+++ FinalsProblem09.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class FinalsProblem09 {
-
+public class FinalsProblem09 extends ConsoleProgram{
+	public void run(){
+		
+		int[][] myMatrix = {
+				{3, 5, 4, 0},
+				{4, 6, 7, 0},
+				{1, 3, 8, 0},
+				{0, 0, 0, 0},
+		};
+		
+		int cols = myMatrix[0].length;
+		int rows = myMatrix.length;
+		
+		for(int i = 0; i < cols; i++){
+			int currInt = myMatrix[0][i];
+			for(int j = 0; j < rows; j++){
+				if(myMatrix[j][i] >= currInt){
+					currInt = myMatrix[i][j];
+				}
+			}
+			myMatrix[cols -1 ][rows - 1] = currInt;
+		}
+		
+		System.out.print(myMatrix);
+	}
 }
