00e2d91352e0785d930bc3a715b68e125c31783e
S <s>
2024-01-18 00:22:10 +0400

    {"types":"C","runs":659}

--- Final2022Session1Compress.java
+++ Final2022Session1Compress.java
@@ -1,32 +1,30 @@
 import acm.program.ConsoleProgram;
 
-public class Final2022Session1Compress extends ConsoleProgram{
-	public void run(){
-		
+public class Final2022Session1Compress extends ConsoleProgram {
+	public void run() {
+
 		String line = "aabcccccaa";
 		String expected = "a2b1c5a2";
 		println(line);
 		println(expected);
-		
+
 		String result = "";
 		int count = 1;
 
-		for(int i = 1; i < line.length(); i++){
-			if(line.charAt(i - 1) != line.charAt(i)){
+		for (int i = 1; i < line.length(); i++) {
+			if (line.charAt(i - 1) != line.charAt(i)) {
 				result += line.charAt(i - 1);
 				result += count;
 				count = 1;
 
-			}else{
+			} else {
 				count++;
 			}
-			
-			
+
 		}
 		result += line.charAt(line.length() - 1);
-        result += count;
+		result += count;
 
-		
 		println(result);
 	}
 
