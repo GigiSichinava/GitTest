87d1b51f9b3061ddf31491dddb808328f7716c92
S <s>
2024-01-19 20:10:19 +0400

    {"types":"C","runs":2}

--- FinalsProblem004.java
+++ FinalsProblem004.java
@@ -1,15 +1,28 @@
 import javax.swing.JButton;
 
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class FinalsProblem004 extends GraphicsProgram{
+	
+	private JButton buttonUp;
+	private JButton buttonDown;
+	
+	private static final int D = 50;
+	
+	private GOval redOval;
+	private GOval blueOval;
+	
 	public void init(){
 		
-		JButton buttonUp = new JButton("Up");
+		buttonUp = new JButton("Up");
 		add(buttonUp, SOUTH);
 		
-		JButton buttonDown = new JButton("Down");
+		buttonDown = new JButton("Down");
 		add(buttonDown, SOUTH);
+		
+		redOval = new GOval(D, D, 10, 10);
+		add(redOval);
 		
 	}
 }
