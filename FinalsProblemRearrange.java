c5adcc0d7f2bbcdec7da5f75087ab79480ada7eb
S <s>
2024-01-19 22:08:15 +0400

    {"types":"C","runs":65}

--- FinalsProblemRearrange.java
+++ FinalsProblemRearrange.java
@@ -7,8 +7,6 @@ import acm.program.ConsoleProgram;
 public class FinalsProblemRearrange extends ConsoleProgram{
 	public void run(){
 		
-		
-		
 		String line = "keep calm and code on";
 		println(line);
 		String expected = "on and keep calm code" ;
@@ -16,24 +14,22 @@ public class FinalsProblemRearrange extends ConsoleProgram{
 		String result = "";
 		
 		StringTokenizer tokenizer = new StringTokenizer(line, " ");
-		
 		Map<Integer, String> stringMap = new TreeMap<>();
 		
 		while(tokenizer.hasMoreTokens()){
 			String part = tokenizer.nextToken();
 			int length = part.length();
-		
-			if(stringMap.containsKey(length)){
-				stringMap.put(length, stringMap.get(length) + " " + part);
-			}else{
+			
+			if(!stringMap.containsKey(length)){
 				stringMap.put(length, part);
+			}else{
+				stringMap.put(length, stringMap.get(length) + " " + part);
 			}
 		}
 		
-		for(String find : stringMap.values()){
-			result += find + " ";
+		for(String put : stringMap.values()){
+			result += put;
 		}
-		
 		
 		println(result);
 	}
