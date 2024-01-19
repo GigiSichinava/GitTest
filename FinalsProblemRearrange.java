58111761ac1054271cc2c9fc249a9f03e618bd74
S <s>
2024-01-19 21:37:28 +0400

    {"types":"C","runs":56}

--- FinalsProblemRearrange.java
+++ FinalsProblemRearrange.java
@@ -1,4 +1,37 @@
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
-public class FinalsProblemRearrange {
+import acm.program.ConsoleProgram;
 
+public class FinalsProblemRearrange extends ConsoleProgram{
+	public void run(){
+		
+		
+		
+		String line = "keep calm and code on";
+		String expected = "on and keep calm code" ;
+		String result = "";
+		
+		StringTokenizer tokenizer = new StringTokenizer(line, " ");
+		
+		Map<Integer, String> stringMap = new TreeMap<>();
+		
+		while(tokenizer.hasMoreTokens()){
+			String part = tokenizer.nextToken();
+			int length = part.length();
+		
+			if(stringMap.containsKey(length)){
+				stringMap.put(length + 1, part);
+			}else{
+				stringMap.put(length, part);
+			}
+		
+		for(String find : stringMap.values()){
+			result += find;
+		}
+		
+		}
+		println(result);
+	}
 }
