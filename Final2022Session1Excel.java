5ad6e6888ca1c97155f8e65c608259f1233f7154
S <s>
2024-01-20 14:43:30 +0400

    {"types":"C","runs":92}

--- Final2022Session1Excel.java
+++ Final2022Session1Excel.java
@@ -1,4 +1,44 @@
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import javax.swing.*;
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Final2022Session1Excel {
+public class Final2022Session1Excel extends GraphicsProgram {
+	private static final int COLUMN_START_X = 10;
+	private static final int COLUMN_START_Y = 20;
+	private static final int ROW_STEP = 20;
+	private static final int COLUMN_STEP = 100;
 
+	private JTextField colField;
+	private JTextField rowField;
+	private JTextField filterField;
+	private JButton colButton;
+	private JButton rowButton;
+	private JButton filterButton;
+
+	private ArrayList<String> columnNames;
+	private ArrayList<ArrayList<String>> rows;
+
+	public void init() {
+		colField = new JTextField(10);
+		rowField = new JTextField(10);
+		filterField = new JTextField(10);
+		colButton = new JButton("add column");
+		rowButton = new JButton("add row");
+		filterButton = new JButton("filter");
+
+		add(colField, SOUTH);
+		add(colButton, SOUTH);
+		add(rowField, SOUTH);
+		add(rowButton, SOUTH);
+		add(filterField, SOUTH);
+		add(filterButton, SOUTH);
+
+		addActionListeners();
+
+		columnNames = new ArrayList<>();
+		rows = new ArrayList<>();
+	}
 }
--- Final2022Session1Sorted.java
+++ Final2022Session1Sorted.java
@@ -1,4 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-public class Final2022Session1Sorted {
+import acm.program.ConsoleProgram;
 
+public class Final2022Session1Sorted extends ConsoleProgram {
+    public void run() {
+        println("sorted({2,3,4,5}) -> true: " + sorted(new ArrayList<>(Arrays.asList(2, 3, 4, 5))));
+        println("sorted({4,1,3,2}) -> true: " + sorted(new ArrayList<>(Arrays.asList(4, 1, 3, 2))));
+        println("sorted({3,1,5,2,4}) -> false: " + sorted(new ArrayList<>(Arrays.asList(3, 1, 5, 2, 4))));
+    }
+
+    private boolean sorted(ArrayList<Integer> a) {
+        // Check if the list can be sorted by a single swap
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = i + 1; j < a.size(); j++) {
+                if (isSorted(listWithSwapped(a, i, j))) {
+                    return true;
+                }
+            }
+        }
+
+        // Check if the list can be sorted by rotating a single sublist
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = i + 1; j < a.size(); j++) {
+                if (isSorted(listWithRotatedSublist(a, i, j))) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isSorted(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) < list.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private ArrayList<Integer> listWithSwapped(ArrayList<Integer> list, int i, int j) {
+        ArrayList<Integer> newList = new ArrayList<>(list);
+        Collections.swap(newList, i, j);
+        return newList;
+    }
+
+    private ArrayList<Integer> listWithRotatedSublist(ArrayList<Integer> list, int i, int j) {
+        ArrayList<Integer> newList = new ArrayList<>(list);
+        Collections.rotate(newList.subList(i, j + 1), -1);
+        return newList;
+    }
 }
