8b9aa1067188b8e37397e91ced1a632bf638e88f
S <s>
2024-01-20 12:49:53 +0400

    {"types":"C","runs":83}

--- FinalsProblemCasino.java
+++ FinalsProblemCasino.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class FinalsProblemCasino {
+public class FinalsProblemCasino extends ConsoleProgram {
 
+	private static final int N_TRIES = 1000;
+	private static final int MIN_NUM = 0;
+	private static final int MAX_NUM = 36;
+	private int startingMoney = 100;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	public void run() {
+
+		while (N_TRIES > 0) {
+			int guess = readInt("Write your guess: ");
+			int moneyInput = readInt("Put money: ");
+			int spinResult = rgen.nextInt(MIN_NUM, MAX_NUM);
+		
+			if(spinResult == guess ){
+				startingMoney += startingMoney * 36;
+			}
+		}
+	
+	
+	}
 }
