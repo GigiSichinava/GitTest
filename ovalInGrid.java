16e261e20a33679f39e94d974380b163d3f69ce9
S <s>
2024-01-20 15:04:50 +0400

    {"types":"C","runs":103}

--- ovalInGrid.java
+++ ovalInGrid.java
@@ -86,6 +86,7 @@ public class ovalInGrid extends GraphicsProgram implements ComponentListener{
 	  
 	    oval1 = new GOval(rectWidth, rectHeight);
 	    oval1.setFilled(true);
+	    oval1.setFillColor(rgen.nextColor());
 	    add(oval1, (getWidth() - rectWidth) / 2, (getHeight() - rectHeight) / 2);
 	    
 	
