edb757ff2aa21541c9fd8b1a076d221b28ce2840
S <s>
2024-01-20 14:55:36 +0400

    {"types":"C","runs":93}

--- Final2022Session1Excel.java
+++ Final2022Session1Excel.java
@@ -1,7 +1,9 @@
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
+
 import javax.swing.*;
+
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
@@ -41,4 +43,81 @@ public class Final2022Session1Excel extends GraphicsProgram {
 		columnNames = new ArrayList<>();
 		rows = new ArrayList<>();
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == colButton) {
+			addColumn(colField.getText());
+		} else if (e.getSource() == rowButton) {
+			addRow(rowField.getText());
+		} else if (e.getSource() == filterButton) {
+			filterRows(filterField.getText());
+		}
+	}
+
+	private void filterRows(String text) {
+		showGrid(text);
+	}
+
+	private void addRow(String text) {
+		StringTokenizer tokenizer = new StringTokenizer(text, ";");
+		ArrayList<String> newRow = new ArrayList<>();
+		while (tokenizer.hasMoreTokens()) {
+			newRow.add(tokenizer.nextToken());
+		}
+		rows.add(newRow);
+
+		showGrid(null);
+	}
+
+	private void showGrid(String filterWrd) {
+		ArrayList<ArrayList<String>> rowsToShow = rows;
+		if (filterWrd != null) {
+			rowsToShow = filteredRows(filterWrd);
+		}
+		drawColumnNames();
+		int x = COLUMN_START_X;
+		int y = COLUMN_START_Y;
+		for (ArrayList<String> row : rowsToShow) {
+			y += ROW_STEP;
+			drawRow(row, x, y);
+
+		}
+	}
+
+	private ArrayList<ArrayList<String>> filteredRows(String filterWrd) {
+		ArrayList<ArrayList<String>> filtered = new ArrayList<>();
+		for (ArrayList<String> row: rows) {
+			if(row.contains(filterWrd)) {
+				filtered.add(row);
+			}
+		}
+		return filtered;
+	}
+
+	private void addColumn(String text) {
+		columnNames.add(text);
+		drawColumnNames();
+	}
+
+	private void drawColumnNames() {
+		removeAll();
+		int x = COLUMN_START_X;
+		int y = COLUMN_START_Y;
+		drawRow(columnNames, x, y);
+		GLine line = new GLine(COLUMN_START_X, COLUMN_START_Y, COLUMN_START_X + COLUMN_STEP * columnNames.size(),
+				COLUMN_START_Y);
+		add(line);
+
+	}
+
+	private void drawRow(ArrayList<String> row, int x, int y) {
+		for (int i = 0; i < row.size(); i++) {
+			int currX = x + i * COLUMN_STEP;
+			GLabel label = new GLabel(row.get(i));
+			add(label, currX, y);
+		}
+
+	}
+
 }
