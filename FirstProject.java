c33fb2c771352f5765263f16aa634e794a17175d
S <s>
2024-01-24 00:41:33 +0400

    {"types":"C","runs":3}

--- FirstProject.java
+++ FirstProject.java
@@ -1,8 +1,32 @@
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class FirstProject extends GraphicsProgram{
+	
+	private static final int N_ROWS = 10;
+	private static final int N_COLS = 10;
+	private GRect rect;
+	
 	public void init(){
 		setSize(900,900);
+	}
+	
+	public void run(){
+		createGrid();
+		createBroski();
+		createMovement();
+	}
+
+	private void createGrid() {
+		for(int i = 0; i < N_ROWS; i++){
+			for(int j = 0; j < N_COLS; j++){
+				int rectWidth = getWidth() / N_COLS;
+				int rectHeight = getHeight() / N_ROWS;
+				rect = new GRect(rectWidth, rectHeight);
+				
+				add(rect, j * rectWidth, i * rectHeight);
+			}
+		}
 	}
 
 }
