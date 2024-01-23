3987f1f12ebfd4aca134f6b1c8ddb9f65174ec54
S <s>
2024-01-24 03:21:52 +0400

    {"types":"C","runs":63}

--- HashMaps.java
+++ HashMaps.java
@@ -1,4 +1,27 @@
+import java.util.HashMap;
 
-public class HashMaps {
+import acm.program.ConsoleProgram;
 
+public class HashMaps extends ConsoleProgram{
+	
+	private static final String SENTINEL = "";
+	
+	public void run(){
+		HashMap<String, Integer> nameMap = new HashMap<>();
+		while(true){
+			String name = readLine();
+			if(name.equals(SENTINEL)){
+				break;
+			}
+			
+			if(!nameMap.containsKey(name)){
+				nameMap.put(name, 0);
+			}
+			
+			int newCount = nameMap.get(name) + 1;
+			nameMap.put(name, newCount);
+		}
+		
+		println(nameMap);
+	}
 }
